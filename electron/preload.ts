a2d0d3b0406e63826999bab1e4893caf0343a779
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 09:50:23 -0400

    Cleanup

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,4 +1,4 @@
-import { ipcRenderer, contextBridge } from 'electron';
+import { contextBridge, ipcRenderer } from 'electron';
 
 declare global {
   interface Window {
@@ -7,39 +7,17 @@ declare global {
   }
 }
 
+// Expose methods to renderer process
 const api = {
-  /**
-   * Here you can expose functions to the renderer process
-   * so they can interact with the main (electron) side
-   * without security problems.
-   *
-   * The function below can accessed using `window.Main.sayHello`
-   */
   sendMessage: (message: string) => {
     ipcRenderer.send('message', message);
   },
-  /**
-    Here function for AppBar
-   */
-  Minimize: () => {
-    ipcRenderer.send('minimize');
-  },
-  Maximize: () => {
-    ipcRenderer.send('maximize');
-  },
-  Close: () => {
-    ipcRenderer.send('close');
-  },
-  /**
-   * Provide an easier way to listen to events
-   */
+  // Provide an easier way to listen to events
   on: (channel: string, callback: (data: any) => void) => {
     ipcRenderer.on(channel, (_, data) => callback(data));
   }
 };
+
 contextBridge.exposeInMainWorld('Main', api);
-/**
- * Using the ipcRenderer directly in the browser through the contextBridge ist not really secure.
- * I advise using the Main/api way !!
- */
+// WARN: Using the ipcRenderer directly in the browser through the contextBridge is insecure
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer);
