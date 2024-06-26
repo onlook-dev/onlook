5a332f0c0c1e3ec187b72d40cc153f13f9cf28a3
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 12:08:09 -0400

    Update top bar

--- src/routes/editor/FrameList.tsx
+++ src/routes/editor/FrameList.tsx
@@ -3,7 +3,7 @@ import React, { useRef } from 'react';
 function FrameList() {
     const webviewRef = useRef(null);
     return (
-        <webview ref={webviewRef} className='w-[96rem] h-[54rem]' src="https://www.onlook.dev/" ></webview>
+        <webview ref={webviewRef} className='w-[96rem] h-[54rem]' src="https://www.framer.com/" ></webview>
     );
 }
 
