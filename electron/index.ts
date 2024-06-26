d2c9a04d4475aaeaa072be3c19e11661f26816d4
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 10:28:43 -0400

    Clean up

--- electron/index.ts
+++ electron/index.ts
@@ -1,11 +1,11 @@
 import { BrowserWindow, IpcMainEvent, app, ipcMain, screen } from 'electron';
 import isDev from 'electron-is-dev';
 import { join } from 'path';
+import { MainChannel } from '../src/lib/constants';
 
 function loadWindowHtml(window: BrowserWindow) {
   const port = process.env.PORT || 3000;
   const url = isDev ? `http://localhost:${port}` : join(__dirname, '../src/out/index.html');
-
   if (isDev) {
     window?.loadURL(url);
   } else {
@@ -34,21 +34,28 @@ function createWindow() {
     window.webContents.openDevTools();
 }
 
-app.whenReady().then(() => {
-  createWindow();
+function initializeApp() {
+  app.whenReady().then(() => {
+    createWindow();
 
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    app.on('activate', () => {
+      if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
   });
-});
 
-app.on('window-all-closed', () => {
-  const macOS = process.platform === 'darwin';
-  if (macOS) app.quit();
-});
+  app.on('window-all-closed', () => {
+    const macOS = process.platform === 'darwin';
+    if (macOS) app.quit();
+  });
+}
 
-// listen the channel `message` and resend the received message to the renderer process
-ipcMain.on('message', (event: IpcMainEvent, message: any) => {
-  console.log(message);
-  setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
-});
+// TODO: Move this to new module
+function setMessageListeners() {
+  ipcMain.on(MainChannel.NOTIFICATION, (event: IpcMainEvent, message: any) => {
+    console.log(message);
+    setTimeout(() => event.sender.send('message', 'hi from electron'), 500);
+  });
+}
+
+initializeApp();
+setMessageListeners();
