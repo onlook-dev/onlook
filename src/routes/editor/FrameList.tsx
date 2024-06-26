e3a7b4900e71a174abee96d26625a059c56284e9
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 09:50:23 -0400

    Working webview and preload script

--- src/routes/editor/FrameList.tsx
+++ src/routes/editor/FrameList.tsx
@@ -3,7 +3,7 @@ import React, { useRef } from 'react';
 function FrameList() {
     const webviewRef = useRef(null);
     return (
-        <webview ref={webviewRef} className='w-[100rem] h-[70rem]' src="https://www.framer.com/" ></webview>
+        <webview ref={webviewRef} className='w-[96rem] h-[54rem]' src="https://www.framer.com/" ></webview>
     );
 }
 
