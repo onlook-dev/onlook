f1888670eb2ba7263863df95278bbbdd7939fc9e
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 16:51:20 -0400

    Clean up

--- electron/main/index.ts
+++ electron/main/index.ts
@@ -1,24 +1,13 @@
-import { BrowserWindow, app, ipcMain, screen, shell } from 'electron'
+import { BrowserWindow, app, screen, shell } from 'electron'
 import { createRequire } from 'node:module'
 import os from 'node:os'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import { APP_NAME } from '../../src/lib/constants'
-import { update } from './update'
 
 const require = createRequire(import.meta.url)
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-// The built directory structure
-//
-// ├─┬ dist-electron
-// │ ├─┬ main
-// │ │ └── index.js    > Electron-Main
-// │ └─┬ preload
-// │   └── index.mjs   > Preload-Scripts
-// ├─┬ dist
-// │ └── index.html    > Electron-Renderer
-//
 process.env.APP_ROOT = path.join(__dirname, '../..')
 
 export const MAIN_DIST = path.join(process.env.APP_ROOT, 'dist-electron')
@@ -44,12 +33,19 @@ let win: BrowserWindow | null = null
 const preload = path.join(__dirname, '../preload/index.mjs')
 const indexHtml = path.join(RENDERER_DIST, 'index.html')
 
-async function createWindow() {
-  // Get the size of the primary display
-  const primaryDisplay = screen.getPrimaryDisplay();
-  const { width, height } = primaryDisplay.workAreaSize;
 
-  // Create a new browser window with dynamic sizing based on the screen
+function loadWindowContent(win: BrowserWindow) {
+  // Load URL or file based on the environment
+  if (VITE_DEV_SERVER_URL) {
+    win.loadURL(VITE_DEV_SERVER_URL);
+    win.webContents.openDevTools();
+  } else {
+    win.loadFile(indexHtml);
+  }
+}
+
+function createWindow() {
+  const { width, height } = screen.getPrimaryDisplay().workAreaSize;
   win = new BrowserWindow({
     title: APP_NAME,
     icon: path.join(process.env.VITE_PUBLIC, 'favicon.ico'),
@@ -64,63 +60,44 @@ async function createWindow() {
       webviewTag: true,
     },
   });
-  win.maximize();
+  return win;
+}
 
-  // Load URL or file based on the environment
-  if (VITE_DEV_SERVER_URL) {
-    win.loadURL(VITE_DEV_SERVER_URL);
-    win.webContents.openDevTools();
-  } else {
-    win.loadFile(indexHtml);
-  }
+function initMainWindow() {
+  const win = createWindow();
+  loadWindowContent(win);
 
   // Ensure links open externally
   win.webContents.setWindowOpenHandler(({ url }) => {
     if (url.startsWith('https:')) shell.openExternal(url);
     return { action: 'deny' };
   });
-
-  // Handle updates
-  update(win);
 }
 
-app.whenReady().then(createWindow)
+function setListeners() {
+  app.whenReady().then(initMainWindow)
 
-app.on('window-all-closed', () => {
-  win = null
-  if (process.platform !== 'darwin') app.quit()
-})
-
-app.on('second-instance', () => {
-  if (win) {
-    // Focus on the main window if the user tried to open another
-    if (win.isMinimized()) win.restore()
-    win.focus()
-  }
-})
-
-app.on('activate', () => {
-  const allWindows = BrowserWindow.getAllWindows()
-  if (allWindows.length) {
-    allWindows[0].focus()
-  } else {
-    createWindow()
-  }
-})
-
-// New window example arg: new windows url
-ipcMain.handle('open-win', (_, arg) => {
-  const childWindow = new BrowserWindow({
-    webPreferences: {
-      preload,
-      nodeIntegration: true,
-      contextIsolation: false,
-    },
+  app.on('window-all-closed', () => {
+    win = null
+    if (process.platform !== 'darwin') app.quit()
   })
 
-  if (VITE_DEV_SERVER_URL) {
-    childWindow.loadURL(`${VITE_DEV_SERVER_URL}#${arg}`)
-  } else {
-    childWindow.loadFile(indexHtml, { hash: arg })
-  }
-})
+  app.on('second-instance', () => {
+    if (win) {
+      // Focus on the main window if the user tried to open another
+      if (win.isMinimized()) win.restore()
+      win.focus()
+    }
+  })
+
+  app.on('activate', () => {
+    const allWindows = BrowserWindow.getAllWindows()
+    if (allWindows.length) {
+      allWindows[0].focus()
+    } else {
+      initMainWindow()
+    }
+  })
+}
+
+setListeners() 
