02b4b51badd9ebc625ee947904ef4ff577225597
Kiet Ho <hoakiet98@gmail.com>
2024-06-25 23:40:10 -0400

    Add top bar

--- src/routes/editor/FrameList.tsx
+++ src/routes/editor/FrameList.tsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 
 function FrameList() {
     const webviewRef = useRef(null);
-
     return (
         <webview ref={webviewRef} className='w-[100rem] h-[70rem]' src="https://www.framer.com/" ></webview>
     );
