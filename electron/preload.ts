ff2587cdc03d62a83d01ca1aa9486e8a1f5c1e89
Kiet Ho <hoakiet98@gmail.com>
2024-06-26 10:02:18 -0400

    Add preload methods

--- electron/preload.ts
+++ electron/preload.ts
@@ -1,4 +1,5 @@
-import { contextBridge, ipcRenderer } from 'electron';
+import { IpcRendererEvent, contextBridge, ipcRenderer } from 'electron';
+import { MainChannel } from '../src/lib/constants';
 
 declare global {
   interface Window {
@@ -7,17 +8,51 @@ declare global {
   }
 }
 
-// Expose methods to renderer process
-const api = {
-  sendMessage: (message: string) => {
-    ipcRenderer.send('message', message);
+const store = {
+  get(val: any) {
+    return ipcRenderer.sendSync('electron-store-get', val);
   },
-  // Provide an easier way to listen to events
-  on: (channel: string, callback: (data: any) => void) => {
-    ipcRenderer.on(channel, (_, data) => callback(data));
+  set(property: string, val: any) {
+    ipcRenderer.send('electron-store-set', property, val);
+  },
+  has(val: any) {
+    return ipcRenderer.sendSync('electron-store-has', val);
   }
+}
+
+const api = {
+  store: store,
+
+  sendMessage<T>(channel: MainChannel, args: T[]) {
+    ipcRenderer.send(channel, args);
+  },
+
+  on<T>(channel: MainChannel, func: (...args: T[]) => void) {
+    const subscription = (_event: IpcRendererEvent, ...args: T[]) =>
+      func(...args);
+    ipcRenderer.on(channel, subscription);
+    return () => ipcRenderer.removeListener(channel, subscription);
+  },
+
+  once<T>(channel: MainChannel, func: (...args: T[]) => void) {
+    ipcRenderer.once(channel, (_event, ...args) => func(...args));
+  },
+
+  invoke<T, P>(channel: MainChannel, ...args: T[]): Promise<P> {
+    return ipcRenderer.invoke(channel, ...args);
+  },
+
+  removeListener<T>(channel: MainChannel, listener: (...args: T[]) => void) {
+    ipcRenderer.removeListener(channel, listener as (event: Electron.IpcRendererEvent, ...args: any[]) => void);
+  },
+
+  removeAllListeners(channel: MainChannel) {
+    ipcRenderer.removeAllListeners(channel);
+  },
 };
 
+// Expose methods to renderer process
 contextBridge.exposeInMainWorld('Main', api);
+
 // WARN: Using the ipcRenderer directly in the browser through the contextBridge is insecure
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer);
