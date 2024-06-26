160157e374af10fdd7c052abec0b06a69fe1c2b8
Kiet Ho <hoakiet98@gmail.com>
2024-06-25 22:30:45 -0400

    Add UI and preload script

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import AppBar from './AppBar';
+import { ThemeProvider } from './components/theme-provider';
 import ProjectEditor from './routes/editor';
 
 function App() {
-  // console.log(window.ipcRenderer);
-
-  const [isOpen, setOpen] = useState(false);
-  const [isSent, setSent] = useState(false);
-  const [fromMain, setFromMain] = useState<string | null>(null);
-
-  const handleToggle = () => {
-    if (isOpen) {
-      setOpen(false);
-      setSent(false);
-    } else {
-      setOpen(true);
-      setFromMain(null);
-    }
-  };
-  const sendMessageToElectron = () => {
-    if (window.Main) {
-      window.Main.sendMessage("Hello I'm from React World");
-    } else {
-      setFromMain('You are in a Browser, so no Electron functions are available');
-    }
-    setSent(true);
-  };
-
-  useEffect(() => {
-    if (isSent && window.Main)
-      window.Main.on('message', (message: string) => {
-        setFromMain(message);
-      });
-  }, [fromMain, isSent]);
-
   return (
-    <div className="flex flex-col h-screen w-screen bg-black">
-      {window.Main && (
-        <div className="flex-none">
-          <AppBar />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex flex-col h-screen w-screen bg-black">
+        {window.Main && (
+          <div className="flex-none">
+            <AppBar />
+          </div>
+        )}
+        <div className='flex-grow overflow-hidden'>
+          <ProjectEditor />
         </div>
-      )}
-      <div className='flex-grow overflow-hidden'>
-        <ProjectEditor />
       </div>
-    </div>
+    </ThemeProvider>
+
   );
 }
 
---
+++ src/routes/editor/EditorTopBar.tsx
@@ -0,0 +1,19 @@
+import { Button } from '@/components/ui/button';
+import { Label } from "@/components/ui/label";
+import React from 'react';
+
+const EditorTopBar = () => {
+    return (
+        <div className='flex flex-row w-full h-full justify-center px-1'>
+            <div className='flex-grow basis-0'>
+                <Button variant='ghost' size="sm" className=''>Actions</Button>
+            </div>
+            <Label className='my-auto font-normal'>Your Project</Label>
+            <div className='flex space-x-2 flex-grow basis-0 justify-end'>
+                <Button variant='ghost' size="sm" className=''>Share</Button>
+                <Button variant='outline' size="sm" className=''>Publish</Button>
+            </div>
+        </div>
+    );
+};
+export default EditorTopBar;
