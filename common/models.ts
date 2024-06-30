e43f212f8abd0d4e8bddd6135b05a89b8860d974
Kiet Ho <hoakiet98@gmail.com>
2024-06-30 16:13:22 -0400

    Saving

--- common/models.ts
+++ common/models.ts
@@ -4,8 +4,5 @@ export interface ElementMetadata {
     rect: DOMRect;
     computedStyle: CSSStyleDeclaration;
     webviewId: string;
-<<<<<<< HEAD
     dataOnlookId?: string;
-=======
->>>>>>> 7815e77 (Add webview management)
 }
