b418bf80ee41a13c96c57e663c1811c0edd3a815
Kiet Ho <hoakiet98@gmail.com>
2024-07-03 10:20:02 -0400

    Add active state

--- src/routes/project/WebviewArea/Webview.tsx
+++ src/routes/project/WebviewArea/Webview.tsx
@@ -12,11 +12,17 @@ function Webview({ messageBridge, metadata }: { messageBridge: WebviewMessageBri
     const editorEngine = useEditorEngine();
     const [webviewPreloadPath, setWebviewPreloadPath] = useState<string>('');
     const [webviewSrc, setWebviewSrc] = useState<string>(metadata.src);
+    // TODO: Move this outside
+    const [selectedWebview, selectWebview] = useState<string>('')
 
     function fetchPreloadPath() {
         window.Main.invoke(MainChannels.WEBVIEW_PRELOAD_PATH).then((preloadPath: any) => {
             setWebviewPreloadPath(preloadPath);
         });
+    }
+
+    function webviewSelected() {
+        return selectedWebview === metadata.id;
     }
 
     function updateUrl(e: React.KeyboardEvent<HTMLInputElement>) {
@@ -72,21 +78,24 @@ function Webview({ messageBridge, metadata }: { messageBridge: WebviewMessageBri
 
     if (webviewPreloadPath)
         return (
-            <div className='flex flex-col space-y-4'>
-                <div className='flex flex-row items-center space-x-2'>
+            <div className='flex flex-col space-y-2'>
+                <div className={`flex flex-row items-center space-x-2 p-2 rounded-lg backdrop-blur-sm  ${webviewSelected() ? ' bg-black/20 ' : ''}`}>
                     <Button variant='outline' onClick={goBack}><ArrowLeftIcon /></Button>
                     <Button variant='outline' onClick={goForward}><ArrowRightIcon /></Button>
                     <Button variant='outline' onClick={reload}><ReloadIcon /></Button>
                     <Input className='text-xl' value={webviewSrc} onChange={(e) => setWebviewSrc(e.target.value)} onKeyDown={updateUrl} />
                 </div>
-                <webview
-                    id={metadata.id}
-                    ref={webviewRef}
-                    className='w-[96rem] h-[70rem]'
-                    src={metadata.src}
-                    preload={`file://${webviewPreloadPath}`}
-                    allowpopups={"true" as any}
-                ></webview>
+                <div className='relative'>
+                    <webview
+                        id={metadata.id}
+                        ref={webviewRef}
+                        className={`w-[96rem] h-[70rem] bg-black/10 backdrop-blur-sm`}
+                        src={metadata.src}
+                        preload={`file://${webviewPreloadPath}`}
+                        allowpopups={"true" as any}
+                    ></webview>
+                    {!webviewSelected() && (<div className='absolute inset-0 bg-transparent hover:ring-2 ring-red-900' onClick={() => selectWebview(metadata.id)}></div>)}
+                </div>
             </div>
         );
 }
