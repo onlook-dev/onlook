826433d68f75c0bcc8019abfc43038a9869022c1
Kiet Ho <hoakiet98@gmail.com>
2024-07-02 22:48:16 -0400

    Clean up

--- common/helpers.ts
+++ common/helpers.ts
@@ -6,17 +6,16 @@ export function querySelectorCommand(selector: string) {
 
 export function compareTemplateNodes(node1: TemplateNode, node2: TemplateNode): number {
     if (node1.startTag.start.line < node2.startTag.start.line) {
-        return -1;  // node1 comes before node2
+        return -1;
     } else if (node1.startTag.start.line > node2.startTag.start.line) {
-        return 1;   // node1 comes after node2
+        return 1;
     } else {
-        // Lines are the same, compare columns
         if (node1.startTag.start.column < node2.startTag.start.column) {
-            return -1;  // node1 comes before node2
+            return -1;
         } else if (node1.startTag.start.column > node2.startTag.start.column) {
-            return 1;   // node1 comes after node2
+            return 1;
         } else {
-            return 0;   // Both nodes are at the same position
+            return 0;
         }
     }
 }
--- electron/main/code/files.ts
+++ electron/main/code/files.ts
@@ -28,26 +28,32 @@ export async function writeFile(filePath: string, content: string): Promise<void
 export async function readBlock(templateNode: TemplateNode): Promise<string> {
     try {
         const filePath = templateNode.path;
+
         const startTag = templateNode.startTag;
-        const endTag = templateNode.endTag || startTag;
         const startRow = startTag.start.line;
         const startColumn = startTag.start.column;
+
+        const endTag = templateNode.endTag || startTag;
         const endRow = endTag.end.line;
         const endColumn = endTag.end.column;
 
         const fileContent = await readFile(filePath);
         const lines = fileContent.split('\n');
 
-        const selectedText = lines.slice(startRow - 1, endRow) // Slice from startRow to endRow
+        const selectedText = lines.slice(startRow - 1, endRow)
             .map((line, index, array) => {
-                if (index === 0 && array.length === 1) { // Only one line
+                if (index === 0 && array.length === 1) {
+                    // Only one line
                     return line.substring(startColumn - 1, endColumn);
-                } else if (index === 0) { // First line of multiple
+                } else if (index === 0) {
+                    // First line of multiple
                     return line.substring(startColumn - 1);
-                } else if (index === array.length - 1) { // Last line
+                } else if (index === array.length - 1) {
+                    // Last line
                     return line.substring(0, endColumn);
                 }
-                return line; // Full lines in between
+                // Full lines in between
+                return line;
             })
             .join('\n');
 
@@ -59,14 +65,16 @@ export async function readBlock(templateNode: TemplateNode): Promise<string> {
 }
 
 export async function writeCodeResults(codeResults: CodeResult[]): Promise<void> {
+    // Write from bottom to prevent line offset
     const sortedCodeResults = codeResults.sort((a, b) => compareTemplateNodes(a.param.templateNode, b.param.templateNode)).toReversed()
-
     const files = new Map<string, string>()
+
     for (const result of sortedCodeResults) {
         let fileContent = files.get(result.param.templateNode.path)
         if (!fileContent) {
             fileContent = await readFile(result.param.templateNode.path)
         }
+
         const newFileContent = await writeBlock(result.param.templateNode, result.generated, fileContent)
         files.set(result.param.templateNode.path, newFileContent)
     }
@@ -79,10 +87,10 @@ export async function writeCodeResults(codeResults: CodeResult[]): Promise<void>
 export async function writeBlock(templateNode: TemplateNode, newBlock: string, fileContent: string): Promise<string> {
     try {
         const startTag = templateNode.startTag;
-        const endTag = templateNode.endTag || startTag;
-
         const startRow = startTag.start.line;
         const startColumn = startTag.start.column;
+
+        const endTag = templateNode.endTag || startTag;
         const endRow = endTag.end.line;
         const endColumn = endTag.end.column;
 
@@ -95,6 +103,7 @@ export async function writeBlock(templateNode: TemplateNode, newBlock: string, f
 
         const newFileContent = [before, firstLine + newBlock + lastLine, after].join('\n');
         return newFileContent;
+
     } catch (error: any) {
         console.error('Error replacing range in file:', error);
         throw error;
