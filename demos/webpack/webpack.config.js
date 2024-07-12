467836193270e7c785fdcb741ee2ba60e8f1dc76
Kiet <31864905+Kitenite@users.noreply.github.com>
2024-07-12 17:03:48 -0400

    Add webpack support docs (#55)
    * Add webpack support docs
    
    * Fix babel plugin peer dependecy issue

---
+++ demos/webpack/webpack.config.js
@@ -0,0 +1,36 @@
+// webpack.config.js
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "public", "index.html"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
+  devServer: {
+    port: 3000,
+  },
+};
