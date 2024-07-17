8d15eb786065f537fb762aa24c4be8dd69bcb062
Kiet Ho <hoakiet98@gmail.com>
2024-07-17 16:21:32 -0400

    Update primitives to work and remove old usages

---
+++ app/tailwind.primitives.js
@@ -0,0 +1,169 @@
+export const colors = {
+    amber: '#ff8020', //to be extended
+    black: {
+        DEFAULT: '#000000',
+        30: '#0000004d',
+        60: '#00000099',
+        85: '#000000d9',
+    },
+    purple: '#6c33ff', //to be extended
+    red: {
+        //to be extended
+        DEFAULT: '#ea364e',
+        20: '#ea364e33',
+    },
+    blue: {
+        100: '#001b2e',
+        200: '#002a48',
+        300: '#003e69',
+        400: '#00538f',
+        500: '#006ab5',
+        600: '#0081de',
+        700: '#90d1ff',
+        800: '#109bff',
+        900: '#53b8ff',
+        1000: '#e3f3ff',
+    },
+    gray: {
+        100: '#1a1a1a',
+        200: '#1f1f1f',
+        300: '#333333',
+        400: '#494949',
+        500: '#606060',
+        600: '#787878',
+        700: '#c7c7c7',
+        800: '#929292',
+        900: '#acacac',
+        1000: '#ffffff',
+    },
+    green: {
+        100: '#00240c',
+        200: '#002a0e',
+        300: '#004116',
+        400: '#00591e',
+        500: '#007226',
+        600: '#008c2f',
+        700: '#00e14b',
+        800: '#00a838',
+        900: '#00c441',
+        1000: '#d8ffe5',
+    },
+    teal: {
+        100: '#00211c',
+        200: '#002a23',
+        300: '#004036',
+        400: '#005849',
+        500: '#00715e',
+        600: '#008b74',
+        700: '#00deba',
+        800: '#00a68b',
+        900: '#00c1a2',
+        1000: '#cbfff6',
+    },
+    yellow: {
+        100: '#211a00',
+        200: '#312600',
+        300: '#493900',
+        400: '#644e00',
+        500: '#7f6300',
+        600: '#9b7900',
+        700: '#f6c100',
+        800: '#b99000',
+        900: '#d7a800',
+        1000: '#fff0bc',
+    },
+};
+
+export const fontSize = {
+    title1: [
+        '2.25rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+        },
+    ],
+    title2: [
+        '1rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+        },
+    ],
+    title3: [
+        '1rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+        },
+    ],
+    largePlus: [
+        '1rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: '500',
+        },
+    ],
+    large: [
+        '1rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: 'normal',
+        },
+    ],
+    regularPlus: [
+        '0.9375rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: '500',
+        },
+    ],
+    regular: [
+        '0.9375rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: 'normal',
+        },
+    ],
+    smallPlus: [
+        '0.8125rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: '500',
+        },
+    ],
+    small: [
+        '0.8125rem',
+        {
+            lineHeight: '8.75',
+            fontWeight: 'normal',
+        },
+    ],
+    miniPlus: [
+        '1rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: '500',
+        },
+    ],
+    mini: [
+        '1rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+        },
+    ],
+    microPlus: [
+        '0.6875rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: '500',
+        },
+    ],
+    micro: [
+        '0.6875rem',
+        {
+            lineHeight: 'normal',
+            fontWeight: 'normal',
+        },
+    ],
+};
