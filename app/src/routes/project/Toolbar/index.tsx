f214ed26bb7294f7e2831c48b5f3b73605a58d0e
Kiet Ho <hoakiet98@gmail.com>
2024-08-05 16:19:04 -0400

    Clean up

--- app/src/routes/project/Toolbar/index.tsx
+++ app/src/routes/project/Toolbar/index.tsx
@@ -38,6 +38,7 @@ const Toolbar = observer(() => {
                     <HandIcon />
                 </ToggleGroupItem>
                 <ToggleGroupItem
+                    disabled={true}
                     value={EditorMode.INSERT_DIV}
                     aria-label={EditorMode.INSERT_DIV + ' Mode'}
                 >
