be776d84d0a7d34c50beebe554822da705b1ed88
Bach Le <bach.le@c0x12c.com>
2024-08-07 10:24:32 +0700

    Revert changes

--- demos/next/next.config.mjs
+++ demos/next/next.config.mjs
@@ -4,7 +4,8 @@ import path from "path";
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
-    swcPlugins: [["@onlook/nextjs", { root: path.resolve(".") }]]
-  }
+    swcPlugins: [["@onlook/nextjs", { root: path.resolve(".") }]],
+  },
 };
+
 export default nextConfig;
