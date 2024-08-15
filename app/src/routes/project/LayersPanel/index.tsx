b9d21177c8a5a027eedaff3ed602ab95754e62e2
Kiet <31864905+Kitenite@users.noreply.github.com>
2024-08-15 16:23:21 -0400

    Resize layers panel (#164)

--- app/src/routes/project/LayersPanel/index.tsx
+++ app/src/routes/project/LayersPanel/index.tsx
@@ -3,12 +3,45 @@ import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 import { EditorMode } from '@/lib/models';
 import clsx from 'clsx';
 import { observer } from 'mobx-react-lite';
+import { useRef, useState } from 'react';
 import { useEditorEngine } from '..';
 import LayersTab from './LayersTab';
 import { capitalizeFirstLetter } from '/common/helpers';
 
 const LayersPanel = observer(() => {
     const editorEngine = useEditorEngine();
+    const panelRef = useRef<HTMLDivElement>(null);
+    const [panelWidth, setPanelWidth] = useState(240);
+
+    const startResize = (e: any) => {
+        e.preventDefault();
+        e.stopPropagation();
+
+        const panel = panelRef.current;
+        if (!panel) {
+            return;
+        }
+        const startX = e.clientX;
+        const boundingRect = panel.getBoundingClientRect();
+        const startWidth = boundingRect.width;
+
+        const resize: any = (e: MouseEvent) => {
+            const currentWidth = startWidth + e.clientX - startX;
+            setPanelWidth(currentWidth);
+        };
+
+        const stopResize = (e: any) => {
+            e.preventDefault();
+            e.stopPropagation();
+
+            window.removeEventListener('mousemove', resize);
+            window.removeEventListener('mouseup', stopResize);
+        };
+
+        window.addEventListener('mousemove', resize);
+        window.addEventListener('mouseup', stopResize);
+    };
+
     enum TabValue {
         LAYERS = 'layers',
         COMPONENTS = 'components',
@@ -41,11 +74,17 @@ const LayersPanel = observer(() => {
     return (
         <div
             className={clsx(
-                'border max-w-60 min-w-60 bg-black/80 backdrop-blur rounded-tr-xl shadow',
+                'border min-w-60 max-w-96 bg-black/80 backdrop-blur rounded-tr-xl shadow',
                 editorEngine.mode === EditorMode.INTERACT ? 'hidden' : 'visible',
             )}
+            ref={panelRef}
+            style={{ width: `${panelWidth}px` }}
         >
             {renderTabs()}
+            <div
+                className="absolute -right-1 top-0 h-full w-2 cursor-ew-resize"
+                onMouseDown={startResize}
+            />
         </div>
     );
 });
