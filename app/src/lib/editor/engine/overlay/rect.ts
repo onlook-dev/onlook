a828015d2f2a45e258beaf1b816ce47f64408ce6
Kiet Ho <hoakiet98@gmail.com>
2024-08-31 10:17:09 -0400

    Move showDimensions into clickRect

--- app/src/lib/editor/engine/overlay/rect.ts
+++ app/src/lib/editor/engine/overlay/rect.ts
@@ -42,11 +42,7 @@ export class RectImpl implements Rect {
         this.element.appendChild(this.svgElement);
     }
 
-    render(
-        { width, height, top, left }: RectDimensions,
-        isComponent: boolean = false,
-        showDimensions: boolean = false,
-    ) {
+    render({ width, height, top, left }: RectDimensions, isComponent: boolean = false) {
         this.svgElement.setAttribute('width', width.toString());
         this.svgElement.setAttribute('height', height.toString());
         this.svgElement.setAttribute('viewBox', `0 0 ${width} ${height}`);
@@ -55,43 +51,6 @@ export class RectImpl implements Rect {
         this.rectElement.setAttribute('stroke', isComponent ? '#A855F7' : '#FF0E48');
         this.element.style.top = `${top}px`;
         this.element.style.left = `${left}px`;
-
-        if (showDimensions) {
-            const text = document.createElementNS(this.svgNamespace, 'text') as SVGTextElement;
-            text.setAttribute('x', '0');
-            text.setAttribute('y', '0');
-            text.setAttribute('fill', 'white');
-            text.setAttribute('font-size', '12');
-            text.textContent = `${parseInt(width.toString())} x ${parseInt(height.toString())}`;
-
-            // Temporarily add the text to measure it
-            this.svgElement.appendChild(text);
-            const bbox = text.getBBox();
-            this.svgElement.removeChild(text);
-
-            const padding = { top: 2, bottom: 2, left: 4, right: 4 };
-            const rectWidth = bbox.width + padding.left + padding.right;
-            const rectHeight = bbox.height + padding.top + padding.bottom;
-            const rectX = (width - rectWidth) / 2;
-            const rectY = height;
-
-            const textRect = document.createElementNS(this.svgNamespace, 'rect');
-            textRect.setAttribute('x', rectX.toString());
-            textRect.setAttribute('y', rectY.toString());
-            textRect.setAttribute('width', rectWidth.toString());
-            textRect.setAttribute('height', rectHeight.toString());
-            textRect.setAttribute('fill', 'red');
-            textRect.setAttribute('rx', '2');
-
-            // Adjust text position
-            text.setAttribute('x', `${width / 2}`);
-            text.setAttribute('y', `${rectY + rectHeight / 2}`);
-            text.setAttribute('text-anchor', 'middle');
-            text.setAttribute('dominant-baseline', 'middle');
-
-            this.svgElement.appendChild(textRect);
-            this.svgElement.appendChild(text);
-        }
     }
 }
 
@@ -319,6 +278,43 @@ export class ClickRect extends RectImpl {
         this.svgElement.appendChild(fullRect);
     }
 
+    showDimensions(width: number, height: number, isComponent: boolean = false) {
+        const text = document.createElementNS(this.svgNamespace, 'text') as SVGTextElement;
+        text.setAttribute('x', '0');
+        text.setAttribute('y', '0');
+        text.setAttribute('fill', 'white');
+        text.setAttribute('font-size', '12');
+        text.textContent = `${parseInt(width.toString())} x ${parseInt(height.toString())}`;
+
+        // Temporarily add the text to measure it
+        this.svgElement.appendChild(text);
+        const bbox = text.getBBox();
+        this.svgElement.removeChild(text);
+
+        const padding = { top: 2, bottom: 2, left: 4, right: 4 };
+        const rectWidth = bbox.width + padding.left + padding.right;
+        const rectHeight = bbox.height + padding.top + padding.bottom;
+        const rectX = (width - rectWidth) / 2;
+        const rectY = height;
+
+        const textRect = document.createElementNS(this.svgNamespace, 'rect');
+        textRect.setAttribute('x', rectX.toString());
+        textRect.setAttribute('y', rectY.toString());
+        textRect.setAttribute('width', rectWidth.toString());
+        textRect.setAttribute('height', rectHeight.toString());
+        textRect.setAttribute('fill', isComponent ? '#A855F7' : '#FF0E48');
+        textRect.setAttribute('rx', '2');
+
+        // Adjust text position
+        text.setAttribute('x', `${width / 2}`);
+        text.setAttribute('y', `${rectY + rectHeight / 2}`);
+        text.setAttribute('text-anchor', 'middle');
+        text.setAttribute('dominant-baseline', 'middle');
+
+        this.svgElement.appendChild(textRect);
+        this.svgElement.appendChild(text);
+    }
+
     render(
         {
             width,
@@ -336,15 +332,15 @@ export class ClickRect extends RectImpl {
             padding: string;
         },
         isComponent?: boolean,
-        showDimensions: boolean = false,
     ) {
         // Sometimes a selected element can be removed. We handle this gracefully.
         try {
             this.updateMargin(margin, { width, height });
             this.updatePadding(padding, { width, height });
+            this.showDimensions(width, height, isComponent);
 
             // Render the base rect (the element itself) on top
-            super.render({ width, height, top, left }, isComponent, showDimensions);
+            super.render({ width, height, top, left }, isComponent);
         } catch (error) {
             console.warn(error);
         }
