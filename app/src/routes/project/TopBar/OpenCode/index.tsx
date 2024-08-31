1b5d1e0b35c3852e7bb464eee430962e1f0dc121
Kiet Ho <hoakiet98@gmail.com>
2024-08-31 14:55:26 -0400

    Simplify dropdown button

--- app/src/routes/project/TopBar/OpenCode/index.tsx
+++ app/src/routes/project/TopBar/OpenCode/index.tsx
@@ -6,12 +6,13 @@ import {
     DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Component1Icon, ComponentInstanceIcon } from '@radix-ui/react-icons';
+import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import { useEditorEngine } from '../..';
 import { WebViewElement } from '/common/models/element';
 import { TemplateNode } from '/common/models/element/templateNode';
 
-const OpenCode = () => {
+const OpenCode = observer(() => {
     const editorEngine = useEditorEngine();
     const [instance, setInstance] = useState<TemplateNode | undefined>();
     const [root, setRoot] = useState<TemplateNode | undefined>();
@@ -32,37 +33,30 @@ const OpenCode = () => {
         editorEngine.code.viewSource(templateNode);
     }
 
-    function renderButton(disableClick = false) {
-        return (
-            <Button
-                disabled={!instance && !root}
-                variant="outline"
-                size="sm"
-                className=""
-                onClick={() => !disableClick && viewSource(instance || root)}
-            >
-                <div className="text-default h-3 w-3 mr-1">
-                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256">
-                        <g fill="#ffffff">
-                            <g transform="scale(10.66667,10.66667)">
-                                <path d="M22,4.94v14.09c0,0 -2.69,1.3 -4,1.93v-17.93c1.42,0.68 4,1.91 4,1.91zM4.65,13.35l2.16,1.97c-0.97,0.73 -1.77,1.34 -2.26,1.7l-2.11,-1.65zM16,3.02v5.32c0,0 -1.36,1.04 -3.17,2.41c-0.94,-0.72 -1.94,-1.48 -2.9,-2.2l5.94,-5.41zM16,20.983v-5.323c0,0 -9.202,-6.994 -11.453,-8.681l-2.103,1.652z">
-                                    {' '}
-                                </path>
-                            </g>
-                        </g>
-                    </svg>
-                </div>
-                Open in VSCode
-            </Button>
-        );
-    }
     return (
         <DropdownMenu>
-            {instance ? (
-                <DropdownMenuTrigger asChild> {renderButton(true)} </DropdownMenuTrigger>
-            ) : (
-                renderButton()
-            )}
+            <DropdownMenuTrigger asChild disabled={!instance}>
+                <Button
+                    disabled={!instance && !root}
+                    variant="outline"
+                    size="sm"
+                    className=""
+                    onClick={() => viewSource(instance || root)}
+                >
+                    <div className="text-default h-3 w-3 mr-1">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256">
+                            <g fill="#ffffff">
+                                <g transform="scale(10.66667,10.66667)">
+                                    <path d="M22,4.94v14.09c0,0 -2.69,1.3 -4,1.93v-17.93c1.42,0.68 4,1.91 4,1.91zM4.65,13.35l2.16,1.97c-0.97,0.73 -1.77,1.34 -2.26,1.7l-2.11,-1.65zM16,3.02v5.32c0,0 -1.36,1.04 -3.17,2.41c-0.94,-0.72 -1.94,-1.48 -2.9,-2.2l5.94,-5.41zM16,20.983v-5.323c0,0 -9.202,-6.994 -11.453,-8.681l-2.103,1.652z">
+                                        {' '}
+                                    </path>
+                                </g>
+                            </g>
+                        </svg>
+                    </div>
+                    Open in VSCode
+                </Button>
+            </DropdownMenuTrigger>
             <DropdownMenuContent>
                 <DropdownMenuItem
                     className="text-xs"
@@ -85,6 +79,6 @@ const OpenCode = () => {
             </DropdownMenuContent>
         </DropdownMenu>
     );
-};
+});
 
 export default OpenCode;
