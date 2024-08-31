05b105a19704ecd524e5990bca062f07b8f055ee
Kiet Ho <hoakiet98@gmail.com>
2024-08-31 15:32:58 -0400

    Add new vscode logo

--- app/src/routes/project/TopBar/OpenCode/index.tsx
+++ app/src/routes/project/TopBar/OpenCode/index.tsx
@@ -9,6 +9,7 @@ import { Component1Icon, ComponentInstanceIcon } from '@radix-ui/react-icons';
 import { observer } from 'mobx-react-lite';
 import { useEffect, useState } from 'react';
 import { useEditorEngine } from '../..';
+import VsCodeIcon from './vscode.svg';
 import { WebViewElement } from '/common/models/element';
 import { TemplateNode } from '/common/models/element/templateNode';
 
@@ -43,16 +44,8 @@ const OpenCode = observer(() => {
                     className=""
                     onClick={() => viewSource(instance || root)}
                 >
-                    <div className="text-default h-3 w-3 mr-1">
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0,0,256,256">
-                            <g fill="#ffffff">
-                                <g transform="scale(10.66667,10.66667)">
-                                    <path d="M22,4.94v14.09c0,0 -2.69,1.3 -4,1.93v-17.93c1.42,0.68 4,1.91 4,1.91zM4.65,13.35l2.16,1.97c-0.97,0.73 -1.77,1.34 -2.26,1.7l-2.11,-1.65zM16,3.02v5.32c0,0 -1.36,1.04 -3.17,2.41c-0.94,-0.72 -1.94,-1.48 -2.9,-2.2l5.94,-5.41zM16,20.983v-5.323c0,0 -9.202,-6.994 -11.453,-8.681l-2.103,1.652z">
-                                        {' '}
-                                    </path>
-                                </g>
-                            </g>
-                        </svg>
+                    <div className="text-default h-3 w-3 mr-2">
+                        <img src={VsCodeIcon} alt="VsCode Icon" />
                     </div>
                     Open in VSCode
                 </Button>
