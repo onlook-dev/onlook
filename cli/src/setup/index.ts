9e830eafcdc75027d49e29b599a1aa8c3e12f329
Kiet Ho <hoakiet98@gmail.com>
2024-09-02 16:01:56 -0400

    Working indetification

--- cli/src/setup/index.ts
+++ cli/src/setup/index.ts
@@ -1,10 +1,7 @@
-#!/usr/bin/env node
-
-// import { ensureConfigOverrides, isCRAProject, modifyStartScript } from './cra';
+import { isCRAProject } from './cra';
 import { isNextJsProject } from './next';
-// import { getFileExtensionByPattern, installPackages } from './utils';
-// import { isViteJsProject, modifyViteConfig } from './vite';
-// import { isWebpackProject, modifyBabelrc, modifyWebpackConfig } from './webpack';
+import { isViteJsProject } from './vite';
+import { isWebpackProject } from './webpack';
 
 const setup = async (): Promise<void> => {
   try {
@@ -18,34 +15,34 @@ const setup = async (): Promise<void> => {
       return;
     }
 
-    // if (await isCRAProject()) {
-    //   console.log('This is a create-react-app project.');
-    //   await installPackages(CRA_DEPENDENCIES);
-    //   ensureConfigOverrides();
-    //   modifyStartScript();
-    //   return;
-    // }
+    if (await isCRAProject()) {
+      console.log('This is a create-react-app project.');
+      // await installPackages(CRA_DEPENDENCIES);
+      // ensureConfigOverrides();
+      // modifyStartScript();
+      return;
+    }
 
-    // if (await isWebpackProject()) {
-    //   console.log('This is a webpack project.');
-    //   await installPackages(WEBPACK_DEPENDENCIES);
-    //   const configFileExtension = await getFileExtensionByPattern(process.cwd(), CONFIG_FILE_PATTERN[BUILD_TOOL_NAME.WEBPACK]);
-    //   if (configFileExtension) {
-    //     modifyWebpackConfig(configFileExtension);
-    //   }
-    //   modifyBabelrc();
-    //   return;
-    // }
+    if (await isWebpackProject()) {
+      console.log('This is a webpack project.');
+      //   await installPackages(WEBPACK_DEPENDENCIES);
+      //   const configFileExtension = await getFileExtensionByPattern(process.cwd(), CONFIG_FILE_PATTERN[BUILD_TOOL_NAME.WEBPACK]);
+      //   if (configFileExtension) {
+      //     modifyWebpackConfig(configFileExtension);
+      //   }
+      //   modifyBabelrc();
+      return;
+    }
 
-    // if (await isViteJsProject()) {
-    //   console.log('This is a Vite project.');
-    //   await installPackages(VITE_DEPENDENCIES);
-    //   const configFileExtension = await getFileExtensionByPattern(process.cwd(), CONFIG_FILE_PATTERN[BUILD_TOOL_NAME.VITE]);
-    //   if (configFileExtension) {
-    //     modifyViteConfig(configFileExtension);
-    //   }
-    //   return;
-    // }
+    if (await isViteJsProject()) {
+      console.log('This is a Vite project.');
+      //   await installPackages(VITE_DEPENDENCIES);
+      //   const configFileExtension = await getFileExtensionByPattern(process.cwd(), CONFIG_FILE_PATTERN[BUILD_TOOL_NAME.VITE]);
+      //   if (configFileExtension) {
+      //     modifyViteConfig(configFileExtension);
+      //   }
+      return;
+    }
 
     console.warn('Cannot determine the project framework.');
 
