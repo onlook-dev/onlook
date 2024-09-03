67752ce874734245640ec31db9bb1870f98b8296
Kiet Ho <hoakiet98@gmail.com>
2024-09-03 08:57:08 -0400

    Vite working

--- demos/remix/vite.config.ts
+++ demos/remix/vite.config.ts
@@ -1,7 +1,7 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [
