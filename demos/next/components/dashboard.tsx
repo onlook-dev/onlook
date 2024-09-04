c903e679f250bc6904b62afe7ae41f91bac01831
Kiet Ho <hoakiet98@gmail.com>
2024-09-04 09:57:35 -0400

    Update babel plugin

--- demos/next/components/dashboard.tsx
+++ demos/next/components/dashboard.tsx
@@ -44,7 +44,7 @@ import {
 } from "@/components/ui/table"
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
 import Link from "next/link"
-import React from "react"
+import React, { Fragment } from "react"
 import {
   ChevronLeftIcon,
   ChevronRightIcon,
@@ -68,7 +68,9 @@ export function Dashboard() {
   return (
     <div className="flex min-h-screen w-full flex-col bg-muted/40">
       <React.Fragment>
-        <Sidebar />
+        <Fragment>
+          <Sidebar />
+        </Fragment>
       </React.Fragment>
       <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
         <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
