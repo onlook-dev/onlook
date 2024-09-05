0dbd8447a8a2fa63c9bbcfda95d868723fd03f65
Kiet Ho <hoakiet98@gmail.com>
2024-09-05 15:59:09 -0400

    Update setup doc link

--- app/common/constants.ts
+++ app/common/constants.ts
@@ -63,7 +63,7 @@ export enum MainChannels {
 export enum Links {
     DISCORD = 'https://discord.gg/hERDfFZCsH',
     GITHUB = 'https://github.com/onlook-dev/onlook',
-    USAGE_DOCS = 'https://github.com/onlook-dev/onlook?tab=readme-ov-file#using-your-own-react-project',
+    USAGE_DOCS = 'https://github.com/onlook-dev/onlook/wiki/How-to-set-up-my-project%3F',
     WIKI = 'https://github.com/onlook-dev/onlook/wiki',
     OPEN_ISSUE = 'https://github.com/onlook-dev/onlook/issues/new/choose',
 }
