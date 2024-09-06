4d2ca38dcbd2275a9a9ffd2c91c0908e64b73728
Kiet Ho <hoakiet98@gmail.com>
2024-09-06 16:26:47 -0400

    Clean up

--- app/electron/preload/webview/dom.ts
+++ app/electron/preload/webview/dom.ts
@@ -4,22 +4,14 @@ import { uuid } from './bundles/uuid.js';
 import { ipcRenderer } from 'electron';
 import { EditorAttributes, WebviewChannels } from '/common/constants';
 
-export function processDom() {
-    function insertDataAttribute(root: Node) {
-        const walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, null);
+export function processDom(root: Node = document.body) {
+    const walker = document.createTreeWalker(root, NodeFilter.SHOW_ELEMENT, null);
 
-        // Traverse the DOM
-        let node;
-        while ((node = walker.nextNode())) {
-            // Check if the node is an Element
-            if (node.nodeType === Node.ELEMENT_NODE) {
-                // Insert the data attribute
-                (node as HTMLElement).setAttribute(EditorAttributes.DATA_ONLOOK_UNIQUE_ID, uuid());
-            }
+    let node;
+    while ((node = walker.nextNode())) {
+        if (node.nodeType === Node.ELEMENT_NODE) {
+            (node as HTMLElement).setAttribute(EditorAttributes.DATA_ONLOOK_UNIQUE_ID, uuid());
         }
-        ipcRenderer.sendToHost(WebviewChannels.DOM_READY);
     }
-
-    // Usage example
-    insertDataAttribute(document.body);
+    ipcRenderer.sendToHost(WebviewChannels.DOM_READY);
 }
