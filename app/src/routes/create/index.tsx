516846fc16cc8e114a121743656ea278c9c0c749
Kiet Ho <hoakiet98@gmail.com>
2024-09-08 20:31:57 -0400

    Update text

--- app/src/routes/create/index.tsx
+++ app/src/routes/create/index.tsx
@@ -14,17 +14,16 @@ export default function CreateModal() {
                             <FileIcon className="w-4 h-4" />
                         </div>
                         <h3 className="text-sm font-medium text-text-active">
-                            {' '}
-                            Load existing project{' '}
+                            {'Load existing project'}
                         </h3>
-                        <p className="text-xs text-text">Work on your React UI</p>
+                        <p className="text-xs text-text">{'Work on your React UI'}</p>
                     </Card>
                     <Card className="border border-blue-800 bg-blue-900/50 hover:bg-blue-900 hover:cursor-pointer flex flex-col items-center justify-center space-y-2 p-6 transition">
                         <div className="rounded-full p-2 bg-blue-500">
                             <FilePlusIcon className="w-4 h-4" />
                         </div>
-                        <h3 className="text-sm font-medium"> New Onlook project </h3>
-                        <p className="text-xs text-blue-200"> Start a React App </p>
+                        <h3 className="text-sm font-medium"> {'New Onlook project'} </h3>
+                        <p className="text-xs text-blue-200"> {'Start a React App'} </p>
                     </Card>
                 </div>
             </DialogContent>
