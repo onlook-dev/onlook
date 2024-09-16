d88f13a87fd202b74dbea3e9f8e83bd7a0527c7b
Kiet Ho <hoakiet98@gmail.com>
2024-09-16 17:24:15 -0400

    More UI work

--- app/src/routes/projects/index.tsx
+++ app/src/routes/projects/index.tsx
@@ -1,27 +1,34 @@
+import { Button } from '@/components/ui/button';
+import { DotsVerticalIcon, Pencil2Icon } from '@radix-ui/react-icons';
+
 export default function Projects() {
     return (
-        <div className="flex h-screen w-screen relative overflow-hidden bg-black">
-            <div className="absolute top-0 w-full h-12 bg-white/10 flex justify-center items-center px-24">
-                <div className="bg-sky-800 h-full w-48"> </div>
-                <div className="bg-sky-600 mx-auto h-full w-48"> </div>
-                <div className="bg-sky-400 h-full w-48"> </div>
+        <div className="flex h-[calc(100vh-2.5rem)] w-full relative">
+            <div className="absolute top-0 w-full h-8 flex justify-center items-center px-32">
+                <div className="bg-bg h-full w-48"> </div>
+                <div className="bg-bg mx-auto h-full w-48"> </div>
+                <div className="bg-bg h-full w-48"> </div>
             </div>
-            <div className="w-3/5 h-full bg-neutral-800"> </div>
-            <div className="w-2/5 h-full flex flex-col justify-center items-start p-20 bg-black gap-4">
-                <p className="text-5xl text-white">Airbnb.com</p>
-                <div className="flex gap-7 text-white">
+            <div className="w-3/5 h-full"> </div>
+            <div className="w-2/5 h-full flex flex-col justify-center items-start p-20 gap-4 font-light">
+                <p className="text-text-active text-4xl ">Airbnb.com</p>
+                <div className="text-text flex gap-7 text-sm">
                     <p>Last edited 3 days ago </p>
                     <p className="w-36"> localhost: 3000 </p>
                 </div>
-                <div className="flex gap-7">
-                    <div className="w-44 h-12 bg-sky-100 flex justify-center items-center rounded-lg gap-2">
-                        <div className="w-7 h-8 bg-black"> </div>
+                <div className="flex gap-4">
+                    <Button
+                        size="default"
+                        variant={'outline'}
+                        className="gap-4 bg-bg-active border border-border-active"
+                    >
+                        <Pencil2Icon />
                         <p> Edit App </p>
-                    </div>
-                    <div className="w-44 h-12 flex items-center gap-2">
-                        <div className="w-10 h-11 bg-sky-100"> </div>
-                        <p className="text-white"> Project settings </p>
-                    </div>
+                    </Button>
+                    <Button size="default" variant={'ghost'} className="gap-4">
+                        <DotsVerticalIcon />
+                        <p> Project settings</p>
+                    </Button>
                 </div>
             </div>
         </div>
