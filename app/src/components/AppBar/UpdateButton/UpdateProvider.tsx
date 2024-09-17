85ff03c1419a40f5f0596a2334a3cc0c8c624d8e
Kiet Ho <hoakiet98@gmail.com>
2024-09-17 16:52:59 -0400

    Clean up

--- app/src/components/AppBar/UpdateButton/UpdateProvider.tsx
+++ app/src/components/AppBar/UpdateButton/UpdateProvider.tsx
@@ -1,11 +1,5 @@
 import { UpdateManager } from '@/lib/update';
-import { createContext, ReactNode, useContext } from 'react';
+import { createContext, useContext } from 'react';
 
 const UpdateContext = createContext(new UpdateManager());
 export const useUpdateManager = () => useContext(UpdateContext);
-
-const AuthProvider = ({ children }: { children: ReactNode }) => {
-    return <UpdateContext.Provider value={useUpdateManager()}>{children}</UpdateContext.Provider>;
-};
-
-export default AuthProvider;
