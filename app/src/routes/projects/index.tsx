0d73a6995afe71e07df0511cc50bfa06e8393af1
Kiet Ho <hoakiet98@gmail.com>
2024-09-17 22:57:04 -0400

    Make it easy

--- app/src/routes/projects/index.tsx
+++ app/src/routes/projects/index.tsx
@@ -4,7 +4,7 @@ import { SettingsTab } from './SettingsTab';
 import TopBar from './TopBar';
 
 export default function Projects() {
-    const [currentTab, setCurrentTab] = useState<'projects' | 'settings'>('projects');
+    const [currentTab, setCurrentTab] = useState<'projects' | 'settings'>('settings');
 
     return (
         <div className="h-12 w-full">
