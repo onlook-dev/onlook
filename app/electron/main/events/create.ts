1ea4be8a38d39a244c8dba1c32143d2f5a626e5c
Kiet Ho <hoakiet98@gmail.com>
2024-09-21 13:25:19 -0400

    Clean up

--- app/electron/main/events/create.ts
+++ app/electron/main/events/create.ts
@@ -5,9 +5,6 @@ import { MainChannels } from '/common/constants';
 
 export function listenForCreateMessages() {
     ipcMain.handle(MainChannels.CREATE_NEW_PROJECT, (e: Electron.IpcMainInvokeEvent, args) => {
-        console.log(
-            `[createProject] Received request to create project with args: ${JSON.stringify(args)}`,
-        );
         const progressCallback: ProgressCallback = (
             stage: ProjectCreationStage,
             message: string,
@@ -19,28 +16,6 @@ export function listenForCreateMessages() {
         };
 
         const { name, path } = args as { name: string; path: string };
-
-        // setTimeout(() => {
-        //     mainWindow?.webContents.send(MainChannels.CREATE_NEW_PROJECT_CALLBACK, {
-        //         stage: ProjectCreationStage.CLONING,
-        //         message: 'Cloning template...',
-        //     });
-        // }, 1000);
-
-        // setTimeout(() => {
-        //     mainWindow?.webContents.send(MainChannels.CREATE_NEW_PROJECT_CALLBACK, {
-        //         stage: ProjectCreationStage.INSTALLING,
-        //         message: 'Installing...',
-        //     });
-        // }, 3000);
-
-        // setTimeout(() => {
-        //     mainWindow?.webContents.send(MainChannels.CREATE_NEW_PROJECT_CALLBACK, {
-        //         stage: ProjectCreationStage.COMPLETE,
-        //         message: 'Complete!',
-        //     });
-        // }, 5000);
-        // return;
         return createProject(name, path, progressCallback);
     });
 }
