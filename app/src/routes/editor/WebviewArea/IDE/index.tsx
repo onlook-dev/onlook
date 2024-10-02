35d72d39a41bb26f69e67c9200f318ec6a337497
Kiet Ho <hoakiet98@gmail.com>
2024-10-01 22:56:37 -0400

    Saving code

--- app/src/routes/editor/WebviewArea/IDE/index.tsx
+++ app/src/routes/editor/WebviewArea/IDE/index.tsx
@@ -1,10 +1,13 @@
 import { useEditorEngine } from '@/components/Context';
 import { shikiToMonaco } from '@shikijs/monaco/index.mjs';
+import clsx from 'clsx';
 import { observer } from 'mobx-react-lite';
 import * as monaco from 'monaco-editor';
-import { useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { createHighlighter } from 'shiki';
 import './index.css';
+import { MainChannels } from '/common/constants';
+import { CodeDiff } from '/common/models/code';
 import { TemplateNode } from '/common/models/element/templateNode';
 
 export const CodeEditor = observer(() => {
@@ -12,10 +15,15 @@ export const CodeEditor = observer(() => {
     const editor = useRef<monaco.editor.IStandaloneCodeEditor | null>(null);
     const editorEngine = useEditorEngine();
     const decorationsCollection = useRef<monaco.editor.IEditorDecorationsCollection | null>(null);
-    const [path, setPath] = useState<string | null>(null);
+    const [filePath, setFilePath] = useState<string | null>(null);
+    const [hasChange, setHasChange] = useState<boolean>(false);
 
     useEffect(() => {
-        initMonaco();
+        try {
+            initMonaco();
+        } catch (e) {
+            console.error('Error initializing Monaco', e);
+        }
         return () => {
             if (editor.current) {
                 editor.current.dispose();
@@ -34,8 +42,50 @@ export const CodeEditor = observer(() => {
                 fontSize: 18,
             });
             decorationsCollection.current = editor.current.createDecorationsCollection();
+            editor.current.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () =>
+                saveCode(),
+            );
+            editor.current.onDidChangeModelContent(() => {
+                setHasChange(true);
+            });
+            console.log('Monaco initialized');
         }
     }
+
+    const saveCode = useCallback(async () => {
+        if (!filePath) {
+            console.error('Cannot save: No file path set');
+            return;
+        }
+
+        if (!editor.current) {
+            console.error('Cannot save: No editor instance');
+            return;
+        }
+
+        const code = editor.current.getValue();
+        const codeDiffs: CodeDiff[] = [{ path: filePath, original: '', generated: code }];
+        const res = await window.api.invoke(MainChannels.WRITE_CODE_BLOCKS, codeDiffs);
+        if (res) {
+            setHasChange(false);
+        }
+    }, [filePath]);
+
+    useEffect(() => {
+        if (editor.current) {
+            editor.current.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, saveCode);
+
+            const disposable = editor.current.onDidChangeModelContent(() => {
+                if (filePath) {
+                    setHasChange(true);
+                }
+            });
+
+            return () => {
+                disposable.dispose();
+            };
+        }
+    }, [filePath, saveCode]);
 
     async function initHighlighter() {
         const LANGS = ['javascript', 'typescript', 'jsx', 'tsx'];
@@ -53,7 +103,15 @@ export const CodeEditor = observer(() => {
     }
 
     useEffect(() => {
+        getSelectedCode();
+    }, [editorEngine.elements.selected]);
+
+    async function getSelectedCode() {
         if (!editorEngine.elements.selected.length) {
+            return;
+        }
+        if (!editor.current) {
+            console.error('Editor not initialized.');
             return;
         }
 
@@ -61,21 +119,25 @@ export const CodeEditor = observer(() => {
         const instance = editorEngine.ast.getInstance(selectedEl.selector);
         const root = editorEngine.ast.getRoot(selectedEl.selector);
         const templateNode = instance || root;
+
         if (!templateNode) {
+            console.error('No template node found.');
             return;
         }
-        editorEngine.code.getCodeFile(templateNode).then((code: string | null) => {
+
+        setFilePath(templateNode.path);
+        if (templateNode.path !== filePath) {
+            const code = await editorEngine.code.getCodeFile(templateNode);
             if (!code) {
-                console.error('No code found.');
+                console.error('No code returned for path.', templateNode.path);
                 return;
             }
-            if (editor.current) {
-                setPath(templateNode.path);
-                editor.current.setValue(code);
-                highlightAndScrollToCode(templateNode);
-            }
-        });
-    }, [editorEngine.elements.selected]);
+            console.log('Set file path', templateNode.path);
+            editor.current.setValue(code);
+            setHasChange(false);
+        }
+        highlightAndScrollToCode(templateNode);
+    }
 
     const highlightAndScrollToCode = (templateNode: TemplateNode) => {
         if (!editor.current || !decorationsCollection.current) {
@@ -110,8 +172,11 @@ export const CodeEditor = observer(() => {
 
     return (
         <div className="w-full h-[700px] mt-20">
-            <div className="my-2 border rounded-lg p-2 bg-bg text-sm">{path || ''}</div>
-            <div ref={editorContainer} className="w-full h-full" />
+            <div className="my-2 border rounded-lg p-2 bg-bg text-sm flex flex-row items-center">
+                <p>{filePath || ''}</p>
+                {hasChange && <div className="ml-auto bg-red rounded-full h-3 w-3"></div>}
+            </div>
+            <div ref={editorContainer} className={clsx('w-full h-full')} />
         </div>
     );
 });
