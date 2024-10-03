9dd818083e8163357d8106feed9c81ffad3e0cb5
Kiet Ho <hoakiet98@gmail.com>
2024-10-03 18:41:48 -0400

    Clean up

--- app/src/components/Context/index.tsx
+++ app/src/components/Context/index.tsx
@@ -2,8 +2,8 @@ import { AuthManager } from '@/lib/auth';
 import { EditorEngine } from '@/lib/editor/engine';
 import { ProjectsManager } from '@/lib/projects';
 import { RouteManager } from '@/lib/routes';
-import { createContext, useContext } from 'react';
 import { UpdateManager } from '@/lib/update';
+import { createContext, useContext } from 'react';
 
 const authManager = new AuthManager();
 const routeManager = new RouteManager();
@@ -15,10 +15,10 @@ const AuthContext = createContext(authManager);
 const RouteContext = createContext(routeManager);
 const ProjectsContext = createContext(projectsManager);
 const EditorEngineContext = createContext(editorEngine);
-const updateContext = createContext(updateManager);
+const UpdateContext = createContext(updateManager);
 
 export const useAuthManager = () => useContext(AuthContext);
 export const useRouteManager = () => useContext(RouteContext);
 export const useProjectsManager = () => useContext(ProjectsContext);
 export const useEditorEngine = () => useContext(EditorEngineContext);
-export const useUpdateManager = () => useContext(updateContext);
+export const useUpdateManager = () => useContext(UpdateContext);
