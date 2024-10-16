4cf9b6df40e69e0ee95170f348ec6e193ec500bd
Diana Bocakova <diana.bocakova@student.tuke.sk>
2024-10-16 17:30:46 +0200

    Add validation for rename input

--- app/src/routes/projects/ProjectsTab/Select/ProjectSettingsButton.tsx
+++ app/src/routes/projects/ProjectsTab/Select/ProjectSettingsButton.tsx
@@ -27,6 +27,7 @@ export default function ProjectSettingsButton({ project }: { project: Project })
     const [showDeleteDialog, setShowDeleteDialog] = React.useState(false);
     const [showRenameDialog, setShowRenameDialog] = React.useState(false);
     const [projectName, setProjectName] = React.useState(project.name);
+    const isProjectNameEmpty = React.useMemo(() => projectName.length === 0, [projectName]);
 
     const handleDeleteProject = () => {
         projectsManager.deleteProject(project);
@@ -111,17 +112,25 @@ export default function ProjectSettingsButton({ project }: { project: Project })
                     <div className="flex flex-col w-full gap-2">
                         <Label htmlFor="text">Project Name</Label>
                         <Input
+                            minLength={0}
                             type="text"
                             placeholder={getRandomPlaceholder()}
                             value={projectName || ''}
                             onInput={(e) => setProjectName(e.currentTarget.value)}
                         />
+                        {isProjectNameEmpty && (
+                            <p className="text-xs text-red-500">Project name can&apos;t be empty</p>
+                        )}
                     </div>
                     <AlertDialogFooter>
                         <Button variant={'ghost'} onClick={() => setShowRenameDialog(false)}>
                             Cancel
                         </Button>
-                        <Button className="rounded-md text-sm" onClick={handleRenameProject}>
+                        <Button
+                            disabled={isProjectNameEmpty}
+                            className="rounded-md text-sm"
+                            onClick={handleRenameProject}
+                        >
                             Rename
                         </Button>
                     </AlertDialogFooter>
