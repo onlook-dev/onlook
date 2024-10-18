19a311a04e9b03abca72592ead96d635a8bacb44
Kiet <31864905+Kitenite@users.noreply.github.com>
2024-10-18 18:11:55 -0400

    Use correct token for preset window (#604)

--- app/src/components/ui/popover.tsx
+++ app/src/components/ui/popover.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
 import * as PopoverPrimitive from '@radix-ui/react-popover';
+import * as React from 'react';
 
 import { cn } from '@/lib/utils';
 
@@ -28,4 +28,4 @@ const PopoverContent = React.forwardRef<
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
 
-export { Popover, PopoverTrigger, PopoverContent, PopoverAnchor };
+export { Popover, PopoverAnchor, PopoverContent, PopoverTrigger };
