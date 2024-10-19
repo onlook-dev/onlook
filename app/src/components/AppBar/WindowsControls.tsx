c97d901d68eb221eeabb84388892a6be9fca2992
abhiroopc84 <abhiroopc84@icloud.com>
2024-10-19 21:19:06 +0530

    Add unmaximize command

--- app/src/components/AppBar/WindowsControls.tsx
+++ app/src/components/AppBar/WindowsControls.tsx
@@ -1,15 +1,20 @@
-import { Cross1Icon, MinusIcon, SquareIcon } from '@radix-ui/react-icons';
+import { useState } from 'react';
 import { Button } from '../ui/button';
 import { MainChannels } from '/common/constants';
 import { WindowCommand } from '/common/models/project';
 
 export const WindowsControls = () => {
+    const [isMaximized, setIsMaximized] = useState(false);
+
     if (process.platform !== 'win32') {
         return null;
     }
 
     function sendCommand(command: WindowCommand) {
         window.api.invoke(MainChannels.SEND_WINDOW_COMMAND, command);
+        if (command === WindowCommand.MAXIMIZE || command === WindowCommand.UNMAXIMIZE) {
+            setIsMaximized(!isMaximized);
+        }
     }
 
     return (
@@ -23,7 +28,9 @@ export const WindowsControls = () => {
                 <MinusIcon className="h-3 w-3" />
             </Button>
             <Button
-                onClick={() => sendCommand(WindowCommand.MAXIMIZE)}
+                onClick={() =>
+                    sendCommand(isMaximized ? WindowCommand.UNMAXIMIZE : WindowCommand.MAXIMIZE)
+                }
                 variant={'ghost'}
                 className="hover:bg-background-onlook/30 hover:text-foreground outline-border"
                 aria-label="Maximize"
