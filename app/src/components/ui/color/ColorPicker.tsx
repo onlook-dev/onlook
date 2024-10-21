5e6738bfc0b3038971e2c1a47678c5acccc4e08a
Mostafa F <mostafa.foruji@gmail.com>
2024-10-21 13:05:32 +0330

    different input style depending on how many inputs

--- app/src/components/ui/color/ColorPicker.tsx
+++ app/src/components/ui/color/ColorPicker.tsx
@@ -8,16 +8,13 @@ import { mod } from '/common/helpers/math';
 import { DraftableInput } from './input';
 import styled from '@emotion/styled';
 import { checkPattern } from './checkPattern';
-
-const InputColumn: React.FC<JSX.IntrinsicElements['div']> = tw.div`
-  flex flex-col items-center gap-1 min-w-0
-`;
+import { InputGroup } from './input-group';
 
 const Input = tw(DraftableInput)`
   outline-0 w-full h-6 bg-background-onlook/70 rounded focus:ring-1 ring-inset ring-foreground-active text-foreground-primary placeholder:text-foreground-disabled text-center
 `;
 
-type SliderMode = 'rgb' | 'hsv';
+type SliderMode = 'hsv' | 'rgb' | 'hex';
 
 const InputsRow = ({
     color,
@@ -28,96 +25,138 @@ const InputsRow = ({
     onChangeEnd?: (color: Color) => void;
     onChange?: (color: Color) => void;
 }) => {
-    const [mode, setMode] = useState<SliderMode>('hsv');
+    const [mode, setMode] = useState<SliderMode>('hex');
 
     const rgbColor = color.rgb;
 
     return (
         <div className="z-50 grid grid-cols-[48px_1fr_1fr_1fr_46px] gap-1 text-mini">
-            <div className="flex flex-col items-center justify-center gap-1 min-w-0 ">
+            <div className="flex items-center justify-center gap-1 min-w-0 ">
                 <label
                     className="text-small text-foreground-primary cursor-pointer hover:text-foreground-hover bg-background-secondary border-[0.5px] border-foreground-tertiary/50 hover:bg-background-hover w-full flex rounded justify-center py-[0.5px]"
-                    onClick={() => (mode === 'hsv' ? setMode('rgb') : setMode('hsv'))}
+                    onClick={() =>
+                        mode === 'hsv'
+                            ? setMode('rgb')
+                            : mode === 'rgb'
+                              ? setMode('hex')
+                              : setMode('hsv')
+                    }
                 >
                     {mode.toUpperCase()}
                 </label>
             </div>
             {mode === 'hsv' ? (
-                <>
-                    <InputColumn>
-                        <Input
-                            value={Math.round(color.h * 360).toString()}
-                            onChangeValue={(hString) => {
-                                const h = mod(Number.parseInt(hString) / 360, 1);
-                                const newColor = new Color({ ...color, h });
-                                onChangeEnd?.(newColor);
-                                onChange?.(newColor);
-                                return true;
-                            }}
-                        />
-                    </InputColumn>
-                    {(['s', 'v'] as const).map((key) => {
-                        return (
-                            <InputColumn key={key}>
-                                <Input
-                                    value={Math.round(color[key] * 100).toString()}
-                                    onChangeValue={(valueString) => {
-                                        const value = mod(Number.parseInt(valueString) / 100, 1);
-                                        const newColor = new Color({ ...color, [key]: value });
-                                        onChangeEnd?.(newColor);
-                                        onChange?.(newColor);
-                                        return true;
-                                    }}
-                                />
-                            </InputColumn>
-                        );
-                    })}
-                </>
-            ) : (
-                <>
-                    {(['r', 'g', 'b'] as const).map((key) => {
-                        return (
-                            <InputColumn key={key}>
-                                <Input
-                                    value={Math.round(rgbColor[key] * 255).toString()}
-                                    onChangeValue={(valueString) => {
-                                        const value = mod(Number.parseInt(valueString) / 255, 1);
-                                        const newColor = Color.rgb({
-                                            ...rgbColor,
-                                            [key]: value,
-                                        });
-                                        onChangeEnd?.(newColor);
-                                        onChange?.(newColor);
-                                        return true;
-                                    }}
-                                />
-                            </InputColumn>
-                        );
-                    })}
-                </>
-            )}
-
-            <InputColumn>
-                <div className="relative w-full">
+                <InputGroup className="grid grid-cols-subgrid col-span-3 gap-[1px]">
                     <Input
-                        value={Math.round(color.a * 100).toString()}
-                        onChangeValue={(aString) => {
-                            const a = mod(Number.parseInt(aString.replace('%', '')) / 100, 1);
-                            const newColor = new Color({ ...color, a });
+                        value={Math.round(color.h * 360).toString()}
+                        onChangeValue={(hString) => {
+                            const h = mod(Number.parseInt(hString) / 360, 1);
+                            const newColor = new Color({ ...color, h });
                             onChangeEnd?.(newColor);
                             onChange?.(newColor);
                             return true;
                         }}
-                        className="pr-3"
                     />
-                    <span
-                        className="absolute right-[5px] top-1/2 transform -translate-y-1/2 text-foreground-tertiary"
-                        style={{ userSelect: 'none' }}
-                    >
-                        %
-                    </span>
-                </div>
-            </InputColumn>
+                    <Input
+                        value={Math.round(color['s'] * 100).toString()}
+                        onChangeValue={(valueString) => {
+                            const value = mod(Number.parseInt(valueString) / 100, 1);
+                            const newColor = new Color({ ...color, s: value });
+                            onChangeEnd?.(newColor);
+                            onChange?.(newColor);
+                            return true;
+                        }}
+                    />
+                    <Input
+                        value={Math.round(color['v'] * 100).toString()}
+                        onChangeValue={(valueString) => {
+                            const value = mod(Number.parseInt(valueString) / 100, 1);
+                            const newColor = new Color({ ...color, v: value });
+                            onChangeEnd?.(newColor);
+                            onChange?.(newColor);
+                            return true;
+                        }}
+                    />
+                </InputGroup>
+            ) : mode === 'rgb' ? (
+                <InputGroup className="grid grid-cols-subgrid col-span-3 gap-[1px]">
+                    <Input
+                        value={Math.round(rgbColor['r'] * 255).toString()}
+                        onChangeValue={(valueString) => {
+                            const value = mod(Number.parseInt(valueString) / 255, 1);
+                            const newColor = Color.rgb({
+                                ...rgbColor,
+                                r: value,
+                            });
+                            onChangeEnd?.(newColor);
+                            onChange?.(newColor);
+                            return true;
+                        }}
+                    />
+                    <Input
+                        value={Math.round(rgbColor['g'] * 255).toString()}
+                        onChangeValue={(valueString) => {
+                            const value = mod(Number.parseInt(valueString) / 255, 1);
+                            const newColor = Color.rgb({
+                                ...rgbColor,
+                                g: value,
+                            });
+                            onChangeEnd?.(newColor);
+                            onChange?.(newColor);
+                            return true;
+                        }}
+                    />
+                    <Input
+                        value={Math.round(rgbColor['b'] * 255).toString()}
+                        onChangeValue={(valueString) => {
+                            const value = mod(Number.parseInt(valueString) / 255, 1);
+                            const newColor = Color.rgb({
+                                ...rgbColor,
+                                b: value,
+                            });
+                            onChangeEnd?.(newColor);
+                            onChange?.(newColor);
+                            return true;
+                        }}
+                    />
+                </InputGroup>
+            ) : (
+                <InputGroup className="col-span-3">
+                    <Input
+                        value={color.toHex6()}
+                        onChangeValue={(hexString) => {
+                            const newColor = Color.from(
+                                hexString[0] === '#' ? hexString : '#' + hexString,
+                            );
+                            if (!newColor) {
+                                return false;
+                            }
+                            onChange?.(newColor);
+                            onChangeEnd?.(newColor);
+                            return true;
+                        }}
+                    />
+                </InputGroup>
+            )}
+            <div className="relative w-full">
+                <Input
+                    value={Math.round(color.a * 100).toString()}
+                    onChangeValue={(aString) => {
+                        const a = mod(Number.parseInt(aString.replace('%', '')) / 100, 1);
+                        const newColor = new Color({ ...color, a });
+                        onChangeEnd?.(newColor);
+                        onChange?.(newColor);
+                        return true;
+                    }}
+                    className="pr-3"
+                />
+                <span
+                    className="absolute right-[5px] top-1/2 transform -translate-y-1/2 text-foreground-tertiary"
+                    style={{ userSelect: 'none' }}
+                >
+                    %
+                </span>
+            </div>
         </div>
     );
 };
