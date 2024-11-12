6586876aebf9b886883974e5dd34bf5971258085
Kiet Ho <hoakiet98@gmail.com>
2024-11-12 11:36:14 -0500

    Clean up

--- apps/studio/src/routes/projects/ProjectsTab/index.tsx
+++ apps/studio/src/routes/projects/ProjectsTab/index.tsx
@@ -2,18 +2,18 @@ import { useProjectsManager } from '@/components/Context';
 import type { CreateMethod } from '@/routes/projects/helpers';
 import { observer } from 'mobx-react-lite';
 import { ChooseMethod } from './Create/ChooseMethod';
+import SelectProject from './Select';
 
 const ProjectsTab = observer(
     ({ setCreateMethod }: { setCreateMethod: (method: CreateMethod | null) => void }) => {
         const projectsManager = useProjectsManager();
         return (
-            <ChooseMethod setCreateMethod={setCreateMethod} />
-            // <>
-            //     {projectsManager.projects.length === 0 && (
-            //         <ChooseMethod setCreateMethod={setCreateMethod} />
-            //     )}
-            //     {projectsManager.projects.length > 0 && <SelectProject />}
-            // </>
+            <>
+                {projectsManager.projects.length === 0 && (
+                    <ChooseMethod setCreateMethod={setCreateMethod} />
+                )}
+                {projectsManager.projects.length > 0 && <SelectProject />}
+            </>
         );
     },
 );
