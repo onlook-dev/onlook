243b4547a220ef002d53dd73ceaee7a6b2b7fda5
Kiet <31864905+Kitenite@users.noreply.github.com>
2024-12-01 21:48:33 -0500

    Add terminal metrics (#836)

--- apps/studio/eslint.config.js
+++ apps/studio/eslint.config.js
@@ -31,6 +31,7 @@ export default [
             'react/prop-types': 'off',
             '@typescript-eslint/no-explicit-any': 'off',
             '@typescript-eslint/no-unused-vars': 'warn',
+            '@typescript-eslint/no-unused-expressions': 'warn',
         },
     },
 ];
