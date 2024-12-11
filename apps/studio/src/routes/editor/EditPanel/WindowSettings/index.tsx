9c50dd4ce2e8f696a25c84f320aed2c1d3d8c6b0
abhiroopc84 <abhiroopc84@icloud.com>
2024-12-11 12:33:17 +0530

    Add device settings

--- apps/studio/src/routes/editor/EditPanel/WindowSettings/index.tsx
+++ apps/studio/src/routes/editor/EditPanel/WindowSettings/index.tsx
@@ -1,8 +1,8 @@
 import { Button } from '@onlook/ui/button';
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@onlook/ui/select';
 import { Icons } from '@onlook/ui/icons/index';
 import { Separator } from '@onlook/ui/separator';
-import { Input } from '@onlook/ui/input';
+import FrameDimensions from './frameDimensions';
+import DeviceSettings from './DeviceSettings';
 
 const WindowSettings = ({ setIsOpen }: { setIsOpen: (isOpen: boolean) => void }) => {
     return (
@@ -38,102 +38,9 @@ const WindowSettings = ({ setIsOpen }: { setIsOpen: (isOpen: boolean) => void })
                         <span className="text-smallPlus">Delete</span>
                     </Button>
                 </div>
-                <div className="flex flex-col gap-2">
-                    <p className="text-smallPlus text-foreground-primary">Frame Dimensions</p>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Device</span>
-                        <Select>
-                            <SelectTrigger className="w-3/5 bg-background-secondary border-background-secondary py-1 px-2 h-fit">
-                                <SelectValue
-                                    placeholder="Theme"
-                                    defaultValue={'iPhone SE'}
-                                    className="text-small"
-                                />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="iPhone SE">iPhone SE</SelectItem>
-                                <SelectItem value="dark">Dark</SelectItem>
-                                <SelectItem value="system">System</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Orientation</span>
-                        <div className="flex flex-row p-0.5 w-3/5 bg-background-secondary rounded-md">
-                            <Button
-                                size={'icon'}
-                                className="h-full w-full px-0.5 py-1 bg-background-tertiary rounded-md"
-                                variant={'ghost'}
-                            >
-                                <Icons.Potrait className="h-4 w-4" />
-                            </Button>
-                            <Button
-                                size={'icon'}
-                                className="h-full w-full px-0.5 py-1 bg-background-tertiary rounded-md"
-                                variant={'ghost'}
-                            >
-                                <Icons.Landscape className="h-4 w-4" />
-                            </Button>
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Width</span>
-                        <div className="relative w-3/5">
-                            <Input className="bg-background-secondary border-background-secondary py-1.5 h-fit" />
-                            <Icons.Link className="absolute right-2 top-1/2 transform -translate-y-1/2" />
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Height</span>
-                        <div className="relative w-3/5">
-                            <Input className="bg-background-secondary border-background-secondary py-1.5 h-fit" />
-                            <Icons.Link className="absolute right-2 top-1/2 transform -translate-y-1/2" />
-                        </div>
-                    </div>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Responsive</span>
-                        <Select>
-                            <SelectTrigger className="w-3/5 bg-background-secondary border-background-secondary py-1 px-2 h-fit">
-                                <SelectValue placeholder="Theme" defaultValue={'Closest Size'} />
-                            </SelectTrigger>
-                            <SelectContent>
-                                <SelectItem value="Closest Size">Closest Size</SelectItem>
-                                <SelectItem value="dark">Dark</SelectItem>
-                                <SelectItem value="system">System</SelectItem>
-                            </SelectContent>
-                        </Select>
-                    </div>
-                </div>
+                <FrameDimensions />
                 <Separator />
-                <div className="flex flex-col gap-2">
-                    <p className="text-smallPlus text-foreground-primary">Device Settings</p>
-                    <div className="flex flex-row justify-between items-center">
-                        <span className="text-small text-foreground-secondary">Theme</span>
-                        <div className="flex flex-row p-0.5 w-3/5 bg-background-secondary rounded-md">
-                            <Button
-                                size={'icon'}
-                                className="h-full w-full px-0.5 py-1 bg-background-tertiary"
-                                variant={'ghost'}
-                            >
-                                <Icons.Moon className="h-5 w-5" />
-                            </Button>
-                            <Button
-                                size={'icon'}
-                                className="h-full w-full px-0.5 py-1 bg-background-tertiary"
-                                variant={'ghost'}
-                            >
-                                <Icons.Landscape className="h-5 w-5" />
-                            </Button>
-                            <Button
-                                size={'icon'}
-                                className="h-full w-full px-0.5 py-1 bg-background-tertiary"
-                                variant={'ghost'}
-                            >
-                                <Icons.Landscape className="h-5 w-5" />
-                            </Button>
-                        </div>
-                    </div>
-                </div>
+                <DeviceSettings />
             </div>
         </div>
     );
