991b4dea00f768b3f27fc2deabf13729a9c09f92
Kiet Ho <hoakiet98@gmail.com>
2025-01-05 16:43:26 -0800

    Clean up

--- apps/studio/electron/main/events/hosting.ts
+++ apps/studio/electron/main/events/hosting.ts
@@ -4,19 +4,19 @@ import hostingManager from '../hosting';
 
 export function listenForHostingMessages() {
     ipcMain.handle(MainChannels.CREATE_HOSTING_ENV, (e: Electron.IpcMainInvokeEvent, args) => {
-        console.log('createEnv', args);
+        return hostingManager.createEnv(args);
     });
 
     ipcMain.handle(MainChannels.GET_HOSTING_ENV, (e: Electron.IpcMainInvokeEvent, args) => {
-        console.log('getEnv', args);
+        return hostingManager.getEnv(args.envId);
     });
 
     ipcMain.handle(MainChannels.DEPLOY_VERSION, (e: Electron.IpcMainInvokeEvent, args) => {
         const { envId, folderPath, buildScript } = args;
-        return hostingManager.publishEnv(folderPath, buildScript);
+        return hostingManager.publishEnv(envId, folderPath, buildScript);
     });
 
     ipcMain.handle(MainChannels.DELETE_HOSTING_ENV, (e: Electron.IpcMainInvokeEvent, args) => {
-        console.log('deleteEnv', args);
+        return hostingManager.deleteEnv(args.envId);
     });
 }
