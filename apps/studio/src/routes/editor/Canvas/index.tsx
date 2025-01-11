4cf1cd134213915ee03fcad55cc881aa069e8939
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-01-11 15:09:25 -0800

    Fix small element selection (#1019)

--- apps/studio/src/routes/editor/Canvas/index.tsx
+++ apps/studio/src/routes/editor/Canvas/index.tsx
@@ -104,7 +104,7 @@ const Canvas = observer(
             );
         };
 
-        const handleCanvasClicked = (event: React.MouseEvent<HTMLDivElement>) => {
+        const handleCanvasMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
             if (event.target !== containerRef.current) {
                 return;
             }
@@ -149,7 +149,7 @@ const Canvas = observer(
                 <div
                     ref={containerRef}
                     className="overflow-hidden bg-background-onlook flex flex-grow relative"
-                    onClick={handleCanvasClicked}
+                    onMouseDown={handleCanvasMouseDown}
                 >
                     <Overlay>
                         <div
