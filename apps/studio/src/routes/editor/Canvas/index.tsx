1b4e0d823909bc6117d5cd7a26a9d0efc9464ba3
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-01-11 18:34:39 -0800

    Fix zooming issue (#1022)

--- apps/studio/src/routes/editor/Canvas/index.tsx
+++ apps/studio/src/routes/editor/Canvas/index.tsx
@@ -7,172 +7,158 @@ import HotkeysArea from './Hotkeys';
 import Overlay from './Overlay';
 import PanOverlay from './PanOverlay';
 
-const Canvas = observer(
-    ({
-        children,
-        scale,
-        position,
-        onPositionChange,
-        onScaleChange,
-    }: {
-        children: ReactNode;
-        scale: number;
-        position: { x: number; y: number };
-        onPositionChange: (position: any) => void;
-        onScaleChange: (scale: number) => void;
-    }) => {
-        const ZOOM_SENSITIVITY = 0.006;
-        const PAN_SENSITIVITY = 0.52;
-        const MIN_ZOOM = 0.1;
-        const MAX_ZOOM = 3;
-        const MAX_X = 10000;
-        const MAX_Y = 10000;
-        const MIN_X = -5000;
-        const MIN_Y = -5000;
+const Canvas = observer(({ children }: { children: ReactNode }) => {
+    const ZOOM_SENSITIVITY = 0.006;
+    const PAN_SENSITIVITY = 0.52;
+    const MIN_ZOOM = 0.1;
+    const MAX_ZOOM = 3;
+    const MAX_X = 10000;
+    const MAX_Y = 10000;
+    const MIN_X = -5000;
+    const MIN_Y = -5000;
 
-        const editorEngine = useEditorEngine();
-        const containerRef = useRef<HTMLDivElement>(null);
-        const [isPanning, setIsPanning] = useState(false);
+    const editorEngine = useEditorEngine();
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [isPanning, setIsPanning] = useState(false);
+    const scale = editorEngine.canvas.scale;
+    const position = editorEngine.canvas.position;
 
-        const handleWheel = (event: WheelEvent) => {
-            if (event.ctrlKey || event.metaKey) {
-                handleZoom(event);
-            } else {
-                handlePan(event);
-            }
-        };
-
-        const handleZoom = (event: WheelEvent) => {
-            if (!containerRef.current) {
-                return;
-            }
-            event.preventDefault();
-            const zoomFactor = -event.deltaY * ZOOM_SENSITIVITY;
-            const rect = containerRef.current.getBoundingClientRect();
-            const x = event.clientX - rect.left;
-            const y = event.clientY - rect.top;
-
-            const newScale = scale * (1 + zoomFactor);
-            const lintedScale = clampZoom(newScale);
-
-            const deltaX = (x - position.x) * zoomFactor;
-            const deltaY = (y - position.y) * zoomFactor;
-
-            onScaleChange(lintedScale);
-
-            if (newScale < MIN_ZOOM || newScale > MAX_ZOOM) {
-                return;
-            }
-            onPositionChange((prevPosition: { x: number; y: number }) =>
-                clampPosition(
-                    {
-                        x: prevPosition.x - deltaX,
-                        y: prevPosition.y - deltaY,
-                    },
-                    scale,
-                ),
-            );
-        };
-
-        function clampZoom(scale: number) {
-            return Math.min(Math.max(scale, MIN_ZOOM), MAX_ZOOM);
+    const handleWheel = (event: WheelEvent) => {
+        if (event.ctrlKey || event.metaKey) {
+            handleZoom(event);
+        } else {
+            handlePan(event);
         }
+    };
 
-        function clampPosition(position: { x: number; y: number }, scale: number) {
-            const effectiveMaxX = MAX_X * scale;
-            const effectiveMaxY = MAX_Y * scale;
-            const effectiveMinX = MIN_X * scale;
-            const effectiveMinY = MIN_Y * scale;
+    const handleZoom = (event: WheelEvent) => {
+        if (!containerRef.current) {
+            return;
+        }
+        event.preventDefault();
+        const zoomFactor = -event.deltaY * ZOOM_SENSITIVITY;
+        const rect = containerRef.current.getBoundingClientRect();
+        const x = event.clientX - rect.left;
+        const y = event.clientY - rect.top;
 
-            return {
-                x: Math.min(Math.max(position.x, effectiveMinX), effectiveMaxX),
-                y: Math.min(Math.max(position.y, effectiveMinY), effectiveMaxY),
+        const newScale = scale * (1 + zoomFactor);
+        const lintedScale = clampZoom(newScale);
+
+        const deltaX = (x - position.x) * zoomFactor;
+        const deltaY = (y - position.y) * zoomFactor;
+
+        editorEngine.canvas.scale = lintedScale;
+
+        if (newScale < MIN_ZOOM || newScale > MAX_ZOOM) {
+            return;
+        }
+        const newPosition = clampPosition(
+            {
+                x: position.x - deltaX,
+                y: position.y - deltaY,
+            },
+            lintedScale,
+        );
+        editorEngine.canvas.position = newPosition;
+    };
+
+    function clampZoom(scale: number) {
+        return Math.min(Math.max(scale, MIN_ZOOM), MAX_ZOOM);
+    }
+
+    function clampPosition(position: { x: number; y: number }, scale: number) {
+        const effectiveMaxX = MAX_X * scale;
+        const effectiveMaxY = MAX_Y * scale;
+        const effectiveMinX = MIN_X * scale;
+        const effectiveMinY = MIN_Y * scale;
+
+        return {
+            x: Math.min(Math.max(position.x, effectiveMinX), effectiveMaxX),
+            y: Math.min(Math.max(position.y, effectiveMinY), effectiveMaxY),
+        };
+    }
+
+    const handlePan = (event: WheelEvent) => {
+        const deltaX = (event.deltaX + (event.shiftKey ? event.deltaY : 0)) * PAN_SENSITIVITY;
+        const deltaY = (event.shiftKey ? 0 : event.deltaY) * PAN_SENSITIVITY;
+
+        const newPosition = clampPosition(
+            {
+                x: position.x - deltaX,
+                y: position.y - deltaY,
+            },
+            scale,
+        );
+        editorEngine.canvas.position = newPosition;
+    };
+
+    const handleCanvasClicked = (event: React.MouseEvent<HTMLDivElement>) => {
+        if (event.target !== containerRef.current) {
+            return;
+        }
+        editorEngine.webviews.deselectAll();
+        editorEngine.clear();
+    };
+
+    useEffect(() => {
+        const div = containerRef.current;
+        if (div) {
+            div.addEventListener('wheel', handleWheel, { passive: false });
+            div.addEventListener('mousedown', middleMouseButtonDown);
+            div.addEventListener('mouseup', middleMouseButtonUp);
+            return () => {
+                div.removeEventListener('wheel', handleWheel);
+                div.removeEventListener('mousedown', middleMouseButtonDown);
+                div.removeEventListener('mouseup', middleMouseButtonUp);
             };
         }
+    }, [handleWheel]);
 
-        const handlePan = (event: WheelEvent) => {
-            const deltaX = (event.deltaX + (event.shiftKey ? event.deltaY : 0)) * PAN_SENSITIVITY;
-            const deltaY = (event.shiftKey ? 0 : event.deltaY) * PAN_SENSITIVITY;
-            onPositionChange((prevPosition: { x: number; y: number }) =>
-                clampPosition(
-                    {
-                        x: prevPosition.x - deltaX,
-                        y: prevPosition.y - deltaY,
-                    },
-                    scale,
-                ),
-            );
-        };
+    const middleMouseButtonDown = (e: MouseEvent) => {
+        if (e.button === 1) {
+            editorEngine.mode = EditorMode.PAN;
+            setIsPanning(true);
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    };
 
-        const handleCanvasMouseDown = (event: React.MouseEvent<HTMLDivElement>) => {
-            if (event.target !== containerRef.current) {
-                return;
-            }
-            editorEngine.webviews.deselectAll();
-            editorEngine.clear();
-        };
+    const middleMouseButtonUp = (e: MouseEvent) => {
+        if (e.button === 1) {
+            editorEngine.mode = EditorMode.DESIGN;
+            setIsPanning(false);
+            e.preventDefault();
+            e.stopPropagation();
+        }
+    };
 
-        useEffect(() => {
-            const div = containerRef.current;
-            if (div) {
-                div.addEventListener('wheel', handleWheel, { passive: false });
-                div.addEventListener('mousedown', middleMouseButtonDown);
-                div.addEventListener('mouseup', middleMouseButtonUp);
-                return () => {
-                    div.removeEventListener('wheel', handleWheel);
-                    div.removeEventListener('mousedown', middleMouseButtonDown);
-                    div.removeEventListener('mouseup', middleMouseButtonUp);
-                };
-            }
-        }, [handleWheel]);
-
-        const middleMouseButtonDown = (e: MouseEvent) => {
-            if (e.button === 1) {
-                editorEngine.mode = EditorMode.PAN;
-                setIsPanning(true);
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        };
-
-        const middleMouseButtonUp = (e: MouseEvent) => {
-            if (e.button === 1) {
-                editorEngine.mode = EditorMode.DESIGN;
-                setIsPanning(false);
-                e.preventDefault();
-                e.stopPropagation();
-            }
-        };
-
-        return (
-            <HotkeysArea scale={scale} setScale={onScaleChange} setPosition={onPositionChange}>
-                <div
-                    ref={containerRef}
-                    className="overflow-hidden bg-background-onlook flex flex-grow relative"
-                    onMouseDown={handleCanvasMouseDown}
-                >
-                    <Overlay>
-                        <div
-                            id={EditorAttributes.CANVAS_CONTAINER_ID}
-                            style={{
-                                transition: 'transform ease',
-                                transform: `translate(${position.x}px, ${position.y}px) scale(${scale})`,
-                                transformOrigin: '0 0',
-                            }}
-                        >
-                            {children}
-                        </div>
-                    </Overlay>
-                    <PanOverlay
-                        setPosition={onPositionChange}
-                        clampPosition={(position) => clampPosition(position, scale)}
-                        isPanning={isPanning}
-                        setIsPanning={setIsPanning}
-                    />
-                </div>
-            </HotkeysArea>
-        );
-    },
-);
+    return (
+        <HotkeysArea>
+            <div
+                ref={containerRef}
+                className="overflow-hidden bg-background-onlook flex flex-grow relative"
+                onClick={handleCanvasClicked}
+            >
+                <Overlay>
+                    <div
+                        id={EditorAttributes.CANVAS_CONTAINER_ID}
+                        style={{
+                            transition: 'transform ease',
+                            transform: `translate(${position.x}px, ${position.y}px) scale(${scale})`,
+                            transformOrigin: '0 0',
+                        }}
+                    >
+                        {children}
+                    </div>
+                </Overlay>
+                <PanOverlay
+                    clampPosition={(position) => clampPosition(position, scale)}
+                    isPanning={isPanning}
+                    setIsPanning={setIsPanning}
+                />
+            </div>
+        </HotkeysArea>
+    );
+});
 
 export default Canvas;
--- apps/studio/src/routes/editor/TopBar/ZoomControls/index.tsx
+++ apps/studio/src/routes/editor/TopBar/ZoomControls/index.tsx
@@ -1,147 +1,133 @@
+import { useEditorEngine } from '@/components/Context';
 import { HotKeyLabel } from '@/components/ui/hotkeys-label';
 import { DefaultSettings, EditorAttributes } from '@onlook/models/constants';
 import { Input } from '@onlook/ui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '@onlook/ui/popover';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 import { observer } from 'mobx-react-lite';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Hotkey } from '/common/hotkeys';
 
-const ZoomControls = observer(
-    ({
-        scale,
-        handlePosition,
-        handleScale,
-    }: {
-        scale: number;
-        handlePosition: (position: any) => void;
-        handleScale: (scale: number) => void;
-    }) => {
-        const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-        const ZOOM_SENSITIVITY = 0.5;
-        const MIN_ZOOM = 0.1;
-        const MAX_ZOOM = 3;
-        const [inputValue, setInputValue] = useState(`${Math.round(scale * 100)}%`);
+const ZoomControls = observer(() => {
+    const editorEngine = useEditorEngine();
+    const scale = editorEngine.canvas.scale;
 
-        useEffect(() => {
-            setInputValue(`${Math.round(scale * 100)}%`);
-        }, [scale]);
+    const [inputValue, setInputValue] = useState(`${Math.round(scale * 100)}%`);
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-        const handleZoom = (factor: number) => {
-            const container = document.getElementById(EditorAttributes.CANVAS_CONTAINER_ID);
-            if (container == null) {
-                return;
-            }
+    const ZOOM_SENSITIVITY = 0.5;
+    const MIN_ZOOM = 0.1;
+    const MAX_ZOOM = 3;
 
-            const zoomFactor = factor * ZOOM_SENSITIVITY;
-            const newScale = scale * (1 + zoomFactor);
-            const lintedScale = clampZoom(newScale);
-            //Zoom in/out
-            handleScale(lintedScale);
-            setInputValue(`${Math.round(lintedScale * 100)}%`);
-        };
-
-        function clampZoom(scale: number) {
-            return Math.min(Math.max(scale, MIN_ZOOM), MAX_ZOOM);
+    const handleZoom = (factor: number) => {
+        const container = document.getElementById(EditorAttributes.CANVAS_CONTAINER_ID);
+        if (container == null) {
+            return;
         }
 
-        const handleZoomToFit = () => {
-            const container = document.getElementById(EditorAttributes.CANVAS_CONTAINER_ID);
-            const content = container?.firstElementChild as HTMLElement;
-            if (container && content) {
-                const contentRect = content.getBoundingClientRect();
-                const containerRect = container.getBoundingClientRect();
-                const scaleX = containerRect.width / contentRect.width;
-                const scaleY = containerRect.height / contentRect.height;
-                const newScale = Math.min(scaleX, scaleY) * 0.9;
-                //Zoom fit
-                handleScale(newScale);
-                setInputValue(`${Math.round(newScale * 100)}%`);
-                //Position fit
-                const newPosition = {
-                    x: DefaultSettings.POSITION.x,
-                    y: DefaultSettings.POSITION.y,
-                };
-                handlePosition(newPosition);
-            }
-        };
+        const zoomFactor = factor * ZOOM_SENSITIVITY;
+        const newScale = scale * (1 + zoomFactor);
+        const lintedScale = clampZoom(newScale);
+        editorEngine.canvas.scale = lintedScale;
+    };
 
-        const handleCustomZoom = (value: string) => {
-            value = value.trim();
-            const isZoom = /^[0-9]+%?$/.test(value);
-            if (isZoom) {
-                const numericValue = parseInt(value.replace('%', ''));
-                if (!isNaN(numericValue)) {
-                    const newScale = numericValue / 100;
-                    const clampedScale = clampZoom(newScale);
-                    handleScale(clampedScale);
-                    setInputValue(`${Math.round(clampedScale * 100)}%`);
-                }
-            }
-        };
+    function clampZoom(scale: number) {
+        return Math.min(Math.max(scale, MIN_ZOOM), MAX_ZOOM);
+    }
 
-        return (
-            <div className="mx-2 flex flex-row items-center text-mini text-foreground-onlook hover:text-foreground-active transition-all duration-300 ease-in-out h-full p-1">
-                <Popover open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
-                    <PopoverTrigger className="group flex items-center px-2 py-1 rounded hover:bg-accent data-[state=open]:text-foreground-active">
-                        <span>{Math.round(scale * 100)}%</span>
-                        <ChevronDownIcon className="ml-1 h-4 w-4 transition-transform group-data-[state=open]:-rotate-180 duration-200 ease-in-out" />
-                    </PopoverTrigger>
-                    <PopoverContent className="flex flex-col p-1.5 bg-background/85 backdrop-blur-md w-42 min-w-42">
-                        <Input
-                            value={inputValue}
-                            onChange={(e) => setInputValue(e.target.value)}
-                            onKeyDown={(e: { key: string }) => {
-                                if (e.key === 'Enter') {
-                                    handleCustomZoom(inputValue);
-                                }
-                            }}
-                            className={`p-1 h-6 text-left text-smallPlus rounded border mb-1 focus-visible:border-red-500`}
-                            autoFocus
+    const handleZoomToFit = () => {
+        const container = document.getElementById(EditorAttributes.CANVAS_CONTAINER_ID);
+        const content = container?.firstElementChild as HTMLElement;
+        if (container && content) {
+            const contentRect = content.getBoundingClientRect();
+            const containerRect = container.getBoundingClientRect();
+            const scaleX = containerRect.width / contentRect.width;
+            const scaleY = containerRect.height / contentRect.height;
+            const newScale = Math.min(scaleX, scaleY) * 0.9;
+            editorEngine.canvas.scale = newScale;
+            //Position fit
+            const newPosition = {
+                x: DefaultSettings.POSITION.x,
+                y: DefaultSettings.POSITION.y,
+            };
+            editorEngine.canvas.position = newPosition;
+        }
+    };
+
+    const handleCustomZoom = (value: string) => {
+        value = value.trim();
+        const isZoom = /^[0-9]+%?$/.test(value);
+        if (isZoom) {
+            const numericValue = parseInt(value.replace('%', ''));
+            if (!isNaN(numericValue)) {
+                const newScale = numericValue / 100;
+                const clampedScale = clampZoom(newScale);
+                editorEngine.canvas.scale = clampedScale;
+            }
+        }
+    };
+
+    return (
+        <div className="mx-2 flex flex-row items-center text-mini text-foreground-onlook hover:text-foreground-active transition-all duration-300 ease-in-out h-full p-1">
+            <Popover open={isDropdownOpen} onOpenChange={setIsDropdownOpen}>
+                <PopoverTrigger className="group flex items-center px-2 py-1 rounded hover:bg-accent data-[state=open]:text-foreground-active">
+                    <span>{Math.round(scale * 100)}%</span>
+                    <ChevronDownIcon className="ml-1 h-4 w-4 transition-transform group-data-[state=open]:-rotate-180 duration-200 ease-in-out" />
+                </PopoverTrigger>
+                <PopoverContent className="flex flex-col p-1.5 bg-background/85 backdrop-blur-md w-42 min-w-42">
+                    <Input
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)}
+                        onKeyDown={(e: { key: string }) => {
+                            if (e.key === 'Enter') {
+                                handleCustomZoom(inputValue);
+                            }
+                        }}
+                        className={`p-1 h-6 text-left text-smallPlus rounded border mb-1 focus-visible:border-red-500`}
+                        autoFocus
+                    />
+                    <button
+                        onClick={() => handleZoom(1)}
+                        className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
+                    >
+                        <HotKeyLabel
+                            className="w-full justify-between text-mini"
+                            hotkey={Hotkey.ZOOM_IN}
                         />
-                        <button
-                            onClick={() => handleZoom(1)}
-                            className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
-                        >
-                            <HotKeyLabel
-                                className="w-full justify-between text-mini"
-                                hotkey={Hotkey.ZOOM_IN}
-                            />
-                        </button>
-                        <button
-                            onClick={() => handleZoom(-1)}
-                            className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
-                        >
-                            <HotKeyLabel
-                                className="w-full justify-between text-mini"
-                                hotkey={Hotkey.ZOOM_OUT}
-                            />
-                        </button>
-                        <button
-                            onClick={handleZoomToFit}
-                            className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
-                        >
-                            <HotKeyLabel
-                                className="w-full justify-between text-mini"
-                                hotkey={Hotkey.ZOOM_FIT}
-                            />
-                        </button>
-                        <button
-                            onClick={() => handleScale(1)}
-                            className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
-                        >
-                            <span className="flex-grow text-mini">Zoom 100%</span>
-                        </button>
-                        <button
-                            onClick={() => handleScale(2)}
-                            className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
-                        >
-                            <span className="flex-grow text-mini">Zoom 200%</span>
-                        </button>
-                    </PopoverContent>
-                </Popover>
-            </div>
-        );
-    },
-);
+                    </button>
+                    <button
+                        onClick={() => handleZoom(-1)}
+                        className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
+                    >
+                        <HotKeyLabel
+                            className="w-full justify-between text-mini"
+                            hotkey={Hotkey.ZOOM_OUT}
+                        />
+                    </button>
+                    <button
+                        onClick={handleZoomToFit}
+                        className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
+                    >
+                        <HotKeyLabel
+                            className="w-full justify-between text-mini"
+                            hotkey={Hotkey.ZOOM_FIT}
+                        />
+                    </button>
+                    <button
+                        onClick={() => (editorEngine.canvas.scale = 1)}
+                        className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
+                    >
+                        <span className="flex-grow text-mini">Zoom 100%</span>
+                    </button>
+                    <button
+                        onClick={() => (editorEngine.canvas.scale = 2)}
+                        className="w-full text-left px-2 py-1.5 rounded hover:bg-accent"
+                    >
+                        <span className="flex-grow text-mini">Zoom 200%</span>
+                    </button>
+                </PopoverContent>
+            </Popover>
+        </div>
+    );
+});
 export default ZoomControls;
