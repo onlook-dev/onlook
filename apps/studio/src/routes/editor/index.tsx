c8aeb56eef9fbe8371791fa9e80fdae2d4b51806
Kiet Ho <hoakiet98@gmail.com>
2025-01-11 17:49:53 -0800

    Update motion lib

--- apps/studio/src/routes/editor/index.tsx
+++ apps/studio/src/routes/editor/index.tsx
@@ -1,4 +1,3 @@
-import { useEditorEngine } from '@/components/Context';
 import Canvas from './Canvas';
 import EditPanel from './EditPanel';
 import LayersPanel from './LayersPanel';
@@ -7,13 +6,7 @@ import Toolbar from './Toolbar';
 import EditorTopBar from './TopBar';
 import WebviewArea from './WebviewArea';
 
-export const minDimensions = { width: '280px', height: '360px' };
-
 function ProjectEditor() {
-    const MIN_ZOOM = 0.1;
-    const MAX_ZOOM = 3;
-    const editorEngine = useEditorEngine();
-
     return (
         <>
             <div className="relative flex flex-row h-[calc(100vh-2.5rem)] select-none">
