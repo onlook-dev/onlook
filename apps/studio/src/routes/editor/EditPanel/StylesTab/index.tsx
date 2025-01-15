24282a8cbe91d6ca21b516f4bba7a1aa6856404d
Kiet Ho <hoakiet98@gmail.com>
2025-01-15 13:29:23 -0800

    More memoization

--- apps/studio/src/routes/editor/EditPanel/StylesTab/index.tsx
+++ apps/studio/src/routes/editor/EditPanel/StylesTab/index.tsx
@@ -14,7 +14,7 @@ import { Icons } from '@onlook/ui/icons/index';
 import { Tooltip, TooltipContent, TooltipPortal, TooltipTrigger } from '@onlook/ui/tooltip';
 import { cn } from '@onlook/ui/utils';
 import { observer } from 'mobx-react-lite';
-import { memo, useCallback, useMemo } from 'react';
+import { memo, useMemo } from 'react';
 import BorderInput from './compound/BorderInput';
 import DisplayInput from './compound/DisplayInput';
 import NestedInputs from './compound/NestedInputs';
@@ -33,9 +33,7 @@ export const STYLE_GROUP_MAPPING: Record<StyleGroupKey, BaseStyle[]> = {
     [StyleGroupKey.Text]: TextGroup,
 };
 
-const TAILWIND_KEY = 'tw';
-
-const SingleInputRenderer = memo(({ style }: { style: SingleStyle }) => {
+const SingleInput = memo(({ style }: { style: SingleStyle }) => {
     if (style.type === StyleType.Select) {
         return <SelectInput elementStyle={style} />;
     } else if (style.type === StyleType.Dimensions) {
@@ -46,16 +44,30 @@ const SingleInputRenderer = memo(({ style }: { style: SingleStyle }) => {
         return <NumberUnitInput elementStyle={style} />;
     } else if (style.type === StyleType.Text) {
         return <TextInput elementStyle={style} />;
-    } else {
-        return (
-            <div className="flex flex-row items-center">
-                <p>Unknown style: {style.key}</p>
-            </div>
-        );
     }
+    return (
+        <div className="flex flex-row items-center">
+            <p>Unknown style: {style.key}</p>
+        </div>
+    );
 });
+SingleInput.displayName = 'SingleInput';
 
-const CompoundRenderer = memo(({ style }: { style: CompoundStyleImpl }) => {
+const SingleStyle = memo(({ style }: { style: SingleStyle }) => {
+    return (
+        <div className="flex flex-row items-center mt-2">
+            <p className="text-xs w-24 mr-2 text-start text-foreground-onlook">
+                {style.displayName}
+            </p>
+            <div className="text-end ml-auto">
+                <SingleInput style={style} />
+            </div>
+        </div>
+    );
+});
+SingleStyle.displayName = 'SingleStyle';
+
+const CompoundStyle = memo(({ style }: { style: CompoundStyleImpl }) => {
     if (
         [CompoundStyleKey.Margin, CompoundStyleKey.Padding, CompoundStyleKey.Corners].includes(
             style.key,
@@ -66,95 +78,105 @@ const CompoundRenderer = memo(({ style }: { style: CompoundStyleImpl }) => {
         return <DisplayInput compoundStyle={style} />;
     } else if (style.key === CompoundStyleKey.Border) {
         return <BorderInput compoundStyle={style} />;
-    } else {
-        return (
-            <div className="flex flex-row items-center">
-                <p>Unknown compound style: {style.key}</p>
-            </div>
-        );
     }
+    return (
+        <div className="flex flex-row items-center">
+            <p>Unknown compound style: {style.key}</p>
+        </div>
+    );
 });
+CompoundStyle.displayName = 'CompoundStyle';
+
+const StyleGroupComponent = memo(({ baseElementStyles }: { baseElementStyles: BaseStyle[] }) => {
+    return (
+        <>
+            {Object.entries(baseElementStyles).map(([key, value]) => (
+                <div key={key}>
+                    {value.elStyleType === 'compound' ? (
+                        <CompoundStyle style={value as CompoundStyleImpl} />
+                    ) : (
+                        <SingleStyle style={value as SingleStyle} />
+                    )}
+                </div>
+            ))}
+        </>
+    );
+});
+StyleGroupComponent.displayName = 'StyleGroupComponent';
+
+const AccordionHeader = memo(({ groupKey }: { groupKey: string }) => {
+    const editorEngine = useEditorEngine();
+
+    return (
+        <Tooltip>
+            <TooltipTrigger asChild disabled={editorEngine.style.mode !== StyleMode.Instance}>
+                <div
+                    className={cn(
+                        'text-xs flex transition-all items-center group',
+                        editorEngine.style.mode === StyleMode.Instance &&
+                            'gap-1 text-purple-600 dark:text-purple-300 hover:text-purple-500 dark:hover:text-purple-200',
+                    )}
+                >
+                    <Icons.ComponentInstance
+                        className={cn(
+                            'transition-all w-0',
+                            editorEngine.style.mode === StyleMode.Instance &&
+                                'w-3 h-3 text-purple-600 dark:text-purple-300 group-hover:text-purple-500 dark:group-hover:text-purple-200',
+                        )}
+                    />
+                    {groupKey}
+                </div>
+            </TooltipTrigger>
+            <TooltipPortal container={document.getElementById('style-tab-id')}>
+                <TooltipContent>{'Changes apply to instance code.'}</TooltipContent>
+            </TooltipPortal>
+        </Tooltip>
+    );
+});
+AccordionHeader.displayName = 'AccordionHeader';
+
+const TailwindSection = memo(() => {
+    return (
+        <AccordionItem value="tw">
+            <AccordionTrigger>
+                <h2 className="text-xs">Tailwind Classes</h2>
+            </AccordionTrigger>
+            <AccordionContent>
+                <TailwindInput />
+            </AccordionContent>
+        </AccordionItem>
+    );
+});
+TailwindSection.displayName = 'TailwindSection';
 
 const ManualTab = observer(() => {
     const editorEngine = useEditorEngine();
 
-    const renderAccordianHeader = useCallback(
-        (groupKey: string) => {
-            return (
-                <Tooltip>
-                    <TooltipTrigger
-                        asChild
-                        disabled={editorEngine.style.mode !== StyleMode.Instance}
-                    >
-                        <div
-                            className={cn(
-                                'text-xs flex transition-all items-center group',
-                                editorEngine.style.mode === StyleMode.Instance &&
-                                    'gap-1 text-purple-600 dark:text-purple-300 hover:text-purple-500 dark:hover:text-purple-200',
-                            )}
-                        >
-                            <Icons.ComponentInstance
-                                className={cn(
-                                    'transition-all w-0',
-                                    editorEngine.style.mode === StyleMode.Instance &&
-                                        'w-3 h-3 text-purple-600 dark:text-purple-300 group-hover:text-purple-500 dark:group-hover:text-purple-200',
-                                )}
-                            />
-                            {groupKey}
-                        </div>
-                    </TooltipTrigger>
-                    <TooltipPortal container={document.getElementById('style-tab-id')}>
-                        <TooltipContent>{'Changes apply to instance code.'}</TooltipContent>
-                    </TooltipPortal>
-                </Tooltip>
-            );
-        },
-        [editorEngine.style.mode],
-    );
-
-    const styleSections = useMemo(
-        () =>
-            Object.entries(STYLE_GROUP_MAPPING).map(([groupKey, baseElementStyles]) => (
-                <AccordionItem key={groupKey} value={groupKey}>
-                    <AccordionTrigger className=" mb-[-4px] mt-[-2px]">
-                        {renderAccordianHeader(groupKey)}
-                    </AccordionTrigger>
-                    <AccordionContent className="mt-2px">
-                        {groupKey === StyleGroupKey.Text && <TagDetails />}
-                        {renderGroupValues(baseElementStyles)}
-                    </AccordionContent>
-                </AccordionItem>
-            )),
-        [],
-    );
-
-    const tailwindSection = useMemo(
-        () => (
-            <AccordionItem key={TAILWIND_KEY} value={TAILWIND_KEY}>
-                <AccordionTrigger>
-                    <h2 className="text-xs">Tailwind Classes</h2>
+    const styleSections = useMemo(() => {
+        return Object.entries(STYLE_GROUP_MAPPING).map(([groupKey, baseElementStyles]) => (
+            <AccordionItem key={groupKey} value={groupKey}>
+                <AccordionTrigger className="mb-[-4px] mt-[-2px]">
+                    <AccordionHeader groupKey={groupKey} />
                 </AccordionTrigger>
-                <AccordionContent>
-                    <TailwindInput />
+                <AccordionContent className="mt-2px">
+                    {groupKey === StyleGroupKey.Text && <TagDetails />}
+                    <StyleGroupComponent baseElementStyles={baseElementStyles} />
                 </AccordionContent>
             </AccordionItem>
-        ),
-        [],
-    );
-
-    if (editorEngine.elements.selected.length === 0) {
-        return null;
-    }
+        ));
+    }, []);
 
     return (
-        <Accordion
-            className="px-3"
-            type="multiple"
-            defaultValue={[...Object.values(StyleGroupKey), TAILWIND_KEY]}
-        >
-            {tailwindSection}
-            {styleSections}
-        </Accordion>
+        editorEngine.elements.selected.length > 0 && (
+            <Accordion
+                className="px-3"
+                type="multiple"
+                defaultValue={[...Object.values(StyleGroupKey), 'tw']}
+            >
+                <TailwindSection />
+                {styleSections}
+            </Accordion>
+        )
     );
 });
 
