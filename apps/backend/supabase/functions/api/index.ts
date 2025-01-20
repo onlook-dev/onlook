7823f02d96352d4942089079e496e6881bf2fabe
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-01-19 23:03:50 -0800

    Remove auth check on apis for now (#1070)

--- apps/backend/supabase/functions/api/index.ts
+++ apps/backend/supabase/functions/api/index.ts
@@ -2,24 +2,25 @@ import { ApiRoutes, BASE_API_ROUTE } from "@onlook/models/constants/api.ts";
 import { Hono } from 'jsr:@hono/hono';
 import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { aiRouteHandler } from "./ai/index.ts";
-import { authenticateUser } from "./helpers/auth.ts";
 import { hostingRouteHandler } from "./hosting.ts/index.ts";
 
 const app = new Hono();
 
 app.post(`${BASE_API_ROUTE}${ApiRoutes.AI}`, async (c) => {
-    const auth = await authenticateUser(c);
-    if (!auth.success) {
-        return auth.response;
-    }
+    // TODO: Need better auth method
+    // const auth = await authenticateUser(c);
+    // if (!auth.success) {
+    //     return auth.response;
+    // }
     return await aiRouteHandler(await c.req.json());
 });
 
 app.post(`${BASE_API_ROUTE}${ApiRoutes.HOSTING}`, async (c) => {
-    const auth = await authenticateUser(c);
-    if (!auth.success) {
-        return auth.response;
-    }
+    // TODO: Need better auth method
+    // const auth = await authenticateUser(c);
+    // if (!auth.success) {
+    //     return auth.response;
+    // }
     return await hostingRouteHandler(await c.req.json());
 });
 
