981ce205658a77e0548b110b450ba420be3df266
Kiet Ho <hoakiet98@gmail.com>
2025-01-22 16:11:38 -0800

    Clean up

--- apps/studio/src/routes/editor/TopBar/index.tsx
+++ apps/studio/src/routes/editor/TopBar/index.tsx
@@ -6,7 +6,7 @@ import { Tooltip, TooltipContent, TooltipTrigger } from '@onlook/ui/tooltip';
 import { observer } from 'mobx-react-lite';
 import ModeToggle from './ModeToggle';
 import OpenCode from './OpenCode';
-import PricingButton from './Profile';
+import ProfileDropdown from './Profile';
 import ProjectBreadcrumb from './ProjectSelect';
 import ShareProject from './ShareProject';
 import ZoomControls from './ZoomControls';
@@ -83,7 +83,7 @@ const EditorTopBar = observer(() => {
                 <ZoomControls />
                 <OpenCode />
                 <ShareProject />
-                <PricingButton />
+                <ProfileDropdown />
             </div>
         </div>
     );
