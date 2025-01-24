f357c2b2341261e4c65ac649e71545267a9f01f2
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-01-23 22:00:03 -0800

    Handle auth tokens better (#1085)

--- apps/studio/electron/main/events/auth.ts
+++ apps/studio/electron/main/events/auth.ts
@@ -1,13 +1,12 @@
+import { MainChannels } from '@onlook/models/constants';
 import { ipcMain } from 'electron';
 import { mainWindow } from '..';
 import analytics from '../analytics';
 import { PersistentStorage } from '../storage';
-import { MainChannels } from '@onlook/models/constants';
 
 export function listenForAuthMessages() {
     ipcMain.handle(MainChannels.SIGN_OUT, (e: Electron.IpcMainInvokeEvent, args) => {
         PersistentStorage.USER_METADATA.clear();
-        PersistentStorage.AUTH_TOKENS.clear();
         analytics.signOut();
         mainWindow?.webContents.send(MainChannels.USER_SIGNED_OUT);
     });
--- apps/studio/electron/main/storage/index.ts
+++ apps/studio/electron/main/storage/index.ts
@@ -1,11 +1,5 @@
 import type { ChatConversation } from '@onlook/models/chat';
-import type {
-    AppState,
-    AuthTokens,
-    ProjectsCache,
-    UserMetadata,
-    UserSettings,
-} from '@onlook/models/settings';
+import type { AppState, ProjectsCache, UserMetadata, UserSettings } from '@onlook/models/settings';
 import { DirectoryPersistentStorage } from './directory';
 import { SingleFilePersistentStorage } from './file';
 
@@ -13,7 +7,6 @@ export enum StorageType {
     USER_SETTINGS = 'user-settings',
     APP_STATE = 'app-state',
     USER_METADATA = 'user-metadata',
-    AUTH_TOKENS = 'auth-tokens-v1',
     PROJECTS = 'projects',
     CONVERSATIONS = 'conversations-v1',
 }
@@ -26,10 +19,6 @@ export class PersistentStorage {
     );
     static readonly USER_METADATA = new SingleFilePersistentStorage<UserMetadata>(
         StorageType.USER_METADATA,
-    );
-    // TODO: Remove this in favor of supabase auth sessions
-    static readonly AUTH_TOKENS = new SingleFilePersistentStorage<AuthTokens>(
-        StorageType.AUTH_TOKENS,
     );
     static readonly CONVERSATIONS = new DirectoryPersistentStorage<ChatConversation>(
         StorageType.CONVERSATIONS,
