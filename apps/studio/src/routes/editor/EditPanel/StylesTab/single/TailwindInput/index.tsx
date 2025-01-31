ef0e5cab9e645f855f146393645900bb07625ec9
Nithish <83941930+Nithishvb@users.noreply.github.com>
2025-02-01 01:52:08 +0530

    fix: Apply Tailwind class to multiple selected elements (#1134)
    * fix: Apply Tailwind class to multiple selected elements
    
    * fix: resolve className overwriting issue when adding classNames to multiple elements
    
    ---------
    
    Co-authored-by: Nithishvb <Nithishvb@users.noreply.github.com>

--- apps/studio/src/routes/editor/EditPanel/StylesTab/single/TailwindInput/index.tsx
+++ apps/studio/src/routes/editor/EditPanel/StylesTab/single/TailwindInput/index.tsx
@@ -187,6 +187,28 @@ const TailwindInput = observer(() => {
         }
     }
 
+    async function getPrevRootClasses(domEl: DomElement): Promise<string[]> {
+        try {
+            const newRoot = await editorEngine.ast.getTemplateNodeById(domEl.oid);
+            if (newRoot) {
+                const rootClasses: ClassParsingResult = await invokeMainChannel(
+                    MainChannels.GET_TEMPLATE_NODE_CLASS,
+                    newRoot,
+                );
+
+                if (rootClasses.type === 'error') {
+                    console.warn(rootClasses.reason);
+                }
+
+                return rootClasses.type === 'classes' ? rootClasses.value : [];
+            }
+            return [];
+        } catch (err) {
+            console.log('erpr ', err);
+            return [];
+        }
+    }
+
     const createCodeDiffRequest = async (oid: string | undefined, className: string) => {
         if (!oid) {
             console.error('No oid found for createCodeDiffRequest');
@@ -198,18 +220,94 @@ const TailwindInput = observer(() => {
             return;
         }
 
-        const request: CodeDiffRequest = {
-            oid,
-            attributes: { className },
-            textContent: null,
-            overrideClasses: true,
-            structureChanges: [],
-        };
-        const res = await editorEngine.code.getAndWriteCodeDiff([request], true);
+        const request: CodeDiffRequest[] = [];
+        const selectedElements = editorEngine.elements.selected;
+        const selectedElprevClasses = await getPrevRootClasses(selectedElements[0]);
+        const computedClass = compareClassNames(selectedElprevClasses, className.split(' '));
+
+        for (const ele of selectedElements) {
+            let computedClassName: string = '';
+            const isNonSelectedElement = ele.oid !== oid;
+
+            if (isNonSelectedElement) {
+                const prevClasses = await getPrevRootClasses(ele);
+                computedClassName = computeClassForNonSelectedElement(
+                    computedClass.added,
+                    computedClass.removed,
+                    prevClasses,
+                );
+            } else {
+                computedClassName = className;
+            }
+
+            request.push({
+                oid: ele.oid || '',
+                attributes: { className: computedClassName },
+                textContent: null,
+                overrideClasses: true,
+                structureChanges: [],
+            });
+        }
+
+        const res = await editorEngine.code.getAndWriteCodeDiff(request, true);
         if (res) {
             sendAnalytics('tailwind action');
         }
     };
+
+    function computeClassForNonSelectedElement(
+        addedClasses: string[],
+        removedClasses: string[],
+        originalClasses: string[],
+    ): string {
+        const finalClasses: string[] = [...originalClasses];
+        let classRemoved: boolean = false;
+
+        if (removedClasses.length > 0) {
+            removedClasses.forEach((cls) => {
+                const index = finalClasses.indexOf(cls);
+                if (index !== -1) {
+                    finalClasses.splice(index, 1);
+                    classRemoved = true;
+                }
+            });
+        }
+
+        if (removedClasses.length > 0 && classRemoved) {
+            addedClasses.forEach((cls) => {
+                if (!finalClasses.includes(cls)) {
+                    finalClasses.push(cls);
+                }
+            });
+        } else {
+            if (removedClasses.length === 0) {
+                addedClasses.forEach((cls) => {
+                    if (!finalClasses.includes(cls)) {
+                        finalClasses.push(cls);
+                    }
+                });
+            }
+        }
+
+        return finalClasses.join(' ');
+    }
+
+    function compareClassNames(
+        oldClasses: string[],
+        newClasses: string[],
+    ): {
+        added: string[];
+        removed: string[];
+    } {
+        const addedClasses = newClasses.filter((cls) => !oldClasses.includes(cls));
+
+        const removedClasses = oldClasses.filter((cls) => !newClasses.includes(cls));
+
+        return {
+            added: addedClasses,
+            removed: removedClasses,
+        };
+    }
 
     const handleInput = (
         e: React.FormEvent<HTMLTextAreaElement>,
