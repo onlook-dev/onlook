19f1adf887235d300127e4035dd48e1c7b50b8fe
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-01-30 23:03:47 -0800

    Clean up messages padding

--- apps/studio/src/routes/editor/EditPanel/ChatTab/ChatMessages/index.tsx
+++ apps/studio/src/routes/editor/EditPanel/ChatTab/ChatMessages/index.tsx
@@ -36,7 +36,7 @@ const ChatMessages = observer(() => {
                     : rateLimited.monthly_requests_limit;
 
             return (
-                <div className="flex w-full flex-col items-center justify-center gap-2 text-small px-4">
+                <div className="flex w-full flex-col items-center justify-center gap-2 text-small px-4 mb-5">
                     <p className="text-foreground-secondary text-mini my-1 text-blue-300 select-none">
                         You reached your {rateLimited.reason} {requestLimit} message limit.
                     </p>
