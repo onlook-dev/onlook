ab9ae1fb9cfa14b4e2e8ca2391505f5cb055a26a
Kiet Ho <hoakiet98@gmail.com>
2025-02-03 16:11:38 -0800

    Solve optimize-deps issues

--- apps/studio/vite.config.ts
+++ apps/studio/vite.config.ts
@@ -29,6 +29,9 @@ export default defineConfig(({ command }) => {
                 common: path.join(__dirname, 'common'),
             },
         },
+        optimizeDeps: {
+            exclude: ['node_modules/.vite'],
+        },
         plugins: [
             react(),
             electron({
