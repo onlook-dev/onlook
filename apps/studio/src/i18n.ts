90cfe80134905acc778e6fbf8276e746ace55d15
Kiet Ho <hoakiet98@gmail.com>
2025-02-05 15:54:51 -0800

    Clean up

--- apps/studio/src/i18n.ts
+++ apps/studio/src/i18n.ts
@@ -1,6 +1,5 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
-// Import JSON files directly if using webpack/vite
 import enTranslation from './locales/en/translation.json';
 import jaTranslation from './locales/ja/translation.json';
 
