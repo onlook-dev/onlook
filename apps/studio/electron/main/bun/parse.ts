4c4e2319661760f8ef9ec949ea6b566a45bbd526
Kiet Ho <hoakiet98@gmail.com>
2025-02-10 04:17:32 -0800

    Clean up

--- apps/studio/electron/main/bun/parse.ts
+++ apps/studio/electron/main/bun/parse.ts
@@ -7,7 +7,7 @@ export const replaceCommand = (command: string, newCommand: string): string => {
     const finalCommand =
         (packageManagers.includes(cmdName?.toString() || '') ? newCommand : cmdName) || '';
 
-    // For Windows, wrap the entire command in single quotes to handle spaces in the command
+    // For Windows, add '&' to the command to handle special characters
     if (process.platform === 'win32') {
         return (
             '& ' + quote([finalCommand.toString(), ...cmdArgs.map((arg) => arg?.toString() || '')])
