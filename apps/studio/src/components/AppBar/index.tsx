cb02046b8167e6fadf96ebcfb49f7d2de6c4cc58
Kiet Ho <hoakiet98@gmail.com>
2025-02-26 11:51:38 -0800

    Clean up

--- apps/studio/src/components/AppBar/index.tsx
+++ apps/studio/src/components/AppBar/index.tsx
@@ -1,10 +1,5 @@
 import { useRouteManager, useUpdateManager } from '@/components/Context';
 import { Route } from '@/lib/routes';
-import { invokeMainChannel } from '@/lib/utils';
-import { MainChannels } from '@onlook/models/constants';
-import { Button } from '@onlook/ui/button';
-import { Icons } from '@onlook/ui/icons';
-import { Tooltip, TooltipContent, TooltipTrigger } from '@onlook/ui/tooltip';
 import { cn } from '@onlook/ui/utils';
 import { observer } from 'mobx-react-lite';
 import UpdateButton from './UpdateButton';
