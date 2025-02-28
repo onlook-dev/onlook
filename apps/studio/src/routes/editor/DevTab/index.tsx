18258d2d0ad43a7e9980450f8ea36a421d906e16
tycreated <110621523+tycreated@users.noreply.github.com>
2025-02-28 11:52:22 -0600

    Updated DevTab Dropdowns & Button styling

--- apps/studio/src/routes/editor/DevTab/index.tsx
+++ apps/studio/src/routes/editor/DevTab/index.tsx
@@ -26,7 +26,9 @@ const FileTab: React.FC<FileTabProps> = ({ filename }) => {
     return (
         <div className="h-full px-4 relative">
             <div className="absolute right-0 h-[50%] w-[0.5px] bg-foreground/10 top-1/2 -translate-y-1/2"></div>
-            <div className="text-foreground text-sm h-full flex items-center">{filename}</div>
+            <button className="text-foreground text-sm h-full flex items-center hover:text-foreground-hover focus:outline-none">
+                {filename}
+            </button>
         </div>
     );
 };
@@ -42,7 +44,7 @@ const DevTab = observer(() => {
     return (
         <div className="h-full flex flex-col w-full border-l-[0.5px] border-t-[0.5px] border-b-[0.5px] backdrop-blur shadow rounded-tl-xl">
             {/* Top Bar with Edit Code and Files dropdowns */}
-            <div className="flex items-center justify-between h-11 px-4 rounded-tl-xl border-b-[0.5px]">
+            <div className="flex items-center justify-between h-11 pl-4 pr-2 rounded-tl-xl border-b-[0.5px]">
                 <div className="flex items-center space-x-5 h-full">
                     <DropdownMenu>
                         <DropdownMenuTrigger className="flex items-center text-foreground text-sm hover:text-foreground-hover h-full">
@@ -51,10 +53,10 @@ const DevTab = observer(() => {
                             <Icons.ChevronDown className="h-3 w-3" />
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="start" className="-mt-1">
-                            <DropdownMenuItem>Jump to code from canvas</DropdownMenuItem>
-                            <DropdownMenuItem>Editor setting here</DropdownMenuItem>
-                            <DropdownMenuItem>Editor setting here</DropdownMenuItem>
-                            <DropdownMenuItem>Editor setting here</DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Icons.Check className="mr-2 h-4 w-4" />
+                                Jump to code from canvas
+                            </DropdownMenuItem>
                         </DropdownMenuContent>
                     </DropdownMenu>
 
@@ -65,12 +67,12 @@ const DevTab = observer(() => {
                             <Icons.ChevronDown className="h-3 w-3" />
                         </DropdownMenuTrigger>
                         <DropdownMenuContent align="start" className="-mt-1">
-                            <div className="h-[250px] w-[200px] overflow-auto p-1"></div>
+                            <div className="h-[300px] w-[250px] overflow-auto p-1"></div>
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </div>
 
-                <button className="text-foreground hover:text-foreground-hover">
+                <button className="text-foreground hover:text-foreground-hover hover:bg-foreground/5 p-2 rounded flex items-center justify-center">
                     <Icons.CrossS className="h-4 w-4" />
                 </button>
             </div>
