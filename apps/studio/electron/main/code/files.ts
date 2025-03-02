4581bd04beba275503a3d8d1e1ee534c714b0df0
Kiet Ho <hoakiet98@gmail.com>
2025-03-02 15:19:12 -0800

    Revert "feat: watch file changes in directory (#1478)"
    This reverts commit c525b3babf8241b2a95bdd5686901390014a280d.

--- apps/studio/electron/main/code/files.ts
+++ apps/studio/electron/main/code/files.ts
@@ -2,6 +2,7 @@ import { existsSync, promises as fs } from 'fs';
 import * as path from 'path';
 import prettier from 'prettier';
 import writeFileAtomic from 'write-file-atomic';
+import run from '../run';
 
 export async function readFile(filePath: string): Promise<string | null> {
     try {
@@ -32,7 +33,6 @@ export async function writeFile(
 
         // Ensure parent directory exists
         const parentDir = path.dirname(fullPath);
-
         await fs.mkdir(parentDir, { recursive: true });
 
         // Handle base64 encoded content
@@ -47,6 +47,7 @@ export async function writeFile(
 
         if (isNewFile) {
             console.log('New file created:', fullPath);
+            run.addFileToWatcher(fullPath);
         }
     } catch (error: any) {
         console.error('Error writing to file:', error);
