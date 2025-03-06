74802a2477f0ff31d6c1a6f6be5c6ceb5cd843c9
Kiet Ho <hoakiet98@gmail.com>
2025-03-06 15:51:20 -0800

    Update inject script

--- packages/growth/src/inject.ts
+++ packages/growth/src/inject.ts
@@ -1,5 +1,7 @@
 export function injectBuiltWithScript() {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/builtwith@5.3.0/dist/builtwith.min.js';
-    document.head.appendChild(script);
+    // Inject as script in layout.ts
+}
+
+export function addBuiltWithScript() {
+    // Copy builtwith.js to public folder
 }
