1a2edbb506add78efa0b0beb467bd9e7b668a966
Kiet Ho <hoakiet98@gmail.com>
2025-03-19 21:13:29 -0700

    Use actual popover api

--- apps/studio/src/routes/editor/EditPanel/StylesTab/single/ColorInput/ColorBrandPicker.tsx
+++ apps/studio/src/routes/editor/EditPanel/StylesTab/single/ColorInput/ColorBrandPicker.tsx
@@ -1,16 +1,16 @@
 import { useEditorEngine } from '@/components/Context';
+import type { CompoundStyle } from '@/lib/editor/styles/models';
 import type { ColorItem } from '@/routes/editor/LayersPanel/BrandTab/ColorPanel/ColorPalletGroup';
 import { Icons } from '@onlook/ui/icons/index';
 import { Input } from '@onlook/ui/input';
 import { Popover, PopoverContent, PopoverTrigger } from '@onlook/ui/popover';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@onlook/ui/tabs';
 import { Color } from '@onlook/utility';
-import { memo, useEffect, useMemo, useRef, useState } from 'react';
+import { memo, useEffect, useRef, useState } from 'react';
+import { isBackgroundImageEmpty } from '.';
 import ColorButton from './ColorButton';
 import ColorPickerContent from './ColorPicker';
 import ImagePickerContent from './ImagePicker';
-import type { CompoundStyle } from '@/lib/editor/styles/models';
-import { isBackgroundImageEmpty } from '.';
 
 interface ColorBrandPickerProps {
     color: Color;
@@ -131,7 +131,11 @@ const BrandPopoverPicker = memo(
                 <PopoverTrigger>
                     <ColorButton value={color} onClick={() => toggleOpen(!isOpen)} />
                 </PopoverTrigger>
-                <PopoverContent className="backdrop-blur-lg z-10 rounded-lg p-0 shadow-xl overflow-hidden w-56 fixed -left-60">
+                <PopoverContent
+                    className="backdrop-blur-lg z-10 rounded-lg p-0 shadow-xl overflow-hidden w-56"
+                    side="left"
+                    align="start"
+                >
                     <div>
                         <Tabs defaultValue={defaultValue} className="bg-transparent pb-0">
                             <TabsList className="bg-transparent px-2 m-0 gap-2">
