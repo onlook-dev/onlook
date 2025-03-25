a8268cdd4d4d9eb71425482b2b4bcbdc1e11708c
vutnguyen <vu.tran.nguyen@c0x12c.com>
2025-03-25 10:11:23 +0700

    write to tailwind

--- apps/studio/electron/main/assets/styles.ts
+++ apps/studio/electron/main/assets/styles.ts
@@ -28,6 +28,7 @@ import {
     initializeTailwindColorContent,
     isColorsObjectProperty,
     isObjectExpression,
+    modifyTailwindConfig,
 } from './helpers';
 
 export async function updateTailwindColorConfig(
@@ -188,20 +189,15 @@ function updateTailwindConfigFile(
     newName: string,
     newCssVarName: string,
 ): ConfigUpdateResult {
-    const updateAst = parse(configContent, {
-        sourceType: 'module',
-        plugins: ['typescript', 'jsx'],
-    });
-
     let keyUpdated = false;
     let valueUpdated = false;
 
-    traverse(updateAst, {
-        ObjectProperty(path) {
+    const { output } = modifyTailwindConfig(configContent, {
+        visitor: (path) => {
             if (isColorsObjectProperty(path)) {
                 const colorObj = path.node.value;
                 if (!isObjectExpression(colorObj)) {
-                    return;
+                    return false;
                 }
 
                 colorObj.properties.forEach((colorProp) => {
@@ -245,7 +241,7 @@ function updateTailwindConfigFile(
                         } else {
                             const nestedObj = colorProp.value;
                             if (!isObjectExpression(nestedObj)) {
-                                return;
+                                return false;
                             }
                             nestedObj.properties.forEach((nestedProp) => {
                                 if (
@@ -270,11 +266,13 @@ function updateTailwindConfigFile(
                         }
                     }
                 });
+
+                return keyUpdated || valueUpdated;
             }
+            return false;
         },
     });
 
-    const output = generate(updateAst, { retainLines: true, compact: false }, configContent).code;
     return { keyUpdated, valueUpdated, output };
 }
 
