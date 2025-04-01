aa0a59627a7cf277deaf57c37c7bf3ebc283445c
Kiet Ho <hoakiet98@gmail.com>
2025-04-01 15:41:42 -0700

    Working auth

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,11 +1,7 @@
-import { HydrateClient } from "~/trpc/server";
+import Main from "./_components/main";
 
 export default async function Home() {
     return (
-        <HydrateClient>
-            <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-                Hello
-            </main>
-        </HydrateClient>
+        <Main />
     );
 }
