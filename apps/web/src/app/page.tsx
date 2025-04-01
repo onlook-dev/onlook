0148fd97083de9821ee21c940b4d56ed8f7f399f
Kiet Ho <hoakiet98@gmail.com>
2025-03-31 23:25:06 -0700

    Redirect auth

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,18 @@
 import { createClient } from "@/utils/supabase/server";
-import { api, HydrateClient } from "~/trpc/server";
+import { redirect } from "next/navigation";
+import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-    const hello = await api.post.hello({ text: "from tRPC" });
-
-    void api.post.getLatest.prefetch();
     const supabase = await createClient()
-
-    const {
-        data: { user },
-    } = await supabase.auth.getUser()
+    const { data, error } = await supabase.auth.getUser()
+    if (error || !data?.user) {
+        redirect('/login')
+    }
 
     return (
         <HydrateClient>
             <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-                {user?.email}
+                Hello {data.user.email}
             </main>
         </HydrateClient>
     );
