834646799bfb84e7ae3a2ad9702a9dde450be92b
Kiet Ho <hoakiet98@gmail.com>
2025-04-07 14:49:23 -0700

    Fix height issue

--- apps/web/src/app/project/[id]/page.tsx
+++ apps/web/src/app/project/[id]/page.tsx
@@ -9,27 +9,27 @@ import { useState } from "react";
 import { Canvas } from "./_components/canvas";
 import { EditorBar } from "./_components/editor-bar";
 import { StagingToggle } from "./_components/editor-bar/staging-toggle";
+import { RightPanel } from './_components/right-panel';
 import { TopBar } from "./_components/top-bar";
 
 type ElementType = "div" | "text" | "image";
 
 export default function Page({ params }: { params: { id: string } }) {
-    const id = params.id;
     const [selectedElement, setSelectedElement] = useState<ElementType>("div");
 
     return (
-        <div className="relative h-screen w-screen flex flex-row select-none">
+        <div className="h-screen w-screen flex flex-row select-none relative">
             <Canvas>
                 <Frames />
             </Canvas>
 
             {/* <div className="absolute top-10 left-0 animate-layer-panel-in h-[calc(100%-2.60rem)]">
                 <LeftPanel />
-            </div>
+            </div> */}
 
             <div className="absolute top-10 right-0 animate-edit-panel-in h-[calc(100%-2.60rem)]">
                 <RightPanel />
-            </div> */}
+            </div>
 
             <div className="absolute bottom-4 left-1/2 transform -translate-x-1/2 animate-toolbar-up ">
                 {/* <Toolbar /> */}
