9e19f3daf0d020c8e5ffceddc4d92325adae1c53
Kiet Ho <hoakiet98@gmail.com>
2025-04-07 14:19:32 -0700

    Move web client

---
+++ apps/client/.next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},__routerFilterStatic:a,__routerFilterDynamic:a,sortedPages:["\u002F_app"]}}(void 0));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
