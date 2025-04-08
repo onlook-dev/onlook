d9bfb94bd19291061b693606d5554388e42b6ce5
Kiet Ho <hoakiet98@gmail.com>
2025-04-08 11:37:14 -0700

    More build fixes

--- apps/web/client/src/env.ts
+++ apps/web/client/src/env.ts
@@ -16,7 +16,8 @@ export const env = createEnv({
      * `NEXT_PUBLIC_`.
      */
     client: {
-        // NEXT_PUBLIC_CLIENTVAR: z.string(),
+        NEXT_PUBLIC_SUPABASE_URL: z.string(),
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
     },
 
     /**
@@ -25,7 +26,8 @@ export const env = createEnv({
      */
     runtimeEnv: {
         NODE_ENV: process.env.NODE_ENV,
-        // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
+        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
