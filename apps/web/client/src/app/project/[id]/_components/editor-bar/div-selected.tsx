6e1a996a602993efbf421b4fc95379ee1a146cc0
tycreated <thughey99@gmail.com>
2025-04-08 19:15:08 -0700

    Removed chevrons and updated padding

--- apps/web/client/src/app/project/[id]/_components/editor-bar/div-selected.tsx
+++ apps/web/client/src/app/project/[id]/_components/editor-bar/div-selected.tsx
@@ -27,13 +27,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.Layout className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">Flex</span>
-                        </div>
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.Layout className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">Flex</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="min-w-[120px] mt-2 p-1 rounded-lg">
@@ -116,14 +113,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.Width className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">280</span>
-                        </div>
-                        
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.Width className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">280</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg space-y-3">
@@ -162,13 +155,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.Height className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">Hug</span>
-                        </div>
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.Height className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">Hug</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg space-y-3">
@@ -207,13 +197,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.Padding className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">Mixed</span>
-                        </div>
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.Padding className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">Mixed</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg">
@@ -258,13 +245,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.Margin className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">24px</span>
-                        </div>
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.Margin className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">24px</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg">
@@ -310,13 +294,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                     <Button
                         variant="ghost"
-                        className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border min-w-[105px] max-w-[105px] px-2 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
+                        className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                     >
-                        <div className="flex items-center gap-2">
-                            <Icons.CornerRadius className="h-4 w-4 min-h-4 min-w-4" />
-                            <span className="text-sm">8px</span>
-                        </div>
-                        <Icons.ChevronDown className="h-4 w-4 min-h-4 min-w-4 opacity-50" />
+                        <Icons.CornerRadius className="h-4 w-4 min-h-4 min-w-4" />
+                        <span className="text-sm">8px</span>
                     </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg">
@@ -361,12 +342,10 @@ export const DivSelected = () => {
                 <DropdownMenuTrigger asChild>
                 <Button
                 variant="ghost"
-                className="flex items-center justify-between gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border px-3"
+                className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border px-3"
             >
-                <div className="flex items-center gap-2">
-                    <Icons.BorderEdit className="h-4 w-4 min-h-4 min-w-4" />
-                    <span className="text-sm">1px</span>
-                </div>
+                <Icons.BorderEdit className="h-4 w-4 min-h-4 min-w-4" />
+                <span className="text-sm">1px</span>
             </Button>
                 </DropdownMenuTrigger>
                 <DropdownMenuContent align="start" className="w-[280px] mt-1 p-3 rounded-lg">
