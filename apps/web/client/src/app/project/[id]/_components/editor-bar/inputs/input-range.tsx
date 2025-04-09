152a11ebf3750dcbec94e1d8abd268ccffc884ec
tycreated <thughey99@gmail.com>
2025-04-08 18:36:40 -0700

    Input range update

--- apps/web/client/src/app/project/[id]/_components/editor-bar/inputs/input-range.tsx
+++ apps/web/client/src/app/project/[id]/_components/editor-bar/inputs/input-range.tsx
@@ -1,5 +1,5 @@
 import { Icons } from "@onlook/ui-v4/icons";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 interface InputRangeProps {
     value: number;
@@ -11,6 +11,8 @@ interface InputRangeProps {
 export const InputRange = ({ value, icon, unit = "px", onChange }: InputRangeProps) => {
     const Icon = icon ? Icons[icon] : Icons.Padding;
     const [inputValue, setInputValue] = useState(String(value));
+    const rangeRef = useRef<HTMLInputElement>(null);
+    const [isDragging, setIsDragging] = useState(false);
 
     useEffect(() => {
         setInputValue(String(value));
@@ -30,19 +32,46 @@ export const InputRange = ({ value, icon, unit = "px", onChange }: InputRangePro
         }
     };
 
+    const handleMouseDown = (e: React.MouseEvent) => {
+        if (rangeRef.current) {
+            setIsDragging(true);
+            document.addEventListener('mousemove', handleMouseMove);
+            document.addEventListener('mouseup', handleMouseUp);
+        }
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+        if (isDragging && rangeRef.current) {
+            const rect = rangeRef.current.getBoundingClientRect();
+            const percentage = Math.max(0, Math.min(1, (e.clientX - rect.left) / rect.width));
+            const newValue = Math.round(percentage * 500);
+            onChange?.(newValue);
+        }
+    };
+
+    const handleMouseUp = () => {
+        setIsDragging(false);
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    };
+
     return (
         <div className="flex items-center gap-2">
             <div className="flex-1 flex items-center gap-2">
                 <input
+                    ref={rangeRef}
                     type="range"
                     min="0"
                     max="500"
                     value={value}
                     onChange={(e) => onChange?.(Number(e.target.value))}
-                    className="flex-1 h-3 bg-background-tertiary/50 rounded-full appearance-none cursor-pointer 
-                        [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:mt-[-2px] hover:[&::-webkit-slider-thumb]:bg-white/90
-                        [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-0 hover:[&::-moz-range-thumb]:bg-white/90
-                        [&::-ms-thumb]:appearance-none [&::-ms-thumb]:w-4 [&::-ms-thumb]:h-4 [&::-ms-thumb]:rounded-full [&::-ms-thumb]:bg-white hover:[&::-ms-thumb]:bg-white/90"
+                    onMouseDown={handleMouseDown}
+                    className="flex-1 h-3 bg-background-tertiary/50 rounded-full appearance-none cursor-pointer relative
+                        [&::-webkit-slider-runnable-track]:bg-background-tertiary/50 [&::-webkit-slider-runnable-track]:rounded-full [&::-webkit-slider-runnable-track]:h-3
+                        [&::-moz-range-track]:bg-background-tertiary/50 [&::-moz-range-track]:rounded-full [&::-moz-range-track]:h-3
+                        [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:w-4 [&::-webkit-slider-thumb]:h-4 [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-white [&::-webkit-slider-thumb]:mt-[-2px] [&::-webkit-slider-thumb]:cursor-grab hover:[&::-webkit-slider-thumb]:bg-white/90 active:[&::-webkit-slider-thumb]:cursor-grabbing
+                        [&::-moz-range-thumb]:appearance-none [&::-moz-range-thumb]:w-4 [&::-moz-range-thumb]:h-4 [&::-moz-range-thumb]:rounded-full [&::-moz-range-thumb]:bg-white [&::-moz-range-thumb]:border-0 [&::-moz-range-thumb]:cursor-grab hover:[&::-moz-range-thumb]:bg-white/90 active:[&::-moz-range-thumb]:cursor-grabbing
+                        [&::-ms-thumb]:appearance-none [&::-ms-thumb]:w-4 [&::-ms-thumb]:h-4 [&::-ms-thumb]:rounded-full [&::-ms-thumb]:bg-white [&::-ms-thumb]:cursor-grab hover:[&::-ms-thumb]:bg-white/90 active:[&::-ms-thumb]:cursor-grabbing"
                 />
                 <div className="flex items-center bg-background-tertiary/50 justify-between rounded-md px-3 h-[36px]">
                     <input 
