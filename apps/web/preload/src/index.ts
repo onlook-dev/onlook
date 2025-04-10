8030ce2af3f5a0e53c3e958d90f5abec9a550ce7
Kiet Ho <hoakiet98@gmail.com>
2025-04-10 16:36:23 -0700

    Add iframe polyfill

--- apps/web/preload/src/index.ts
+++ apps/web/preload/src/index.ts
@@ -1,4 +1,5 @@
 import { WindowMessenger, connect } from 'penpal';
+import { getMethods } from './api';
 
 const createMessageConnection = async () => {
     console.log("Iframe creating message connection");
@@ -11,13 +12,7 @@ const createMessageConnection = async () => {
     const connection = connect({
         messenger,
         // Methods the iframe window is exposing to the parent window.
-        methods: {
-            mouseMove(x: number, y: number, color: string) {
-                const element = document.elementFromPoint(x, y);
-                if (!element) return;
-                (element as HTMLElement).style.cssText = `border: 2px solid ${color};`;
-            }
-        },
+        methods: getMethods()
     });
 
     const remote = await connection.promise as any;
