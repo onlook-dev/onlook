2b504eb6e5a342cf984649032f7f2547cb1f6e86
tycreated <thughey99@gmail.com>
2025-04-10 17:53:38 -0700

    Updated bucket, border, image styling

--- apps/web/client/src/app/project/[id]/_components/editor-bar/dropdowns/border.tsx
+++ apps/web/client/src/app/project/[id]/_components/editor-bar/dropdowns/border.tsx
@@ -19,7 +19,7 @@ export const Border = () => {
             <DropdownMenuTrigger asChild>
                 <Button
                     variant="ghost"
-                    className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border px-3"
+                    className="flex items-center gap-2 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border px-3 focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
                 >
                     <Icons.BorderEdit className="h-4 w-4 min-h-4 min-w-4" />
                     <span className="text-sm">1px</span>
--- apps/web/client/src/app/project/[id]/_components/editor-bar/dropdowns/color-background.tsx
+++ apps/web/client/src/app/project/[id]/_components/editor-bar/dropdowns/color-background.tsx
@@ -10,20 +10,13 @@ import {
 
 export const ColorBackground = () => {
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button
-                    variant="ghost"
-                    size="icon"
-                    className="flex items-center justify-center px-5 flex-col gap-0.5 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border data-[state=open]:bg-background-tertiary/20 data-[state=open]:text-white data-[state=open]:border data-[state=open]:border-border focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:border-0"
-                >
-                    <Icons.PaintBucket className="h-4 w-4" />
-                    <div className="h-[2.5px] w-5.5 bg-current rounded-full" />
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start" className="min-w-[240px] mt-2 p-3 rounded-lg">
-                {/* Dropdown content will go here */}
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <Button
+        variant="ghost"
+        size="icon"
+        className="flex items-center justify-center px-5 flex-col gap-0.5 text-muted-foreground border border-border/0 cursor-pointer rounded-lg hover:bg-background-tertiary/20 hover:text-white hover:border hover:border-border focus-visible:ring-0 focus-visible:ring-offset-0 focus:outline-none focus-visible:outline-none active:bg-background-tertiary/20 active:text-white active:border active:border-border"
+    >
+        <Icons.PaintBucket className="h-4 w-4" />
+        <div className="h-[2.5px] w-5.5 bg-current rounded-full" />
+    </Button>
     );
 };
