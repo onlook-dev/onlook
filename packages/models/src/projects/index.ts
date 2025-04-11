65e771bf58fe0a760961d627a7b1353711b72f98
Kiet Ho <hoakiet98@gmail.com>
2025-04-11 15:55:24 -0700

    Clean up

--- packages/models/src/projects/index.ts
+++ packages/models/src/projects/index.ts
@@ -56,10 +56,7 @@ export interface Project {
         base: DomainSettings | null;
         custom: DomainSettings | null;
     } | null;
-    env?: Record<string, string>; // Added environment variables
-
-    // deprecated
-    // hosting?: HostingSettings | null;
+    env?: Record<string, string>;
 }
 
 export enum WindowCommand {
