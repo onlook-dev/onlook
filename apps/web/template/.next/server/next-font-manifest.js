4f25fb18e7effbc1c3a9e997778691c5c72de025
Kiet Ho <hoakiet98@gmail.com>
2025-04-12 10:59:47 -0700

    Clean up

---
+++ apps/web/template/.next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kietho/workplace/onlook/clones/dev-1/apps/web/template/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- packages/models/src/constants/ipc.ts
+++ packages/models/src/constants/ipc.ts
@@ -165,12 +165,10 @@ export enum MainChannels {
     GET_DEFAULT_FONT = 'get-default-font',
     UPLOAD_FONTS = 'upload-fonts',
     WATCH_FONT_FILE = 'watch-font-file',
+    FONTS_CHANGED = 'fonts-changed',
 
     // Trainloop
     SAVE_APPLY_RESULT = 'save-apply-result',
-
-    // New channel
-    FONTS_CHANGED = 'fonts-changed',
 }
 
 export enum GitChannels {
