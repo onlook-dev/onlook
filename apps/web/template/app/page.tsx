8d3a78a4304d7f5b19c48f5185b48bbd62415255
Kiet Ho <hoakiet98@gmail.com>
2025-04-22 13:59:59 -0400

    save

--- apps/web/template/app/page.tsx
+++ apps/web/template/app/page.tsx
@@ -3,7 +3,7 @@
 export default function Page() {
     return (
         <div className="w-full min-h-screen flex items-center justify-center bg-white dark:bg-black transition-colors duration-200 flex-col p-4 gap-[32px]" data-oid="1">
-            <div className="text-center text-gray-900 dark:text-gray-100 p-4" data-oid="2">
+            <div className="text-center text-gray-900 dark:text-gray-100 p-4 m-6" data-oid="2">
                 <h1 className="text-4xl md:text-5xl font-semibold mb-4 tracking-tight" data-oid="3">Don't</h1>
                 <h1 className="text-4xl md:text-5xl font-semibold mb-4 tracking-tight" data-oid="4">Worry</h1>
                 <h1 className="text-4xl md:text-5xl font-semibold mb-4 tracking-tight" data-oid="5">Be</h1>
