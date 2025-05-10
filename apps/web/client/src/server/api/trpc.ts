ab4198110f05a102b08bf3660847d9c114dfc49a
Kiet Ho <hoakiet98@gmail.com>
2025-05-10 17:35:16 -0400

    db separate client

--- apps/web/client/src/server/api/trpc.ts
+++ apps/web/client/src/server/api/trpc.ts
@@ -12,7 +12,7 @@ import superjson from 'superjson';
 import { ZodError } from 'zod';
 
 import { createClient } from '@/utils/supabase/server';
-import { db } from '@onlook/db';
+import { db } from '@onlook/db/src/client';
 
 /**
  * 1. CONTEXT
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,2 @@
-export * from './client';
 export * from './dto';
 export * from './schema';
