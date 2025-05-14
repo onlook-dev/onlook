66853ff8c3a188b96fe08de2b49bed8a2b4b9e13
Kiet Ho <hoakiet98@gmail.com>
2025-05-14 16:51:44 -0700

    add docs page

--- docs/src/app/layout.config.tsx
+++ docs/src/app/layout.config.tsx
@@ -8,19 +8,11 @@ import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
  * Docs Layout: app/docs/layout.tsx
  */
 export const baseOptions: BaseLayoutProps = {
-  nav: {
-    title: (
-      <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
-      </>
-    ),
-  },
+    nav: {
+        title: (
+            <>
+                Onlook Docs
+            </>
+        ),
+    },
 };
