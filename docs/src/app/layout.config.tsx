e093008f8fda300e26915a2671714eea5feadb0b
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-05-14 16:52:30 -0700

    refactor: update docs and build (#1838)
    * remove build workflow
    
    * update package.json
    
    * add docs page

---
+++ docs/src/app/layout.config.tsx
@@ -0,0 +1,18 @@
+import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+
+/**
+ * Shared layout configurations
+ *
+ * you can customise layouts individually from:
+ * Home Layout: app/(home)/layout.tsx
+ * Docs Layout: app/docs/layout.tsx
+ */
+export const baseOptions: BaseLayoutProps = {
+    nav: {
+        title: (
+            <>
+                Onlook Docs
+            </>
+        ),
+    },
+};
