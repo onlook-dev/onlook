5a70679ba3082d153a0efd72c33ea9d37e95bf3d
Harjot Singh <harjotsingh20022001@gmail.com>
2025-05-19 20:46:04 +0530

    Implemented the duplicate web frame feature

--- apps/web/client/src/components/store/editor/window/index.ts
+++ apps/web/client/src/components/store/editor/window/index.ts
@@ -1,6 +1,9 @@
 import { sendAnalytics } from '@/utils/analytics';
 import { makeAutoObservable } from 'mobx';
 import type { EditorEngine } from '../engine';
+import { nanoid } from 'nanoid/non-secure';
+import type { WebFrame } from '../../../../../../../../packages/models/src/project/frame';
+
 
 export class WindowManager {
     constructor(private editorEngine: EditorEngine) {
@@ -81,69 +84,62 @@ export class WindowManager {
     }
 
     delete(id?: string) {
-        // if (!this.canDeleteWindow()) {
-        //     console.error('Cannot delete the last window');
-        //     return;
-        // }
-        // let settings = null;
-        // if (id) {
-        //     settings = this.editorEngine.frames.get(id);
-        //     if (!settings) {
-        //         console.error('Window not found');
-        //         return;
-        //     }
-        // } else if (this.editorEngine.frames.selected.length === 0) {
-        //     console.error('No window selected');
-        //     return;
-        // } else {
-        //     settings = this.editorEngine.frames.get(this.editorEngine.frames.selected[0].id) || null;
-        // }
-        // if (!settings) {
-        //     console.error('Window not found');
-        //     return;
-        // }
-        // this.ast.mappings.remove(settings.id);
-        // this.canvas.frames = this.canvas.frames.filter((frame) => frame.id !== settings.id);
-        // const frameView = this.webviews.getWebview(settings.id);
-        // if (frameView) {
-        //     this.webviews.deregister(frameView);
-        // }
+        if (this.editorEngine.canvas.frames.length === 1) {
+            console.error('Cannot delete the last window');
+            return;
+        }
+        let settings: WebFrame | null = null;
+        if (id) {
+            settings = this.editorEngine.canvas.getFrame(id) || null;
+        } else if (this.editorEngine.frames.selected.length === 0) {
+            console.error('No window selected');
+            return;
+        } else {
+            settings = this.editorEngine.canvas.getFrame(this.editorEngine.frames.selected[0].id) || null;
+        }
+        if (!settings) {
+            console.error('Window not found');
+            return;
+        }
+        const currentFrame = settings;
+        this.editorEngine.ast.mappings.remove(settings.id);
+        this.editorEngine.canvas.frames = this.editorEngine.canvas.frames.filter((frame) => frame.id !== settings.id);
+        this.editorEngine.frames.deselect(currentFrame);
+        this.editorEngine.frames.disposeFrame(currentFrame.id);
         sendAnalytics('window delete');
     }
 
     duplicate(id?: string) {
-        // let settings: Frames | null = null;
-        // if (id) {
-        //     settings = this.canvas.getFrame(id) || null;
-        // } else if (this.webviews.selected.length === 0) {
-        //     console.error('No window selected');
-        //     return;
-        // } else {
-        //     settings = this.canvas.getFrame(this.webviews.selected[0].id) || null;
-        // }
-        // if (!settings) {
-        //     console.error('Window not found');
-        //     return;
-        // }
-        // const currentFrame = settings;
-        // const newFrame: Frames = {
-        //     id: nanoid(),
-        //     url: currentFrame.url,
-        //     dimension: {
-        //         width: currentFrame.dimension.width,
-        //         height: currentFrame.dimension.height,
-        //     },
-        //     position: {
-        //         x: currentFrame.position.x + currentFrame.dimension.width + 100,
-        //         y: currentFrame.position.y,
-        //     },
-        //     aspectRatioLocked: currentFrame.aspectRatioLocked,
-        //     orientation: currentFrame.orientation,
-        //     device: currentFrame.device,
-        //     theme: currentFrame.theme,
-        // };
+        let settings: WebFrame | null = null;
+        if (id) {
+            settings = this.editorEngine.canvas.getFrame(id) || null;
+        } else if (this.editorEngine.frames.selected.length === 0) {
+            console.error('No window selected');
+            return;
+        } else {
+            settings = this.editorEngine.canvas.getFrame(this.editorEngine.frames.selected[0].id) || null;
+        }
+        if (!settings) {
+            console.error('Window not found');
+            return;
+        }
+        const currentFrame = settings;
+        const newFrame: WebFrame = {
+            id: nanoid(),
+            url: currentFrame.url,
+            dimension: {
+                width: currentFrame.dimension.width,
+                height: currentFrame.dimension.height,
+            },
+            position: {
+                x: currentFrame.position.x + currentFrame.dimension.width + 100,
+                y: currentFrame.position.y,
+            },
+            type: currentFrame.type,
+            windowMetadata : currentFrame.windowMetadata,
+        };
 
-        // this.canvas.frames = [...this.canvas.frames, newFrame];
+        this.editorEngine.canvas.frames = [...this.editorEngine.canvas.frames, newFrame];
         sendAnalytics('window duplicate');
     }
 }
