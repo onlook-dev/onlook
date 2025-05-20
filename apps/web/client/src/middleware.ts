433304ba578b9568cfdfc2124744221c03ea3d15
Kiet Ho <hoakiet98@gmail.com>
2025-05-20 11:33:56 -0700

    restore root middleware matching

--- apps/web/client/src/middleware.ts
+++ apps/web/client/src/middleware.ts
@@ -15,8 +15,6 @@ export const config = {
          * - favicon.ico (favicon file)
          * Feel free to modify this pattern to include more paths.
          */
-        '/',
-        '/auth/callback',
         '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
     ],
 };
