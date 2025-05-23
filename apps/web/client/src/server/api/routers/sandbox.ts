f564321e22cc580ddf2b659c12f4d6cf2586c66a
Kiet Ho <hoakiet98@gmail.com>
2025-05-23 12:08:36 -0700

    Upgrade csb

--- apps/web/client/src/server/api/routers/sandbox.ts
+++ apps/web/client/src/server/api/routers/sandbox.ts
@@ -1,4 +1,5 @@
 import { CodeSandbox } from '@codesandbox/sdk';
+import { shortenUuid } from '@onlook/utility/src/id';
 import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../trpc';
 
@@ -7,30 +8,30 @@ const sdk = new CodeSandbox(process.env.CSB_API_KEY!);
 export const sandboxRouter = createTRPCRouter({
     start: protectedProcedure.input(z.object({
         sandboxId: z.string(),
+        userId: z.string(),
     })).mutation(async ({ input }) => {
-        const startData = await sdk.sandbox.start(input.sandboxId);
-        return startData;
-    }),
-    // Same as start - Separate endpoint for different state management
-    reconnect: protectedProcedure.input(z.object({
-        sandboxId: z.string(),
-    })).mutation(async ({ input }) => {
-        const startData = await sdk.sandbox.start(input.sandboxId);
-        return startData;
+        const startData = await sdk.sandboxes.resume(input.sandboxId);
+        const session = await startData.createBrowserSession({
+            id: shortenUuid(input.userId, 20)
+        });
+        return session;
     }),
     hibernate: protectedProcedure.input(z.object({
         sandboxId: z.string(),
     })).mutation(async ({ input }) => {
-        await sdk.sandbox.hibernate(input.sandboxId);
+        await sdk.sandboxes.hibernate(input.sandboxId);
     }),
     list: protectedProcedure.query(async () => {
-        const listResponse = await sdk.sandbox.list();
+        const listResponse = await sdk.sandboxes.list();
         return listResponse;
     }),
     fork: protectedProcedure.input(z.object({
         sandboxId: z.string(),
     })).mutation(async ({ input }) => {
-        const sandbox = await sdk.sandbox.create({ template: input.sandboxId });
+        const sandbox = await sdk.sandboxes.create({
+            source: 'template',
+            id: input.sandboxId,
+        });
         return {
             sandboxId: sandbox.id,
             previewUrl: `https://${sandbox.id}-8084.csb.app`,
@@ -39,6 +40,6 @@ export const sandboxRouter = createTRPCRouter({
     delete: protectedProcedure.input(z.object({
         sandboxId: z.string(),
     })).mutation(async ({ input }) => {
-        await sdk.sandbox.shutdown(input.sandboxId);
+        await sdk.sandboxes.shutdown(input.sandboxId);
     }),
 });
