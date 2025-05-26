55f1c8c6180635e71875eebf0a06bec60ce5ec8a
vutnguyen <vu.tran.nguyen@c0x12c.com>
2025-05-26 15:29:45 +0700

    remove logs

--- apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
+++ apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
@@ -7,7 +7,6 @@ import { WebFrameComponent, type WebFrameView } from './web-frame';
 
 export const FrameView = ({ frame }: { frame: Frame }) => {
     const webFrameRef = useRef<WebFrameView>(null);
-    console.log('frame', frame);
     return (
         <div
             className="flex flex-col fixed"
