7d0ecc90d240e6af286d663bee244f794689741e
Kiet Ho <hoakiet98@gmail.com>
2025-05-27 14:20:43 -0700

    update setup

--- apps/web/client/src/env.ts
+++ apps/web/client/src/env.ts
@@ -20,6 +20,8 @@ export const env = createEnv({
     client: {
         NEXT_PUBLIC_SUPABASE_URL: z.string(),
         NEXT_PUBLIC_SUPABASE_ANON_KEY: z.string(),
+        NEXT_PUBLIC_POSTHOG_KEY: z.string(),
+        NEXT_PUBLIC_POSTHOG_HOST: z.string(),
     },
 
     /**
@@ -33,6 +35,8 @@ export const env = createEnv({
         SUPABASE_DATABASE_URL: process.env.SUPABASE_DATABASE_URL,
         NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
         NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+        NEXT_PUBLIC_POSTHOG_KEY: process.env.NEXT_PUBLIC_POSTHOG_KEY,
+        NEXT_PUBLIC_POSTHOG_HOST: process.env.NEXT_PUBLIC_POSTHOG_HOST,
     },
     /**
      * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
