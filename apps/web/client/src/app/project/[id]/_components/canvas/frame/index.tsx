e674aee00f4ab43ca3cfb4cb2afaebcb73dca2c7
Kiet Ho <hoakiet98@gmail.com>
2025-05-31 18:17:29 -0700

    update types

--- apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
+++ apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
@@ -1,24 +1,25 @@
-import { FrameType, type Frame, type WebFrame } from '@onlook/models';
+import type { FrameImpl, WebFrameImpl } from '@/components/store/editor/frames/frame';
+import { FrameType } from '@onlook/models';
 import { useRef } from 'react';
 import { GestureScreen } from './gesture';
 import { ResizeHandles } from './resize-handles';
 import { TopBar } from './top-bar';
 import { WebFrameComponent, type WebFrameView } from './web-frame';
 
-export const FrameView = ({ frame }: { frame: Frame }) => {
+export const FrameView = ({ frame }: { frame: FrameImpl }) => {
     const webFrameRef = useRef<WebFrameView>(null);
     return (
         <div
             className="flex flex-col fixed"
             style={{ transform: `translate(${frame.position.x}px, ${frame.position.y}px)` }}
         >
-            <TopBar frame={frame as WebFrame} />
+            <TopBar frame={frame as WebFrameImpl} />
             <div className="relative">
                 <ResizeHandles frame={frame} />
                 {frame.type === FrameType.WEB && (
-                    <WebFrameComponent frame={frame as WebFrame} ref={webFrameRef} />
+                    <WebFrameComponent frame={frame as WebFrameImpl} ref={webFrameRef} />
                 )}
-                <GestureScreen frame={frame as WebFrame} />
+                <GestureScreen frame={frame as WebFrameImpl} />
             </div>
         </div>
     );
