637417cf6bd571301b9230113398a3c8e4a0ee8c
Kiet Ho <hoakiet98@gmail.com>
2025-06-13 10:39:52 -0700

    use bedrock provider

--- apps/studio/electron/main/chat/llmProvider.ts
+++ apps/studio/electron/main/chat/llmProvider.ts
@@ -1,9 +1,11 @@
+import { createAmazonBedrock, type AmazonBedrockProviderSettings } from '@ai-sdk/amazon-bedrock';
 import { createAnthropic } from '@ai-sdk/anthropic';
 import type { StreamRequestType } from '@onlook/models/chat';
 import { BASE_PROXY_ROUTE, FUNCTIONS_ROUTE, ProxyRoutes } from '@onlook/models/constants';
-import { CLAUDE_MODELS, LLMProvider } from '@onlook/models/llm';
+import { BEDROCK_MODEL_MAP, CLAUDE_MODELS, LLMProvider } from '@onlook/models/llm';
 import { type LanguageModelV1 } from 'ai';
 import { getRefreshedAuthTokens } from '../auth';
+
 export interface OnlookPayload {
     requestType: StreamRequestType;
 }
@@ -28,30 +30,40 @@ async function getAnthropicProvider(
     const apiKey = import.meta.env.VITE_ANTHROPIC_API_KEY;
     const proxyUrl = `${import.meta.env.VITE_SUPABASE_API_URL}${FUNCTIONS_ROUTE}${BASE_PROXY_ROUTE}${ProxyRoutes.ANTHROPIC}`;
 
-    const config: {
-        apiKey?: string;
-        baseURL?: string;
-        headers?: Record<string, string>;
-    } = {};
-
     if (apiKey) {
-        config.apiKey = apiKey;
-    } else {
-        const authTokens = await getRefreshedAuthTokens();
-        if (!authTokens) {
-            throw new Error('No auth tokens found');
-        }
-        config.apiKey = '';
-        config.baseURL = proxyUrl;
-        config.headers = {
-            Authorization: `Bearer ${authTokens.accessToken}`,
-            'X-Onlook-Request-Type': payload.requestType,
-            'anthropic-beta': 'output-128k-2025-02-19',
-        };
+        // Use provided API key
+        const anthropic = createAnthropic({
+            apiKey,
+        });
+        return anthropic(model, {
+            cacheControl: true,
+        });
     }
 
-    const anthropic = createAnthropic(config);
-    return anthropic(model, {
-        cacheControl: true,
-    });
+    const config: AmazonBedrockProviderSettings = {};
+
+    // Use proxy URL
+    const authTokens = await getRefreshedAuthTokens();
+    if (!authTokens) {
+        throw new Error('No auth tokens found');
+    }
+    config.accessKeyId = '';
+    config.secretAccessKey = '';
+    config.region = '';
+    config.baseURL = proxyUrl;
+    config.headers = {
+        Authorization: `Bearer ${authTokens.accessToken}`,
+        'X-Onlook-Request-Type': payload.requestType,
+        'anthropic-beta': 'output-128k-2025-02-19',
+    };
+    return createAnthropicBedrock(config, model);
 }
+
+const createAnthropicBedrock = (
+    config: AmazonBedrockProviderSettings,
+    claudeModel: CLAUDE_MODELS,
+) => {
+    const bedrock = createAmazonBedrock(config);
+    const bedrockModel = BEDROCK_MODEL_MAP[claudeModel];
+    return bedrock(bedrockModel);
+};
--- packages/models/src/llm/index.ts
+++ packages/models/src/llm/index.ts
@@ -6,3 +6,8 @@ export enum CLAUDE_MODELS {
     SONNET = 'claude-3-7-sonnet-20250219',
     HAIKU = 'claude-3-5-haiku-20241022',
 }
+
+export const BEDROCK_MODEL_MAP = {
+    [CLAUDE_MODELS.SONNET]: 'anthropic.claude-sonnet-4-20250514-v1:0',
+    [CLAUDE_MODELS.HAIKU]: 'anthropic.claude-3-5-haiku-20241022-v1:0',
+};
