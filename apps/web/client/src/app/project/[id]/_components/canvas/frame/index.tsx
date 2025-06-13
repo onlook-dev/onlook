72524e676333a1db6329f63b833ead6a313f2561
Kiet Ho <hoakiet98@gmail.com>
2025-06-13 15:07:54 -0700

    move frame

--- apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
+++ apps/web/client/src/app/project/[id]/_components/canvas/frame/index.tsx
@@ -1,12 +1,13 @@
 import type { FrameImpl, WebFrameImpl } from '@/components/store/editor/frames/frame';
 import { FrameType } from '@onlook/models';
+import { observer } from 'mobx-react-lite';
 import { useRef } from 'react';
 import { GestureScreen } from './gesture';
 import { ResizeHandles } from './resize-handles';
 import { TopBar } from './top-bar';
 import { WebFrameComponent, type WebFrameView } from './web-frame';
 
-export const FrameView = ({ frame }: { frame: FrameImpl }) => {
+export const FrameView = observer(({ frame }: { frame: FrameImpl }) => {
     const webFrameRef = useRef<WebFrameView>(null);
     return (
         <div
@@ -23,4 +24,4 @@ export const FrameView = ({ frame }: { frame: FrameImpl }) => {
             </div>
         </div>
     );
-};
+});
