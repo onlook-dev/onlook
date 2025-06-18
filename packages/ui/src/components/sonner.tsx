ba859541a080ceb80eb70e442d5b7adffc29f1fa
Kiet Ho <hoakiet98@gmail.com>
2025-06-18 15:39:49 -0700

    fix toast styles

--- packages/ui/src/components/sonner.tsx
+++ packages/ui/src/components/sonner.tsx
@@ -12,9 +12,9 @@ const Toaster = ({ ...props }: ToasterProps) => {
             className="toaster group"
             style={
                 {
-                    '--normal-bg': 'var(--popover)',
-                    '--normal-text': 'var(--popover-foreground)',
-                    '--normal-border': 'var(--border)',
+                    '--normal-bg': 'hsl(var(--card))',
+                    '--normal-text': 'hsl(var(--card-foreground))',
+                    '--normal-border': 'hsl(var(--border))',
                 } as React.CSSProperties
             }
             {...props}
