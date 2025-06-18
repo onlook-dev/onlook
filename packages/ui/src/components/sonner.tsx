478f58399c2e93c6afafb38b378c7bd3b496feac
Kiet Ho <hoakiet98@gmail.com>
2025-06-18 15:39:49 -0700

    fix toast styles

--- packages/ui/src/components/sonner.tsx
+++ packages/ui/src/components/sonner.tsx
@@ -6,7 +6,20 @@ import { Toaster as Sonner, type ToasterProps, toast } from 'sonner';
 const Toaster = ({ ...props }: ToasterProps) => {
     const { theme = 'system' } = useTheme();
 
-    return <Sonner theme={theme as ToasterProps['theme']} className="toaster group" {...props} />;
+    return (
+        <Sonner
+            theme={theme as ToasterProps['theme']}
+            className="toaster group"
+            style={
+                {
+                    '--normal-bg': 'hsl(var(--card))',
+                    '--normal-text': 'hsl(var(--card-foreground))',
+                    '--normal-border': 'hsl(var(--border))',
+                } as React.CSSProperties
+            }
+            {...props}
+        />
+    );
 };
 
 export { Toaster, toast };
