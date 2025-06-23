16e4a940183b4d0403b58b6b584fdad04e1661a1
Kiet Ho <hoakiet98@gmail.com>
2025-06-23 10:21:57 -0700

    improve callback pages

--- apps/web/client/src/app/callback/stripe/cancel/index.tsx
+++
@@ -1,3 +0,0 @@
-export default function Cancel() {
-    return <div>Hello World, you cancelled your subscription</div>;
-}
--- apps/web/client/src/app/callback/stripe/cancel/page.tsx
+++ apps/web/client/src/app/callback/stripe/cancel/page.tsx
@@ -1,14 +1,12 @@
-'use client';
-
 import { Icons } from "@onlook/ui/icons/index";
 import MessageScreen from "../message-screen";
 
-export default function Success() {
+export default function Cancel() {
     return (
         <MessageScreen
-            title="Subscription Activated!"
-            message="Your subscription to Onlook has been activated. You can now close this page."
+            title="Subscription Canceled"
+            message="Your subscription to Onlook has been canceled. You can now close this page."
             icon={<Icons.CheckCircled className="size-10 text-green-500" />}
         />
-    )
+    );
 }
