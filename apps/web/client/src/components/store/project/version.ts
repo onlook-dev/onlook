92c11eaef463c8035fe6e814cf7c537cbe20b5c3
Kiet Ho <hoakiet98@gmail.com>
2025-06-27 16:10:59 -0700

    clean up

--- apps/web/client/src/components/store/project/version.ts
+++ apps/web/client/src/components/store/project/version.ts
@@ -61,12 +61,6 @@ export class VersionsManager {
                 };
             }
 
-            this.isSaving = true;
-
-            sendAnalytics('versions create commit', {
-                message,
-            });
-
             const isInitialized = await this.gitManager.isRepoInitialized();
             if (!isInitialized) {
                 await this.gitManager.initRepo();
