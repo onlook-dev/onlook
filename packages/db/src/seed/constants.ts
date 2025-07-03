b12d43e0a44f4a83ec23585ec3c9ee0825685544
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-07-03 16:41:20 -0700

    feat: update user name in db (#2346)

--- packages/db/src/seed/constants.ts
+++ packages/db/src/seed/constants.ts
@@ -1,5 +1,9 @@
 export const SEED_USER = {
-    EMAIL: 'test@test.com',
-    PASSWORD: 'test',
-    ID: '2585ea6b-6303-4f21-977c-62af2f5a21f5',
-} 
+    FIRST_NAME: 'Joan',
+    LAST_NAME: 'Doe',
+    DISPLAY_NAME: 'Joan Doe',
+    EMAIL: 'support@onlook.com',
+    PASSWORD: 'password',
+    ID: '2585ea6b-6303-4f21-977c-62af2f5a21f4',
+    AVATAR_URL: 'https://i.pravatar.cc/',
+} as const;
