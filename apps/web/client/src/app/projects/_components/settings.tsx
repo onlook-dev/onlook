539a54ed462f3e322ee74b07540ca9d6ad1a193c
Kiet Ho <hoakiet98@gmail.com>
2025-07-07 17:53:42 -0700

    clean up

--- apps/web/client/src/app/projects/_components/settings.tsx
+++ apps/web/client/src/app/projects/_components/settings.tsx
@@ -25,8 +25,8 @@ import { useEffect, useMemo, useState } from 'react';
 
 export function Settings({ project, refetch }: { project: Project; refetch: () => void }) {
     const t = useTranslations();
-    const { mutate: deleteProject } = api.project.delete.useMutation();
-    const { mutate: updateProject } = api.project.update.useMutation();
+    const { mutateAsync: deleteProject } = api.project.delete.useMutation();
+    const { mutateAsync: updateProject } = api.project.update.useMutation();
     const [showDeleteDialog, setShowDeleteDialog] = useState(false);
     const [showRenameDialog, setShowRenameDialog] = useState(false);
     const [projectName, setProjectName] = useState(project.name);
@@ -42,8 +42,8 @@ export function Settings({ project, refetch }: { project: Project; refetch: () =
         refetch();
     };
 
-    const handleRenameProject = () => {
-        updateProject(
+    const handleRenameProject = async () => {
+        await updateProject(
             {
                 id: project.id,
                 project: {
