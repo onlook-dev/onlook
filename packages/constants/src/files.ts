a5782d02f0616eeec96dffc52958a86345fdb8ae
Kiet Ho <hoakiet98@gmail.com>
2025-07-10 09:19:24 -0700

    move router type

--- packages/constants/src/files.ts
+++ packages/constants/src/files.ts
@@ -117,13 +117,3 @@ export const COMPRESSION_IMAGE_PRESETS = {
         effort: 6,
     },
 } as const;
-
-export enum RouterType {
-    APP = 'app',
-    PAGES = 'pages',
-}
-
-export type RouterConfig = {
-    type: RouterType;
-    basePath: string;
-};
