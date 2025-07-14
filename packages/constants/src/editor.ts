45ca5036b826829721b0929b4eba42a6dec8bdb3
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-07-14 16:18:54 -0700

    feat: use vercel as preload cdn (#2443)

--- packages/constants/src/editor.ts
+++ packages/constants/src/editor.ts
@@ -70,8 +70,6 @@ export const DefaultSettings = {
 };
 
 export const DEFAULT_COLOR_NAME = 'DEFAULT';
-
-export const PRELOAD_SCRIPT_SRC =
+export const PRELOAD_SCRIPT_SRC = 'https://onlook.com/preload/index.js';
+export const DEPRECATED_PRELOAD_SCRIPT_SRC =
     'https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/preload/dist/index.js';
-
-export const DEPRECATED_PRELOAD_SCRIPT_SRC = 'onlook-dev/web';
