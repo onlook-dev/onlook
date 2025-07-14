dcd3f8a72cc65d81aa7e373695c34351c50a1a9c
Kiet Ho <hoakiet98@gmail.com>
2025-07-13 23:50:07 -0700

    better loading

--- apps/web/client/src/app/project/[id]/_components/top-bar/publish/dropdown/loading.tsx
+++ apps/web/client/src/app/project/[id]/_components/top-bar/publish/dropdown/loading.tsx
@@ -3,7 +3,7 @@ import { Progress } from '@onlook/ui/progress';
 export const LoadingState = ({ type, message, progress }: { type: 'preview' | 'custom', message: string, progress: number }) => {
     return (
         <div className="p-4 flex flex-col gap-2">
-            <p className="text-foreground-primary">{type === 'preview' ? 'Preview' : 'Custom'} domain</p>
+            <p className="text-foreground-primary">{type === 'preview' ? 'Base' : 'Custom'} domain</p>
             <p className="text-foreground-secondary">{message}</p>
             <Progress value={progress} className="w-full" />
         </div>
