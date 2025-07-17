9725a2698ca6f3c95a66e66240a8ef2031511ca8
Kiet Ho <hoakiet98@gmail.com>
2025-07-17 15:07:22 -0700

    clean up

--- apps/web/template/app/layout.tsx
+++ apps/web/template/app/layout.tsx
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
     openGraph: {
+        url: 'https://onlook.com',
         siteName: 'Onlook',
     },
     title: 'My New App',
