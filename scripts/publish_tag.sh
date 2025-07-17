160267f1ba922497f5d41edea26bd10f2f4200b3
Kiet Ho <hoakiet98@gmail.com>
2025-07-16 17:24:23 -0700

    Publish version v0.2.31

--- scripts/publish_tag.sh
+++ scripts/publish_tag.sh
@@ -11,4 +11,4 @@ git commit -m "Publish version v$VERSION"
 git tag -a v$VERSION -m "Version $VERSION"
 
 # Push the tag to the remote repository
-git push --follow-tags origin HEAD
+git push --follow-tags https://github.com/onlook-dev/onlook.git HEAD
