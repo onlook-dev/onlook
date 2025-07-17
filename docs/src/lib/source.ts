d8205a970346aace6ec9979cf191b729516e718f
itsNintu <alex@creatorsofweb.com>
2025-07-18 03:41:08 +0700

    Added SEO fixes for main site and docs.
    Lots of changes, I should have done multiple commits.
    
    Both main site and docs have next-sitemap added and configured. All other baseline technical seo implementations have also been completed (both main site and docs).
    
    Docs I have also restructured to make sense to the user, removing some old rendundant content and updating other to work for onlook web.
    
    Stuck with a build error: Module not found: Package path ./hide-if-empty is not exported from package, not sure if this is just a local issue.

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -4,6 +4,6 @@ import { loader } from 'fumadocs-core/source';
 // See https://fumadocs.vercel.app/docs/headless/source-api for more info
 export const source = loader({
     // it assigns a URL to your pages
-    baseUrl: '/docs',
+    baseUrl: '/',
     source: docs.toFumadocsSource(),
 });
