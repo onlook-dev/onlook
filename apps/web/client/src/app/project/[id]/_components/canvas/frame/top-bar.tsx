9e186d692e33aae48b11f50193666588758027bc
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-07-18 15:06:44 -0700

    Revert "feat: pages selector in top bar (#2471)" (#2484)
    This reverts commit 7eb67e7ae79836cdaabb5fe3d668c7622b5cd38b.

--- apps/web/client/src/app/project/[id]/_components/canvas/frame/top-bar.tsx
+++ apps/web/client/src/app/project/[id]/_components/canvas/frame/top-bar.tsx
@@ -7,107 +7,114 @@ import { observer } from 'mobx-react-lite';
 import Link from 'next/link';
 import { useRef } from 'react';
 import { HoverOnlyTooltip } from '../../editor-bar/hover-tooltip';
-import { PageSelector } from './page-selector';
 
-export const TopBar = observer(({ frame }: { frame: WebFrame }) => {
-    const editorEngine = useEditorEngine();
-    const isSelected = editorEngine.frames.isSelected(frame.id);
-    const topBarRef = useRef<HTMLDivElement>(null);
-    const toolBarRef = useRef<HTMLDivElement>(null);
-    const topBarWidth = topBarRef.current?.clientWidth ?? 0;
-    const toolBarWidth = toolBarRef.current?.clientWidth ?? 0;
-    const padding = 210;
-    const shouldShowExternalLink = (topBarWidth - toolBarWidth - padding) * editorEngine.canvas.scale > 250;
+export const TopBar = observer(
+    ({ frame }: { frame: WebFrame }) => {
+        const editorEngine = useEditorEngine();
+        const isSelected = editorEngine.frames.isSelected(frame.id);
+        const topBarRef = useRef<HTMLDivElement>(null);
+        const urlRef = useRef<HTMLDivElement>(null);
+        const topBarWidth = (topBarRef.current?.clientWidth ?? 0);
+        const urlWidth = (urlRef.current?.clientWidth ?? 0);
+        const shouldShowExternalLink = ((topBarWidth - urlWidth) * editorEngine.canvas.scale) > 250;
 
-    const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        clearElements();
-
-        const startX = e.clientX;
-        const startY = e.clientY;
-        const startPositionX = frame.position.x;
-        const startPositionY = frame.position.y;
-
-        const handleMove = (e: MouseEvent) => {
-            const scale = editorEngine.canvas.scale;
-            const deltaX = (e.clientX - startX) / scale;
-            const deltaY = (e.clientY - startY) / scale;
-
-            const newPosition = {
-                x: startPositionX + deltaX,
-                y: startPositionY + deltaY,
-            };
-
-            editorEngine.frames.updateAndSaveToStorage(frame.id, { position: newPosition });
-        };
-
-        const endMove = (e: MouseEvent) => {
+        const handleMouseDown = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
             e.preventDefault();
             e.stopPropagation();
-            window.removeEventListener('mousemove', handleMove);
-            window.removeEventListener('mouseup', endMove);
+            clearElements();
+
+            const startX = e.clientX;
+            const startY = e.clientY;
+            const startPositionX = frame.position.x;
+            const startPositionY = frame.position.y;
+
+            const handleMove = (e: MouseEvent) => {
+                const scale = editorEngine.canvas.scale;
+                const deltaX = (e.clientX - startX) / scale;
+                const deltaY = (e.clientY - startY) / scale;
+
+                const newPosition = {
+                    x: startPositionX + deltaX,
+                    y: startPositionY + deltaY,
+                };
+
+                editorEngine.frames.updateAndSaveToStorage(frame.id, { position: newPosition });
+            };
+
+            const endMove = (e: MouseEvent) => {
+                e.preventDefault();
+                e.stopPropagation();
+                window.removeEventListener('mousemove', handleMove);
+                window.removeEventListener('mouseup', endMove);
+            };
+
+            window.addEventListener('mousemove', handleMove);
+            window.addEventListener('mouseup', endMove);
         };
 
-        window.addEventListener('mousemove', handleMove);
-        window.addEventListener('mouseup', endMove);
-    };
+        const clearElements = () => {
+            editorEngine.elements.clear();
+            editorEngine.overlay.clear();
+        };
 
-    const clearElements = () => {
-        editorEngine.elements.clear();
-        editorEngine.overlay.clear();
-    };
+        const handleReload = () => {
+            editorEngine.frames.reloadView(frame.id);
+        };
 
-    const handleReload = () => {
-        editorEngine.frames.reloadView(frame.id);
-    };
+        const handleClick = () => {
+            editorEngine.frames.select([frame]);
+        };
 
-    const handleClick = () => {
-        editorEngine.frames.select([frame]);
-    };
-
-    return (
-        <div
-            ref={topBarRef}
-            className={cn(
-                'rounded-lg bg-background-primary/10 hover:shadow h-6 m-auto flex flex-row items-center backdrop-blur-lg overflow-hidden relative shadow-sm border-input text-foreground-secondary group-hover:text-foreground cursor-grab active:cursor-grabbing',
-                isSelected && 'text-teal-400 fill-teal-400',
-            )}
-            style={{
-                height: `${28 / editorEngine.canvas.scale}px`,
-                width: `${frame.dimension.width}px`,
-                marginBottom: `${10 / editorEngine.canvas.scale}px`,
-            }}
-            onMouseDown={handleMouseDown}
-            onClick={handleClick}
-        >
+        return (
             <div
-                className="flex flex-row items-center gap-1"
+                ref={topBarRef}
+                className={
+                    cn(
+                        'rounded-lg bg-background-primary/10 hover:shadow h-6 m-auto flex flex-row items-center backdrop-blur-lg overflow-hidden relative shadow-sm border-input text-foreground-secondary group-hover:text-foreground cursor-grab active:cursor-grabbing',
+                        isSelected && 'text-teal-400 fill-teal-400',
+                    )
+                }
                 style={{
-                    transform: `scale(${1 / editorEngine.canvas.scale})`,
-                    transformOrigin: 'left center',
+                    height: `${28 / editorEngine.canvas.scale}px`,
+                    width: `${frame.dimension.width}px`,
+                    marginBottom: `${10 / editorEngine.canvas.scale}px`,
                 }}
-                ref={toolBarRef}
+                onMouseDown={handleMouseDown}
+                onClick={handleClick}
             >
-                <HoverOnlyTooltip content="Refresh Page" side="top" className="mb-1" hideArrow>
-                    <Button
-                        variant="ghost"
-                        size="icon"
-                        className="cursor-pointer"
-                        onClick={handleReload}
+                <div
+                    className="flex flex-row items-center gap-2"
+                    style={{
+                        transform: `scale(${1 / editorEngine.canvas.scale})`,
+                        transformOrigin: 'left center',
+                    }}
+                >
+                    <HoverOnlyTooltip
+                        content="Refresh Page"
+                        side="top"
+                        className='mb-1'
+                        hideArrow
                     >
-                        <Icons.Reload />
-                    </Button>
-                </HoverOnlyTooltip>
-                <PageSelector frame={frame} />
-                <div className="text-small overflow-hidden text-ellipsis whitespace-nowrap" >
-                    {frame.url}
+                        <Button variant="ghost" size="icon" className="cursor-pointer" onClick={handleReload}>
+                            <Icons.Reload />
+                        </Button>
+                    </HoverOnlyTooltip>
+
+                        <div
+                            ref={urlRef}
+                            className="text-small overflow-hidden text-ellipsis whitespace-nowrap">
+                            {frame.url}
+                        </div>
                 </div>
-            </div>
-            <HoverOnlyTooltip content="Preview in new tab" side="top" hideArrow className="mb-1">
+                <HoverOnlyTooltip
+                    content="Preview in new tab"
+                    side="top"
+                    hideArrow
+                    className='mb-1'
+                >
                 <Link
                     className="absolute right-1 top-1/2 -translate-y-1/2 transition-opacity duration-300"
-                    href={frame.url.replace(/\[([^\]]+)\]/g, 'temp-$1')} // Dynamic routes are not supported so we replace them with a temporary value
+                    href={frame.url}
                     target="_blank"
                     style={{
                         transform: `scale(${1 / editorEngine.canvas.scale})`,
@@ -120,7 +127,10 @@ export const TopBar = observer(({ frame }: { frame: WebFrame }) => {
                         <Icons.ExternalLink />
                     </Button>
                 </Link>
-            </HoverOnlyTooltip>
-        </div>
-    );
-});
+                </HoverOnlyTooltip>
+
+            </div>
+        );
+    },
+);
+
