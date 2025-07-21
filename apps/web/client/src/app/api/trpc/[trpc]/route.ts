83b4d593e6d6812c05a7d46b1e6e08a631869f09
Kiet Ho <hoakiet98@gmail.com>
2025-07-21 15:23:06 -0700

    revert vercel.json

--- apps/web/client/src/app/api/trpc/[trpc]/route.ts
+++ apps/web/client/src/app/api/trpc/[trpc]/route.ts
@@ -4,8 +4,6 @@ import { env } from '~/env';
 import { appRouter } from '~/server/api/root';
 import { createTRPCContext } from '~/server/api/trpc';
 
-export const maxDuration = 800;
-
 /**
  * This wraps the `createTRPCContext` helper and provides the required context for the tRPC API when
  * handling a HTTP request (e.g. when you make requests from Client Components).
