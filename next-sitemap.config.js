1a33c85362bde59028571a5b1849029670825d26
Kiet Ho <hoakiet98@gmail.com>
2025-07-22 10:10:30 -0700

    remove sitemap

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: process.env.NEXT_PUBLIC_SITE_URL || 'https://onlook.com',
-  generateRobotsTxt: true,
-  exclude: ['/api/*', '/sitemap', '/404'],
-  // Uncomment below if you expect huge URL count
-  // sitemapSize: 5000,
-};
