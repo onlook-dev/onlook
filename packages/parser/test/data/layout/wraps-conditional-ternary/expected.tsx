1a038bc27af005fe83679a66c5111be5215f7d92
vutnguyen <vu.tran.nguyen@c0x12c.com>
2025-07-23 06:52:29 +0700

    fix: output script file

--- packages/parser/test/data/layout/wraps-conditional-ternary/expected.tsx
+++ packages/parser/test/data/layout/wraps-conditional-ternary/expected.tsx
@@ -1,4 +1,4 @@
 import React from 'react';import Script from "next/script";
 export default function Layout() {
-  return <html lang="en"><body><Script src="onlook-preload-script.js" strategy="beforeInteractive" type="module" id="onlook-preload-script.js"></Script>
+  return <html lang="en"><body><Script src="/onlook-preload-script.js" strategy="beforeInteractive" type="module" id="onlook-preload-script.js"></Script>
       {true ? <div /> : <span />}</body></html>;}
