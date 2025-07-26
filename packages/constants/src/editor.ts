31c5528f2ff51603b728ea5935f6cc6ccfc4cc79
Kiet Ho <hoakiet98@gmail.com>
2025-07-25 19:23:11 -0700

    udpate

--- packages/constants/src/editor.ts
+++ packages/constants/src/editor.ts
@@ -74,7 +74,7 @@ export const DEFAULT_COLOR_NAME = 'DEFAULT';
 
 const CDN_PRELOAD_SCRIPT_SRC =
     'https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/client/public/onlook-preload-script.js';
-const LOCAL_PRELOAD_SCRIPT_SRC = `/onlook-preload-script.js`;
+export const LOCAL_PRELOAD_SCRIPT_SRC = `/onlook-preload-script.js`;
 export const PRELOAD_SCRIPT_SRC = isDev ? LOCAL_PRELOAD_SCRIPT_SRC : CDN_PRELOAD_SCRIPT_SRC;
 
 export const DEPRECATED_PRELOAD_SCRIPT_SRCS = [
