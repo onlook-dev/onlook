578ac6567299c917a169c4a39aed221f8d95ae78
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-07-29 12:41:41 -0700

    refactor: move script strategy to afterInteractive (#2557)
    * move script strategy to afterInteractive
    
    * fix opacity
    
    * update zaraz

--- packages/parser/test/data/layout/adds-script-if-missing/expected.tsx
+++ packages/parser/test/data/layout/adds-script-if-missing/expected.tsx
@@ -7,6 +7,6 @@ import Script from "next/script";export default function Document() {
             <body>
                 <main />
             
-        <Script src="https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/client/public/onlook-preload-script.js" strategy="beforeInteractive" type="module" id="https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/client/public/onlook-preload-script.js"></Script>
+        <Script src="https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/client/public/onlook-preload-script.js" strategy="afterInteractive" type="module" id="https://cdn.jsdelivr.net/gh/onlook-dev/onlook@main/apps/web/client/public/onlook-preload-script.js"></Script>
       </body>
         </html>);}
