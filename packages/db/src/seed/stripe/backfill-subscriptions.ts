96f89d3f9a5d81a240ded2f58a4c549c3344c23a
Kiet Ho <hoakiet98@gmail.com>
2025-07-31 15:31:09 -0700

    clean up

--- packages/db/src/seed/stripe/backfill-subscriptions.ts
+++ packages/db/src/seed/stripe/backfill-subscriptions.ts
@@ -6,6 +6,7 @@ import { usageRecords } from '@/schema/subscription/usage';
 import { users } from '@/schema/user/user';
 import { db } from '@onlook/db/src/client';
 import { UsageType } from '@onlook/models';
+import { SubscriptionStatus } from '@onlook/stripe';
 import { createStripeClient } from '@onlook/stripe/src/client';
 import { and, count, eq, gte, lt } from 'drizzle-orm';
 import type { PgTransaction } from 'drizzle-orm/pg-core';
@@ -26,6 +27,7 @@ interface DbSubscription extends Subscription {
 
 export const getAllSubscriptions = async (): Promise<DbSubscription[]> => {
     const subs = (await db.query.subscriptions.findMany({
+        where: eq(subscriptions.status, SubscriptionStatus.ACTIVE),
         with: {
             product: true,
             price: true,
@@ -58,6 +60,7 @@ export const getStripeItems = async (subscriptions: DbSubscription[]) => {
 }
 
 const insertRateLimit = async (tx: PgTransaction<any, any, any>, item: StripeItem) => {
+    console.log(`Inserting rate limit for subscription ${item.subscription.id}`);
     // One month from the start date. NOT SURE IF THIS IS CORRECT.
     const endDate = new Date(item.subscription.startedAt.getTime() + 30 * 24 * 60 * 60 * 1000);
     console.log(`End date: ${endDate}`);
@@ -78,8 +81,7 @@ const insertRateLimit = async (tx: PgTransaction<any, any, any>, item: StripeIte
     const usageCount = usageCountResult[0]?.count || 0;
     const remainingUsage = Math.max(0, item.subscription.price.monthlyMessageLimit - usageCount);
 
-    // Insert rate limit record
-    await tx.insert(rateLimits).values({
+    const insertValue = {
         userId: item.subscription.userId,
         subscriptionId: item.subscription.id,
         startedAt: item.subscription.startedAt,
@@ -89,28 +91,40 @@ const insertRateLimit = async (tx: PgTransaction<any, any, any>, item: StripeIte
         carryOverKey: uuid(),
         carryOverTotal: 0,
         stripeSubscriptionItemId: item.stripeSubscriptionItemId,
-    });
+    }
+
+    console.log(`Inserting rate limit for subscription ${item.subscription.id}: ${JSON.stringify(insertValue, null, 2)}`);
+    // Insert rate limit record
+    await tx.insert(rateLimits).values(insertValue)
 }
 
 export const backfillSubscriptions = async () => {
+    console.log('Backfilling subscriptions...');
     const subs = await getAllSubscriptions();
+    console.log(`Found ${subs.length} subscriptions`);
     const stripeItems = await getStripeItems(subs);
+    console.log(`Found ${stripeItems.length} stripe items`);
 
     for (const item of stripeItems) {
-        db.transaction(async (tx: PgTransaction<any, any, any>) => {
+        console.log(`Backfilling subscription ${item.subscription.id}`);
+        await db.transaction(async (tx) => {
+            console.log(`Updating subscription ${item.subscription.id}`);
             // Update subscription
             await tx.update(subscriptions).set({
                 stripeCurrentPeriodStart: item.stripeCurrentPeriodStart,
                 stripeCurrentPeriodEnd: item.stripeCurrentPeriodEnd,
             }).where(eq(subscriptions.id, item.subscription.id));
 
+            console.log(`Updating user ${item.subscription.userId}`);
             // Update user
             await tx.update(users).set({
                 stripeCustomerId: item.stripeCustomerId,
             }).where(eq(users.id, item.subscription.userId));
 
+            console.log(`Inserting rate limit for subscription ${item.subscription.id}`);
             // Insert rate limit based on usage records and subscription price
             await insertRateLimit(tx, item);
+            console.log(`Rate limit inserted for subscription ${item.subscription.id}`);
         });
     }
 }
