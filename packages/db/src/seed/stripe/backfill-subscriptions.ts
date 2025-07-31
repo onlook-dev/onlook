9d1ef768772dd733cac591eb7b73170cfa5f7960
Kiet Ho <hoakiet98@gmail.com>
2025-07-31 14:42:42 -0700

    add backfill script

--- packages/db/src/seed/stripe/backfill-subscriptions.ts
+++ packages/db/src/seed/stripe/backfill-subscriptions.ts
@@ -1,51 +1,67 @@
 // Backfill stripe subscriptions with current period start and end
 
 import { subscriptions, type Subscription } from '@/schema/subscription/subscription';
+import { users } from '@/schema/user/user';
 import { db } from '@onlook/db/src/client';
 import { createStripeClient } from '@onlook/stripe/src/client';
 import { eq } from 'drizzle-orm';
-import Stripe from 'stripe';
 
 export const getAllSubscriptions = async (): Promise<Subscription[]> => {
     const subs = await db.select().from(subscriptions);
     return subs;
 }
 
-export const getStripeSubscriptionItems = async (stripeSubscriptionItemIds: string[]): Promise<Stripe.SubscriptionItem[]> => {
+export const getStripeItems = async (subscriptions: Subscription[]) => {
     const stripe = createStripeClient();
-    let stripeSubItems: Stripe.SubscriptionItem[] = [];
+    const items: {
+        userId: string,
+        subscriptionId: string,
+        stripeSubscriptionItemId: string,
+        stripeCustomerId: string,
+        stripeCurrentPeriodStart: Date,
+        stripeCurrentPeriodEnd: Date,
+    }[] = [];
 
-    for (const id of stripeSubscriptionItemIds) {
-        const subscriptionItem = await stripe.subscriptionItems.retrieve(id);
-        stripeSubItems.push(subscriptionItem);
+    for (const sub of subscriptions) {
+        const subscriptionItem = await stripe.subscriptionItems.retrieve(sub.stripeSubscriptionItemId);
+        const stripeCurrentPeriodStart = new Date(subscriptionItem.current_period_start * 1000);
+        const stripeCurrentPeriodEnd = new Date(subscriptionItem.current_period_end * 1000);
+        const stripeSubscription = await stripe.subscriptions.retrieve(sub.stripeSubscriptionId);
+
+        items.push({
+            userId: sub.userId,
+            subscriptionId: sub.id,
+            stripeSubscriptionItemId: sub.stripeSubscriptionItemId,
+            stripeCustomerId: stripeSubscription.customer as string,
+            stripeCurrentPeriodStart,
+            stripeCurrentPeriodEnd,
+        });
     }
-    return stripeSubItems;
+
+    return items;
 }
 
 export const backfillSubscriptions = async () => {
     const subs = await getAllSubscriptions();
-    const stripeSubItems = await getStripeSubscriptionItems(subs.map((s) => s.stripeSubscriptionItemId));
-    for (const sub of subs) {
-        const stripeSubItem = stripeSubItems.find((s) => s.id === sub.stripeSubscriptionItemId);
-        if (!stripeSubItem) {
-            console.log(`Subscription ${sub.stripeSubscriptionId} not found in Stripe`);
-            continue;
-        }
+    const stripeItems = await getStripeItems(subs);
 
-        const stripeCurrentPeriodStart = new Date(stripeSubItem.current_period_start * 1000);
-        const stripeCurrentPeriodEnd = new Date(stripeSubItem.current_period_end * 1000);
+    for (const item of stripeItems) {
+        db.transaction(async (tx) => {
+            // Update subscription
+            await tx.update(subscriptions).set({
+                stripeCurrentPeriodStart: item.stripeCurrentPeriodStart,
+                stripeCurrentPeriodEnd: item.stripeCurrentPeriodEnd,
+            }).where(eq(subscriptions.id, item.subscriptionId));
 
-        console.log(stripeCurrentPeriodStart);
-        console.log(stripeCurrentPeriodEnd);
-
-        await db.update(subscriptions).set({
-            stripeCurrentPeriodStart,
-            stripeCurrentPeriodEnd,
-        }).where(eq(subscriptions.id, sub.id));
+            // Update user
+            await tx.update(users).set({
+                stripeCustomerId: item.stripeCustomerId,
+            }).where(eq(users.id, item.userId));
+        });
     }
 }
 
 (async () => {
-    console.log(await getAllSubscriptions());
+    await backfillSubscriptions();
     process.exit(0);
 })();
