e5a318d5091f1914710840f7ab8f5629b4fddf6d
Kiet Ho <hoakiet98@gmail.com>
2025-08-11 21:42:09 -0700

    handle error handler

--- apps/web/client/src/app/api/chat/route.ts
+++ apps/web/client/src/app/api/chat/route.ts
@@ -92,17 +92,22 @@ export const streamResponse = async (req: NextRequest) => {
 }
 
 export function errorHandler(error: unknown) {
-    if (error == null) {
+    try {
+        console.error('Error in chat', error);
+        if (!error) {
+            return 'unknown error';
+        }
+
+        if (typeof error === 'string') {
+            return error;
+        }
+
+        if (error instanceof Error) {
+            return error.message;
+        }
+        return JSON.stringify(error);
+    } catch (error) {
+        console.error('Error in errorHandler', error);
         return 'unknown error';
     }
-
-    if (typeof error === 'string') {
-        return error;
-    }
-
-    if (error instanceof Error) {
-        return error.message;
-    }
-
-    return JSON.stringify(error);
 }
