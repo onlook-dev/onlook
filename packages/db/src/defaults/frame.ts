0844d6f0b0f3ace3a959ba12c54f505927147bd2
Kiet Ho <hoakiet98@gmail.com>
2025-08-13 14:09:41 -0700

    feat: add multiple frames to a new project

--- packages/db/src/defaults/frame.ts
+++ packages/db/src/defaults/frame.ts
@@ -3,15 +3,16 @@ import type { Frame as DbFrame } from '@onlook/db';
 import { FrameType } from '@onlook/models';
 import { v4 as uuidv4 } from 'uuid';
 
-export const createDefaultFrame = (canvasId: string, url: string): DbFrame => {
+export const createDefaultFrame = (canvasId: string, url: string, overrides?: Partial<DbFrame>): DbFrame => {
     return {
         id: uuidv4(),
-        canvasId: canvasId,
+        canvasId,
         type: FrameType.WEB,
-        url: url,
+        url,
         x: DefaultSettings.FRAME_POSITION.x.toString(),
         y: DefaultSettings.FRAME_POSITION.y.toString(),
         width: DefaultSettings.FRAME_DIMENSION.width.toString(),
         height: DefaultSettings.FRAME_DIMENSION.height.toString(),
+        ...overrides,
     };
 };
