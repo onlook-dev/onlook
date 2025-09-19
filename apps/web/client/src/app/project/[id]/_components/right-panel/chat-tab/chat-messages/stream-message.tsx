c495b6503fdd99604572bbef2d2af33b77869fc5
Kiet Ho <hoakiet98@gmail.com>
2025-09-18 22:01:03 -0700

    make thinking display more consistently

--- apps/web/client/src/app/project/[id]/_components/right-panel/chat-tab/chat-messages/stream-message.tsx
+++ apps/web/client/src/app/project/[id]/_components/right-panel/chat-tab/chat-messages/stream-message.tsx
@@ -1,27 +1,15 @@
 import { type ChatMessage } from '@onlook/models';
-import { Icons } from '@onlook/ui/icons';
-import { useMemo } from 'react';
 import { MessageContent } from './message-content';
 
 export const StreamMessage = ({ message }: { message: ChatMessage }) => {
-    const isAssistantStreamMessage = useMemo(() => message?.role === 'assistant', [message?.role]);
-
-    return (
-        <>
-            {message && isAssistantStreamMessage && (
-                <div className="px-4 pt-2 text-small content-start flex flex-col text-wrap gap-2">
-                    <MessageContent
-                        messageId={message.id}
-                        parts={message.parts}
-                        applied={false}
-                        isStream={true}
-                    />
-                </div>
-            )}
-            <div className="flex w-full h-full flex-row items-center gap-2 px-4 my-2 text-small content-start text-foreground-secondary">
-                <Icons.LoadingSpinner className="animate-spin" />
-                <p>Thinking ...</p>
-            </div>
-        </>
+    return message && (
+        <div className="px-4 pt-2 text-small content-start flex flex-col text-wrap gap-2">
+            <MessageContent
+                messageId={message.id}
+                parts={message.parts}
+                applied={false}
+                isStream={true}
+            />
+        </div>
     );
 };
