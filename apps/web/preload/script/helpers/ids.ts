0a1e602d0f2e81d71beb4ce64693487bf1ac8b12
Kiet <31864905+Kitenite@users.noreply.github.com>
2025-10-01 11:32:13 -0700

    fix: misc types (#2943)

--- apps/web/preload/script/helpers/ids.ts
+++ apps/web/preload/script/helpers/ids.ts
@@ -2,7 +2,7 @@ import { EditorAttributes } from '@onlook/constants';
 import { nanoid } from 'nanoid/non-secure';
 
 export function getOrAssignDomId(node: HTMLElement): string {
-    let domId = node.getAttribute(EditorAttributes.DATA_ONLOOK_DOM_ID) as string;
+    let domId = node.getAttribute(EditorAttributes.DATA_ONLOOK_DOM_ID);
     if (!domId) {
         domId = `odid-${nanoid()}`;
         node.setAttribute(EditorAttributes.DATA_ONLOOK_DOM_ID, domId);
@@ -12,10 +12,10 @@ export function getOrAssignDomId(node: HTMLElement): string {
 
 export const VALID_DATA_ATTR_CHARS = 'abcdefghijklmnopqrstuvwxyz0123456789-._:';
 
-export function getOid(node: HTMLElement): string | undefined {
-    return node.getAttribute(EditorAttributes.DATA_ONLOOK_ID) as string;
+export function getOid(node: HTMLElement): string | null {
+    return node.getAttribute(EditorAttributes.DATA_ONLOOK_ID);
 }
 
-export function getInstanceId(node: HTMLElement): string | undefined {
-    return node.getAttribute(EditorAttributes.DATA_ONLOOK_INSTANCE_ID) as string;
+export function getInstanceId(node: HTMLElement): string | null {
+    return node.getAttribute(EditorAttributes.DATA_ONLOOK_INSTANCE_ID);
 }
